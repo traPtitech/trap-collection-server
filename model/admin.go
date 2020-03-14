1f32f2c3c578ea0a81d5f2da09b86752691257f5
mazrean <mazrean@localhost.localdomain>
2020-03-14 20:08:50 +0900

    init

--- model/admin.go
+++
@@ -1,6 +0,0 @@
-package model
-
-//AdminList 管理者一覧の構造体
-type AdminList struct {
-	Admin string `json:"id,omitempty" db:"user_traqid"`
-}
--- model/seat.go
+++
@@ -1,7 +0,0 @@
-package model
-
-//PostSeat Postされる席の構造体
-type PostSeat struct {
-	ID     string `json:"id,omitempty"`
-	Status string `json:"status,omitempty"`
-}
