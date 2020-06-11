e5ae621bfba7f3d5732c236f85eed1306b1ca7fe
mazrean <50093633+mazrean@users.noreply.github.com>
2020-06-11 21:44:04 +0900

    たぶん動くと思う

--- mockgen.sh
+++ mockgen.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-sudo docker-compose -f docker/mock/docker-compose.yml up
+sudo COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker/mock/docker-compose.yml up
 sudo docker-compose -f docker/mock/docker-compose.yml down
