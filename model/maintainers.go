dfa5bb9b339293e5a6333c5c88669f1ba0aa064a
mazrean <shunsuke.w04@gmail.com>
2020-09-17 08:29:17 +0900

    post /games/:gameID/maintainers実装完

--- model/maintainers.go
+++ model/maintainers.go
@@ -3,9 +3,11 @@ package model
 //go:generate mockgen -source=$GOFILE -destination=mock_${GOFILE} -package=$GOPACKAGE
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/jinzhu/gorm"
+	gormbulk "github.com/t-tiger/gorm-bulk-insert"
 )
 
 // Maintainer gameのmaintainerの構造体
@@ -22,11 +24,13 @@ type Maintainer struct {
 // MaintainerMeta maintainerテーブルのリポジトリ
 type MaintainerMeta interface {
 	CheckMaintainerID(userID string, gameID string) (bool, error)
+	InsertMaintainer(gameID string, userIDs []string) error
 }
 
 // CheckMaintainerID ゲームの管理者のチェック
 func (*DB) CheckMaintainerID(userID string, gameID string) (bool, error) {
 	var maintainer Maintainer
+
 	err := db.Select("user_id").
 		Where("game_id = ? AND user_id = ?", gameID, userID).
 		First(&maintainer).Error
@@ -35,5 +39,25 @@ func (*DB) CheckMaintainerID(userID string, gameID string) (bool, error) {
 	} else if err != nil {
 		return false, err
 	}
+
 	return true, nil
+}
+
+// InsertMaintainer 管理者の追加
+func (*DB) InsertMaintainer(gameID string, userIDs []string) error {
+	interfaceUserIDs := make([]interface{}, 0, len(userIDs))
+	for _,user := range userIDs {
+		interfaceUserIDs = append(interfaceUserIDs, Maintainer{
+			GameID: gameID,
+			UserID: user,
+			Role: 0,
+		})
+	}
+
+	err := gormbulk.BulkInsert(db, interfaceUserIDs, 3000, "CreatedAt", "DeletedAt")
+	if err != nil {
+		return fmt.Errorf("failed to bulk insert maintainers: %w", err)
+	}
+
+	return nil
 }
