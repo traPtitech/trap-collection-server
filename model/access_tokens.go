9083c72466b96328a3503b0d31ead2032ac9d9f3
mazrean <shunsuke.w04@gmail.com>
2021-05-05 17:40:52 +0900

    fix expired_at datetime

--- model/access_tokens.go
+++ model/access_tokens.go
@@ -10,7 +10,7 @@ type AccessToken struct {
 	ID          uint         `gorm:"type:int(11) unsigned auto_increment;PRIMARY_KEY;"`
 	KeyID       string       `gorm:"type:varchar(36);NOT NULL;"`
 	AccessToken string       `gorm:"type:varchar(36);NOT NULL;"`
-	ExpiresIn   int          `gorm:"type:int(11);NOT NULL;"`
+	ExpiresIn   time.Time    `gorm:"type:int(11);NOT NULL;"`
 	CreatedAt   time.Time    `gorm:"type:datetime;NOT NULL;"`
 	DeletedAt   sql.NullTime `gorm:"type:datetime;DEFAULT:NULL;"`
 }
