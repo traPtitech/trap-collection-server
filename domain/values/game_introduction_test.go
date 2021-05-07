d9ea8fb6c6feaa11f04c7b4decfcfe92727234d3
mazrean <shunsuke.w04@gmail.com>
2021-05-08 02:34:59 +0900

    game introduction domain test

---
+++ domain/values/game_introduction_test.go
@@ -0,0 +1,60 @@
+package values
+
+import (
+	"testing"
+	"testing/quick"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewGameIntoductionIDFromString(t *testing.T) {
+	t.Parallel()
+
+	assertion := assert.New(t)
+
+	tests := []struct{
+		description string
+		id string
+		err error
+	} {
+		{
+			description: "正しいuuid(大文字)なのでエラーなし",
+			id: "BFDF0B4B-0A5A-45E8-A41C-0976D12A115F",
+			err: nil,
+		},
+		{
+			description: "正しいuuid(小文字)なのでエラーなし",
+			id: "bfdf0b4b-0a5a-45e8-a41c-0976d12a115f",
+			err: nil,
+		},
+		{
+			description: "文字数が少ないのでエラー",
+			id: "BFDF0B4B-0A5A-45E8-A41C-0976D12A115",
+			err: ErrInvalidFormat,
+		},
+		{
+			description: "文字数0なのでエラー",
+			id: "",
+			err: ErrInvalidFormat,
+		},
+	}
+
+	for _, test := range tests {
+		_, err := NewGameIntoductionIDFromString(test.id)
+
+		if test.err == nil {
+			assertion.NoErrorf(err, test.description+"/no error")
+		} else {
+			assertion.ErrorIs(err, test.err, test.description+"/error")
+		}
+	}
+
+	err := quick.Check(func(id id) bool {
+		_, err := NewGameIntoductionIDFromString(uuid.UUID(id).String())
+		return err == nil
+	}, nil)
+	if err != nil {
+		t.Error("black box test error:", err.Error())
+	}
+}
