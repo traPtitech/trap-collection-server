9fef7c481a2d16e257fbadaf09d7ae155ed39899
mazrean <shunsuke.w04@gmail.com>
2021-05-18 21:42:45 +0900

    impl PostSeatVersion

--- router/seat_version.go
+++ router/seat_version.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"fmt"
+
 	"github.com/traPtitech/trap-collection-server/model"
 	"github.com/traPtitech/trap-collection-server/openapi"
 )
@@ -15,4 +17,13 @@ func newSeatVersion(db model.DBMeta) *SeatVersion {
 	return &SeatVersion{
 		db: db,
 	}
+}
+
+func (sv *SeatVersion) PostSeatVersion(newSeatVersion *openapi.NewSeatVersion) (*openapi.SeatVersion, error) {
+	seatVersion, err := sv.db.InsertSeatVersion(uint(newSeatVersion.Hight), uint(newSeatVersion.Width))
+	if err != nil {
+		return nil, fmt.Errorf("failed to insert seat version: %w", err)
+	}
+
+	return seatVersion, nil
 }
