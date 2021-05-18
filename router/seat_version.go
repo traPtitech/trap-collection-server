32b71c21ce39c7cc0be50ed336bb6c154b625253
mazrean <shunsuke.w04@gmail.com>
2021-05-18 20:57:09 +0900

    create SeatVersion struct

--- router/seat_version.go
+++ router/seat_version.go
@@ -1,8 +1,18 @@
 package router
 
-import "github.com/traPtitech/trap-collection-server/openapi"
+import (
+	"github.com/traPtitech/trap-collection-server/model"
+	"github.com/traPtitech/trap-collection-server/openapi"
+)
 
 // SeatVersion 席のバージョン管理の構造体
 type SeatVersion struct {
+	db           model.DBMeta
 	openapi.SeatVersionApi
+}
+
+func newSeatVersion(db model.DBMeta) *SeatVersion {
+	return &SeatVersion{
+		db: db,
+	}
 }
