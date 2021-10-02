005b4995d9e7f17d671049b2c0a609a518308c28
mazrean <shunsuke.w04@gmail.com>
2021-10-03 02:15:40 +0900

    LauncherAuthService抽象化

---
+++ src/service/launcher_auth.go
@@ -0,0 +1,16 @@
+package service
+
+import (
+	"context"
+
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+)
+
+type LauncherAuth interface {
+	CreateLauncherUser(ctx context.Context, launcherVersionID values.LauncherVersionID, userNum int) ([]values.LauncherUserProductKey, error)
+	GetLauncherUsers(ctx context.Context, launcherVersionID values.LauncherVersionID) ([]*domain.LauncherUser, error)
+	RevokeProductKey(ctx context.Context, user values.LauncherUserID) error
+	LoginLauncher(ctx context.Context, productKey values.LauncherUserProductKey) (*domain.LauncherSession, error)
+	LauncherAuth(ctx context.Context, accessToken values.LauncherSessionAccessToken) (*domain.LauncherUser, *domain.LauncherVersion, error)
+}
