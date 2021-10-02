706d116141011f3b0e15cba2839eca1ff4870f41
mazrean <shunsuke.w04@gmail.com>
2021-10-03 03:02:33 +0900

    CreateLauncherUser実装

--- src/service/v1/launcher_auth.go
+++ src/service/v1/launcher_auth.go
@@ -1,7 +1,14 @@
 package v1
 
 import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
 	"github.com/traPtitech/trap-collection-server/src/repository"
+	"github.com/traPtitech/trap-collection-server/src/service"
 )
 
 type LauncherAuth struct {
@@ -23,4 +30,37 @@ func NewLauncherAuth(
 		launcherUserRepository:    launcherUserRepository,
 		launcherSessionRepository: launcherSessionRepository,
 	}
+}
+
+func (la *LauncherAuth) CreateLauncherUser(ctx context.Context, launcherVersionID values.LauncherVersionID, userNum int) ([]*domain.LauncherUser, error) {
+	_, err := la.launcherVersionRepository.GetLauncherVersion(ctx, launcherVersionID)
+	if errors.Is(err, repository.ErrRecordNotFound) {
+		return nil, service.ErrInvalidLauncherVersion
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to get launcher version: %w", err)
+	}
+
+	launcherUsers := make([]*domain.LauncherUser, 0, userNum)
+	for i := 0; i < userNum; i++ {
+		productKey, err := values.NewLauncherUserProductKey()
+		if err != nil {
+			return nil, fmt.Errorf("failed to create product key: %w", err)
+		}
+
+		launcherUsers = append(launcherUsers, domain.NewLauncherUser(
+			values.NewLauncherUserID(),
+			productKey,
+		))
+	}
+
+	launcherUsers, err = la.launcherUserRepository.CreateLauncherUsers(ctx, launcherVersionID, launcherUsers)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create launcher users: %w", err)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed in transaction: %w", err)
+	}
+
+	return launcherUsers, nil
 }
