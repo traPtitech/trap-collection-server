ab288ac2d0329c6c960f4bef4feae2bc2a9ce68b
mazrean <shunsuke.w04@gmail.com>
2021-10-03 20:59:31 +0900

    CreateLauncherUsers実装

--- src/repository/gorm2/launcher_user.go
+++ src/repository/gorm2/launcher_user.go
@@ -1,5 +1,15 @@
 package gorm2
 
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+)
+
 type LauncherUser struct {
 	db *DB
 }
@@ -8,4 +18,32 @@ func NewLauncherUser(db *DB) *LauncherUser {
 	return &LauncherUser{
 		db: db,
 	}
+}
+
+func (lu *LauncherUser) CreateLauncherUsers(ctx context.Context, launcherVersionID values.LauncherVersionID, launcherUsers []*domain.LauncherUser) ([]*domain.LauncherUser, error) {
+	if len(launcherUsers) == 0 {
+		return []*domain.LauncherUser{}, nil
+	}
+
+	db, err := lu.db.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get db: %w", err)
+	}
+
+	dbLauncherUsers := make([]*LauncherUserTable, 0, len(launcherUsers))
+	for _, launcherUser := range launcherUsers {
+		dbLauncherUsers = append(dbLauncherUsers, &LauncherUserTable{
+			ID:                uuid.UUID(launcherUser.GetID()),
+			LauncherVersionID: uuid.UUID(launcherVersionID),
+			ProductKey:        string(launcherUser.GetProductKey()),
+			CreatedAt:         time.Now(),
+		})
+	}
+
+	err = db.Create(&dbLauncherUsers).Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to create launcher users: %w", err)
+	}
+
+	return launcherUsers, nil
 }
