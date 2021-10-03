3246fa17dea47f80869f260c566e6b628f9293e0
mazrean <shunsuke.w04@gmail.com>
2021-10-03 18:49:32 +0900

    LauncherUserTable作成

--- src/repository/gorm2/tables.go
+++ src/repository/gorm2/tables.go
@@ -9,9 +9,23 @@ import (
 
 var (
 	tables = []interface{}{
+		&LauncherUserTable{},
 		&LauncherSessionTable{},
 	}
 )
+
+type LauncherUserTable struct {
+	ID                uuid.UUID              `gorm:"type:varchar(36);not null;primaryKey"`
+	LauncherVersionID uuid.UUID              `gorm:"type:varchar(36);not null"`
+	ProductKey        string                 `gorm:"type:varchar(29);not null;unique"`
+	CreatedAt         time.Time              `gorm:"type:datetime;not null;default:CURRENT_TIMESTAMP"`
+	DeletedAt         gorm.DeletedAt         `gorm:"type:DATETIME NULL;default:NULL"`
+	LauncherSessions  []LauncherSessionTable `gorm:"foreignKey:LauncherUserID"`
+}
+
+func (lut *LauncherUserTable) TableName() string {
+	return "launcher_users"
+}
 
 type LauncherSessionTable struct {
 	ID             uuid.UUID      `gorm:"type:varchar(36);not null;primaryKey"`
