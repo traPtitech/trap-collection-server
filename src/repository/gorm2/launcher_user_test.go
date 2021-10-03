a1231b12cb922550877899e5cda8017b0b9dedc3
mazrean <shunsuke.w04@gmail.com>
2021-10-04 00:50:29 +0900

    TestDeleteLauncherUser追加

--- src/repository/gorm2/launcher_user_test.go
+++ src/repository/gorm2/launcher_user_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/traPtitech/trap-collection-server/src/domain"
 	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/repository"
 )
 
 func TestCreateLauncherUsers(t *testing.T) {
@@ -145,6 +146,85 @@ func TestCreateLauncherUsers(t *testing.T) {
 			}
 
 			assert.ElementsMatch(t, testCase.launcherUsers, launcherUsers)
+		})
+	}
+}
+
+func TestDeleteLauncherUser(t *testing.T) {
+	t.Parallel()
+
+	launcherUserRepository := NewLauncherUser(testDB)
+
+	ctx := context.Background()
+
+	db, err := testDB.getDB(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	launcherVersionID := values.NewLauncherVersionID()
+	dbLauncherVersion := LauncherVersionTable{
+		ID:        uuid.UUID(launcherVersionID),
+		Name:      "TestDeleteLauncherUser",
+		CreatedAt: time.Now(),
+	}
+
+	err = db.Create(&dbLauncherVersion).Error
+	if err != nil {
+		t.Errorf("failed to create launcher version: %v", err)
+	}
+
+	type test struct {
+		description         string
+		validLauncherUserID bool
+		isErr               bool
+		err                 error
+	}
+
+	testCases := []test{
+		{
+			description:         "ユーザーが存在するのでエラーなし",
+			validLauncherUserID: true,
+		},
+		{
+			description:         "ユーザーが存在しないのでエラー",
+			validLauncherUserID: false,
+			isErr:               true,
+			err:                 repository.ErrNoRecordDeleted,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			launcherUserID := values.NewLauncherUserID()
+			if testCase.validLauncherUserID {
+				productKey, err := values.NewLauncherUserProductKey()
+				if err != nil {
+					t.Errorf("failed to create product key: %v", err)
+				}
+
+				dbLauncherUser := LauncherUserTable{
+					ID:                uuid.UUID(launcherUserID),
+					ProductKey:        string(productKey),
+					LauncherVersionID: uuid.UUID(launcherVersionID),
+				}
+				err = db.Create(&dbLauncherUser).Error
+				if err != nil {
+					t.Errorf("failed to create launcher user: %v", err)
+				}
+			}
+
+			err := launcherUserRepository.DeleteLauncherUser(ctx, launcherUserID)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
 		})
 	}
 }
