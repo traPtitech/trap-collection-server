fa86d9b102c88dc5167cd49d5523da7d1a4bd4d0
mazrean <shunsuke.w04@gmail.com>
2021-10-06 01:07:38 +0900

    TestGetLauncherVersionAndUserAndSessionByAccessToken追加

--- src/repository/gorm2/launcher_version_test.go
+++ src/repository/gorm2/launcher_version_test.go
@@ -292,3 +292,313 @@ func TestGetLauncherUsersByLauncherVersionID(t *testing.T) {
 		})
 	}
 }
+
+func TestGetLauncherVersionAndUserAndSessionByAccessToken(t *testing.T) {
+	t.Parallel()
+
+	launcherVersionRepository := NewLauncherVersion(testDB)
+
+	ctx := context.Background()
+
+	db, err := testDB.getDB(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	productKey1, err := values.NewLauncherUserProductKey()
+	if err != nil {
+		t.Errorf("failed to create product key: %v", err)
+	}
+
+	productKey2, err := values.NewLauncherUserProductKey()
+	if err != nil {
+		t.Errorf("failed to create product key: %v", err)
+	}
+
+	productKey3, err := values.NewLauncherUserProductKey()
+	if err != nil {
+		t.Errorf("failed to create product key: %v", err)
+	}
+
+	productKey5, err := values.NewLauncherUserProductKey()
+	if err != nil {
+		t.Errorf("failed to create product key: %v", err)
+	}
+
+	accessToken1, err := values.NewLauncherSessionAccessToken()
+	if err != nil {
+		t.Errorf("failed to create access token: %v", err)
+	}
+
+	accessToken2, err := values.NewLauncherSessionAccessToken()
+	if err != nil {
+		t.Errorf("failed to create access token: %v", err)
+	}
+
+	accessToken3, err := values.NewLauncherSessionAccessToken()
+	if err != nil {
+		t.Errorf("failed to create access token: %v", err)
+	}
+
+	accessToken4, err := values.NewLauncherSessionAccessToken()
+	if err != nil {
+		t.Errorf("failed to create access token: %v", err)
+	}
+
+	accessToken5, err := values.NewLauncherSessionAccessToken()
+	if err != nil {
+		t.Errorf("failed to create access token: %v", err)
+	}
+
+	type test struct {
+		description       string
+		dbLauncherVersion LauncherVersionTable
+		accessToken       values.LauncherSessionAccessToken
+		launcherVersion   *domain.LauncherVersion
+		launcherUser      *domain.LauncherUser
+		launcherSession   *domain.LauncherSession
+		isErr             bool
+		err               error
+	}
+
+	launcherVersionID1 := values.NewLauncherVersionID()
+	launcherVersionID2 := values.NewLauncherVersionID()
+	launcherVersionID3 := values.NewLauncherVersionID()
+	launcherVersionID4 := values.NewLauncherVersionID()
+	launcherVersionID5 := values.NewLauncherVersionID()
+
+	launcherUserID1 := values.NewLauncherUserID()
+	launcherUserID2 := values.NewLauncherUserID()
+	launcherUserID3 := values.NewLauncherUserID()
+	launcherUserID5 := values.NewLauncherUserID()
+
+	launcherSessionID1 := values.NewLauncherSessionID()
+	launcherSessionID2 := values.NewLauncherSessionID()
+	launcherSessionID3 := values.NewLauncherSessionID()
+	launcherSessionID5 := values.NewLauncherSessionID()
+
+	questionnaireURL, err := url.Parse("https://example.com/questionnaire")
+	if err != nil {
+		t.Errorf("failed to create url: %v", err)
+	}
+
+	now := time.Now()
+
+	testCases := []test{
+		{
+			description: "通常の状態なので問題なし",
+			accessToken: accessToken1,
+			dbLauncherVersion: LauncherVersionTable{
+				ID:        uuid.UUID(launcherVersionID1),
+				Name:      "TestGetVersion,User,Session1",
+				CreatedAt: now,
+				LauncherUsers: []LauncherUserTable{
+					{
+						ID:         uuid.UUID(launcherUserID1),
+						ProductKey: string(productKey1),
+						CreatedAt:  now,
+						LauncherSessions: []LauncherSessionTable{
+							{
+								ID:          uuid.UUID(launcherSessionID1),
+								AccessToken: string(accessToken1),
+								ExpiresAt:   now.Add(time.Hour),
+								CreatedAt:   now,
+							},
+						},
+					},
+				},
+			},
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("TestGetVersion,User,Session1"),
+				now,
+			),
+			launcherUser: domain.NewLauncherUser(
+				launcherUserID1,
+				productKey1,
+			),
+			launcherSession: domain.NewLauncherSession(
+				launcherSessionID1,
+				accessToken1,
+				now.Add(time.Hour),
+			),
+		},
+		{
+			description: "questionnaireURLが存在しても問題なし",
+			accessToken: accessToken5,
+			dbLauncherVersion: LauncherVersionTable{
+				ID:   uuid.UUID(launcherVersionID5),
+				Name: "TestGetVersion,User,Session5",
+				QuestionnaireURL: sql.NullString{
+					String: "https://example.com/questionnaire",
+					Valid:  true,
+				},
+				CreatedAt: now,
+				LauncherUsers: []LauncherUserTable{
+					{
+						ID:         uuid.UUID(launcherUserID5),
+						ProductKey: string(productKey5),
+						CreatedAt:  now,
+						LauncherSessions: []LauncherSessionTable{
+							{
+								ID:          uuid.UUID(launcherSessionID5),
+								AccessToken: string(accessToken5),
+								ExpiresAt:   now.Add(time.Hour),
+								CreatedAt:   now,
+							},
+						},
+					},
+				},
+			},
+			launcherVersion: domain.NewLauncherVersionWithQuestionnaire(
+				launcherVersionID5,
+				values.NewLauncherVersionName("TestGetVersion,User,Session5"),
+				values.NewLauncherVersionQuestionnaireURL(questionnaireURL),
+				now,
+			),
+			launcherUser: domain.NewLauncherUser(
+				launcherUserID5,
+				productKey5,
+			),
+			launcherSession: domain.NewLauncherSession(
+				launcherSessionID5,
+				accessToken5,
+				now.Add(time.Hour),
+			),
+		},
+		{
+			description: "バージョンが削除されているのでエラー",
+			accessToken: accessToken2,
+			dbLauncherVersion: LauncherVersionTable{
+				ID:        uuid.UUID(launcherVersionID2),
+				Name:      "TestGetVersion,User,Session2",
+				CreatedAt: now,
+				DeletedAt: gorm.DeletedAt{
+					Time:  now,
+					Valid: true,
+				},
+				LauncherUsers: []LauncherUserTable{
+					{
+						ID:         uuid.UUID(launcherUserID2),
+						ProductKey: string(productKey2),
+						CreatedAt:  now,
+						LauncherSessions: []LauncherSessionTable{
+							{
+								ID:          uuid.UUID(launcherSessionID2),
+								AccessToken: string(accessToken2),
+								ExpiresAt:   now.Add(time.Hour),
+								CreatedAt:   now,
+							},
+						},
+					},
+				},
+			},
+			isErr: true,
+			err:   repository.ErrRecordNotFound,
+		},
+		{
+			description: "ユーザーが削除されているのでエラー",
+			accessToken: accessToken3,
+			dbLauncherVersion: LauncherVersionTable{
+				ID:        uuid.UUID(launcherVersionID3),
+				Name:      "TestGetVersion,User,Session3",
+				CreatedAt: now,
+				LauncherUsers: []LauncherUserTable{
+					{
+						ID:         uuid.UUID(launcherUserID3),
+						ProductKey: string(productKey3),
+						CreatedAt:  now,
+						DeletedAt: gorm.DeletedAt{
+							Time:  now,
+							Valid: true,
+						},
+						LauncherSessions: []LauncherSessionTable{
+							{
+								ID:          uuid.UUID(launcherSessionID3),
+								AccessToken: string(accessToken3),
+								ExpiresAt:   now.Add(time.Hour),
+								CreatedAt:   now,
+							},
+						},
+					},
+				},
+			},
+			isErr: true,
+			err:   repository.ErrRecordNotFound,
+		},
+		{
+			description: "アクセストークンが存在しないのでエラー",
+			accessToken: accessToken4,
+			dbLauncherVersion: LauncherVersionTable{
+				ID:        uuid.UUID(launcherVersionID4),
+				Name:      "TestGetVersion,User,Session4",
+				CreatedAt: now,
+			},
+			isErr: true,
+			err:   repository.ErrRecordNotFound,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			err := db.Create(&testCase.dbLauncherVersion).Error
+			if err != nil {
+				t.Errorf("failed to create test data: %v", err)
+			}
+
+			if testCase.dbLauncherVersion.DeletedAt.Valid {
+				err = db.Delete(&testCase.dbLauncherVersion).Error
+				if err != nil {
+					t.Errorf("failed to delete test data: %v", err)
+				}
+			}
+			for _, launcherUser := range testCase.dbLauncherVersion.LauncherUsers {
+				if launcherUser.DeletedAt.Valid {
+					err = db.Delete(&launcherUser).Error
+					if err != nil {
+						t.Errorf("failed to delete test data: %v", err)
+					}
+				}
+				for _, launcherSession := range launcherUser.LauncherSessions {
+					if launcherSession.DeletedAt.Valid {
+						err = db.Delete(&launcherSession).Error
+						if err != nil {
+							t.Errorf("failed to delete test data: %v", err)
+						}
+					}
+				}
+			}
+
+			launcherVersion, launcherUser, launcherSession, err := launcherVersionRepository.GetLauncherVersionAndUserAndSessionByAccessToken(ctx, testCase.accessToken)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, testCase.launcherVersion.GetID(), launcherVersion.GetID())
+			assert.Equal(t, testCase.launcherVersion.GetName(), launcherVersion.GetName())
+
+			expectQuestionnaireURL, _ := testCase.launcherVersion.GetQuestionnaireURL()
+			actualQuestionnaireURL, _ := launcherVersion.GetQuestionnaireURL()
+			assert.Equal(t, expectQuestionnaireURL, actualQuestionnaireURL)
+
+			assert.WithinDuration(t, testCase.launcherVersion.GetCreatedAt(), launcherVersion.GetCreatedAt(), time.Second)
+
+			assert.Equal(t, testCase.launcherUser.GetID(), launcherUser.GetID())
+			assert.Equal(t, testCase.launcherUser.GetProductKey(), launcherUser.GetProductKey())
+
+			assert.Equal(t, testCase.launcherSession.GetID(), launcherSession.GetID())
+			assert.Equal(t, testCase.launcherSession.GetAccessToken(), launcherSession.GetAccessToken())
+			assert.WithinDuration(t, testCase.launcherSession.GetExpiresAt(), launcherSession.GetExpiresAt(), time.Second)
+		})
+	}
+}
