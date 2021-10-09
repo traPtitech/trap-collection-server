9b8abee68759a995ddf9bc78b10697edfce485c9
mazrean <shunsuke.w04@gmail.com>
2021-10-10 02:41:04 +0900

    Callback実装

--- src/handler/v1/oauth2.go
+++ src/handler/v1/oauth2.go
@@ -1,6 +1,13 @@
 package v1
 
 import (
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
 	"github.com/traPtitech/trap-collection-server/src/service"
 )
 
@@ -14,4 +21,48 @@ func NewOAuth2(session *Session, oidcService service.OIDC) *OAuth2 {
 		session:     session,
 		oidcService: oidcService,
 	}
+}
+
+func (o *OAuth2) Callback(strCode string, c echo.Context) error {
+	code := values.NewOIDCAuthorizationCode(strCode)
+
+	session, err := o.session.getSession(c)
+	if err != nil {
+		log.Printf("error: failed to get session: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get session")
+	}
+
+	strCodeVerifier, err := o.session.getCodeVerifier(session)
+	if errors.Is(err, ErrNoValue) {
+		return echo.NewHTTPError(http.StatusBadRequest, "no code verifier")
+	}
+	if err != nil {
+		log.Printf("error: failed to get code verifier: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get code verifier")
+	}
+	codeVerifier := values.NewOIDCCodeVerifierFromString(strCodeVerifier)
+
+	authState := domain.NewOIDCAuthState(
+		values.OIDCCodeChallengeMethodSha256,
+		codeVerifier,
+	)
+
+	authSession, err := o.oidcService.Callback(c.Request().Context(), authState, code)
+	if errors.Is(err, service.ErrInvalidAuthStateOrCode) {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid auth state or code")
+	}
+	if err != nil {
+		log.Printf("error: failed to callback: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to callback")
+	}
+
+	o.session.setAuthSession(session, authSession)
+
+	err = o.session.save(c, session)
+	if err != nil {
+		log.Printf("error: failed to save session: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to save session")
+	}
+
+	return nil
 }
