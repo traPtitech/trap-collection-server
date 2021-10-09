c42438e63a189fad02a3f9be5caed21284361710
mazrean <shunsuke.w04@gmail.com>
2021-10-09 23:17:36 +0900

    Use追加

--- src/handler/v1/session.go
+++ src/handler/v1/session.go
@@ -2,6 +2,8 @@ package v1
 
 import (
 	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
 	"github.com/traPtitech/trap-collection-server/pkg/common"
 )
 
@@ -19,4 +21,8 @@ func NewSession(key common.SessionKey, secret common.SessionSecret) *Session {
 		secret: string(secret),
 		store:  store,
 	}
+}
+
+func (s *Session) Use(e *echo.Echo) {
+	e.Use(session.Middleware(s.store))
 }
