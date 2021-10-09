3ffeb197e654820c7418890cf9e5826a9bbc6244
mazrean <shunsuke.w04@gmail.com>
2021-10-09 23:21:34 +0900

    getSession追加

--- src/handler/v1/session.go
+++ src/handler/v1/session.go
@@ -1,6 +1,8 @@
 package v1
 
 import (
+	"fmt"
+
 	"github.com/gorilla/sessions"
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
@@ -25,4 +27,13 @@ func NewSession(key common.SessionKey, secret common.SessionSecret) *Session {
 
 func (s *Session) Use(e *echo.Echo) {
 	e.Use(session.Middleware(s.store))
+}
+
+func (s *Session) getSession(c echo.Context) (*sessions.Session, error) {
+	session, err := s.store.Get(c.Request(), s.key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get session: %w", err)
+	}
+
+	return session, nil
 }
