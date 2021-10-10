dfbd6a2afa4abc7970eda144444c3089987bfbb7
mazrean <shunsuke.w04@gmail.com>
2021-10-11 01:35:20 +0900

    cacheのinterface修正

--- src/cache/user.go
+++ src/cache/user.go
@@ -6,10 +6,13 @@ import (
 	"context"
 
 	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
 	"github.com/traPtitech/trap-collection-server/src/service"
 )
 
 type User interface {
-	GetMe(ctx context.Context, session *domain.OIDCSession) (*service.UserInfo, error)
+	GetMe(ctx context.Context, accessToken values.OIDCAccessToken) (*service.UserInfo, error)
+	SetMe(ctx context.Context, session *domain.OIDCSession, user *service.UserInfo) error
 	GetAllActiveUsers(ctx context.Context) ([]*service.UserInfo, error)
+	SetAllActiveUsers(ctx context.Context, users []*service.UserInfo) error
 }
