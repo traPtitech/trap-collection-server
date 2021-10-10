27799d527d9535f670a305a247eb54b027c94143
mazrean <shunsuke.w04@gmail.com>
2021-10-10 20:59:28 +0900

    TestCheckTrapMemberAuth追加

--- src/handler/v1/middleware_test.go
+++ src/handler/v1/middleware_test.go
@@ -102,6 +102,129 @@ func TestLauncherAuthMiddleware(t *testing.T) {
 	}
 }
 
+func TestCheckTrapMemberAuth(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockLauncherAuthService := mock.NewMockLauncherAuth(ctrl)
+	mockOIDCService := mock.NewMockOIDC(ctrl)
+	session := NewSession("key", "secret")
+
+	middleware := NewMiddleware(session, mockLauncherAuthService, mockOIDCService)
+
+	type test struct {
+		description      string
+		sessionExist     bool
+		authSessionExist bool
+		accessToken      string
+		expiresAt        time.Time
+		executeTraPAuth  bool
+		TraPAuthErr      error
+		isOk             bool
+		message          string
+		isErr            bool
+		err              error
+	}
+
+	testCases := []test{
+		{
+			description:      "特に問題ないのでtrue",
+			sessionExist:     true,
+			authSessionExist: true,
+			accessToken:      "access token",
+			expiresAt:        time.Now(),
+			executeTraPAuth:  true,
+			isOk:             true,
+		},
+		{
+			description:  "セッションがないのでfalse",
+			sessionExist: false,
+			isOk:         false,
+			message:      "no access token",
+		},
+		{
+			description:      "authSessionがないのでfalse",
+			sessionExist:     true,
+			authSessionExist: false,
+			isOk:             false,
+			message:          "no access token",
+		},
+		{
+			description:      "TraPAuthがErrOIDCSessionExpiredなのでfalse",
+			sessionExist:     true,
+			authSessionExist: true,
+			accessToken:      "access token",
+			expiresAt:        time.Now(),
+			executeTraPAuth:  true,
+			TraPAuthErr:      service.ErrOIDCSessionExpired,
+			isOk:             false,
+			message:          "access token is expired",
+		},
+		{
+			description:      "TraPAuthがエラー(ErrOIDCSessionExpired以外)なのでエラー",
+			sessionExist:     true,
+			authSessionExist: true,
+			accessToken:      "access token",
+			expiresAt:        time.Now(),
+			executeTraPAuth:  true,
+			TraPAuthErr:      errors.New("error"),
+			isErr:            true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			e := echo.New()
+			req := httptest.NewRequest(http.MethodGet, "/", nil)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+
+			if testCase.sessionExist {
+				sess, err := session.store.New(req, session.key)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if testCase.authSessionExist {
+					sess.Values[accessTokenSessionKey] = testCase.accessToken
+					sess.Values[expiresAtSessionKey] = testCase.expiresAt
+				}
+
+				err = sess.Save(req, rec)
+				if err != nil {
+					t.Fatalf("failed to save session: %v", err)
+				}
+
+				setCookieHeader(c)
+			}
+
+			if testCase.executeTraPAuth {
+				mockOIDCService.
+					EXPECT().
+					TraPAuth(gomock.Any(), gomock.Any()).
+					Return(testCase.TraPAuthErr)
+			}
+
+			ok, message, err := middleware.checkTrapMemberAuth(c)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.Equal(t, testCase.isOk, ok)
+			assert.Equal(t, testCase.message, message)
+		})
+	}
+}
+
 func TestCheckLauncherAuth(t *testing.T) {
 	t.Parallel()
 
