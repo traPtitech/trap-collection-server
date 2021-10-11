a6f1651bbb536656c722a2267e5b7b61d29e0c5e
mazrean <shunsuke.w04@gmail.com>
2021-10-12 00:26:12 +0900

    GetUsers実装

--- src/handler/v1/user.go
+++ src/handler/v1/user.go
@@ -47,3 +47,34 @@ func (u *User) GetMe(c echo.Context) (*openapi.User, error) {
 		Name: string(userInfo.GetName()),
 	}, nil
 }
+
+func (u *User) GetUsers(c echo.Context) ([]*openapi.User, error) {
+	session, err := getSession(c)
+	if err != nil {
+		log.Printf("error: failed to get session: %v\n", err)
+		return nil, echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	authSession, err := u.session.getAuthSession(session)
+	if err != nil {
+		// middlewareでログイン済みなことは確認しているので、ここではエラーになりえないはず
+		log.Printf("error: failed to get auth session: %v\n", err)
+		return nil, echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	userInfos, err := u.userService.GetAllActiveUser(c.Request().Context(), authSession)
+	if err != nil {
+		log.Printf("error: failed to get user info: %v\n", err)
+		return nil, echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	users := make([]*openapi.User, 0, len(userInfos))
+	for _, userInfo := range userInfos {
+		users = append(users, &openapi.User{
+			Id:   uuid.UUID(userInfo.GetID()).String(),
+			Name: string(userInfo.GetName()),
+		})
+	}
+
+	return users, nil
+}
