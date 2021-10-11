a8bbbaa6b4d1ed91c1cae7905d9d559f03ebfdff
mazrean <shunsuke.w04@gmail.com>
2021-10-11 21:57:26 +0900

    GetMe実装

--- src/auth/traQ/user.go
+++ src/auth/traQ/user.go
@@ -1,10 +1,18 @@
 package traq
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/url"
 
+	"github.com/google/uuid"
 	"github.com/traPtitech/trap-collection-server/pkg/common"
+	"github.com/traPtitech/trap-collection-server/src/auth"
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/service"
 )
 
 type User struct {
@@ -17,4 +25,61 @@ func NewUser(client *http.Client, baseURL common.TraQBaseURL) *User {
 		client:  client,
 		baseURL: (*url.URL)(baseURL),
 	}
+}
+
+type getUsersMeResponse struct {
+	ID    uuid.UUID `json:"id"`
+	Name  string    `json:"name"`
+	State int       `json:"state"`
+}
+
+func (u *User) GetMe(ctx context.Context, session *domain.OIDCSession) (*service.UserInfo, error) {
+	path := *u.baseURL
+	path.Path += "/users/me"
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, path.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", session.GetAccessToken()))
+
+	res, err := u.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	switch res.StatusCode {
+	case http.StatusOK:
+	case http.StatusUnauthorized:
+		return nil, auth.ErrInvalidSession
+	case http.StatusInternalServerError:
+		return nil, auth.ErrIdpBroken
+	default:
+		return nil, fmt.Errorf("unexpected status code: %d", res.StatusCode)
+	}
+
+	var response getUsersMeResponse
+	err = json.NewDecoder(res.Body).Decode(&response)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	var status values.TraPMemberStatus
+	switch response.State {
+	case 0:
+		status = values.TrapMemberStatusDeactivated
+	case 1:
+		status = values.TrapMemberStatusActive
+	case 2:
+		status = values.TrapMemberStatusSuspended
+	default:
+		return nil, fmt.Errorf("unexpected state: %d", response.State)
+	}
+
+	return service.NewUserInfo(
+		values.NewTrapMemberID(response.ID),
+		values.NewTrapMemberName(response.Name),
+		status,
+	), nil
 }
