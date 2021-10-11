f5b3ba283c8fbcfaa9a27cf45d17014158eb4772
mazrean <shunsuke.w04@gmail.com>
2021-10-12 00:20:40 +0900

    GetMe実装

--- src/handler/v1/user.go
+++ src/handler/v1/user.go
@@ -1,6 +1,14 @@
 package v1
 
-import "github.com/traPtitech/trap-collection-server/src/service"
+import (
+	"log"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/traPtitech/trap-collection-server/openapi"
+	"github.com/traPtitech/trap-collection-server/src/service"
+)
 
 type User struct {
 	session     *Session
@@ -12,4 +20,30 @@ func NewUser(session *Session, userService service.User) *User {
 		session:     session,
 		userService: userService,
 	}
+}
+
+func (u *User) GetMe(c echo.Context) (*openapi.User, error) {
+	session, err := getSession(c)
+	if err != nil {
+		log.Printf("error: failed to get session: %v\n", err)
+		return nil, echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	authSession, err := u.session.getAuthSession(session)
+	if err != nil {
+		// middlewareでログイン済みなことは確認しているので、ここではエラーになりえないはず
+		log.Printf("error: failed to get auth session: %v\n", err)
+		return nil, echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	userInfo, err := u.userService.GetMe(c.Request().Context(), authSession)
+	if err != nil {
+		log.Printf("error: failed to get user info: %v\n", err)
+		return nil, echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	return &openapi.User{
+		Id:   uuid.UUID(userInfo.GetID()).String(),
+		Name: string(userInfo.GetName()),
+	}, nil
 }
