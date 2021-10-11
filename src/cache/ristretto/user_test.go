3a9e3614eff804b32845609bac758c6d40807cde
mazrean <shunsuke.w04@gmail.com>
2021-10-11 20:23:43 +0900

    TestSetMe追加

--- src/cache/ristretto/user_test.go
+++ src/cache/ristretto/user_test.go
@@ -5,10 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/traPtitech/trap-collection-server/src/cache"
+	"github.com/traPtitech/trap-collection-server/src/domain"
 	"github.com/traPtitech/trap-collection-server/src/domain/values"
 	"github.com/traPtitech/trap-collection-server/src/service"
 )
@@ -90,6 +92,108 @@ func TestGetMe(t *testing.T) {
 			}
 
 			assert.Equal(t, testCase.userInfo, user)
+		})
+	}
+}
+
+func TestSetMe(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	userCache, err := NewUser()
+	if err != nil {
+		t.Fatalf("failed to create user cache: %v", err)
+	}
+
+	type test struct {
+		description string
+		keyExist    bool
+		beforeValue *service.UserInfo
+		session     *domain.OIDCSession
+		userInfo    *service.UserInfo
+		ttl         time.Duration
+		isErr       bool
+		err         error
+	}
+
+	now := time.Now()
+
+	testCases := []test{
+		{
+			description: "特に問題ないのでエラーなし",
+			session: domain.NewOIDCSession(
+				values.NewOIDCAccessToken("access token1"),
+				now.Add(2*time.Second),
+			),
+			userInfo: service.NewUserInfo(
+				values.NewTrapMemberID(uuid.New()),
+				values.NewTrapMemberName("mazrean"),
+				values.TrapMemberStatusActive,
+			),
+			ttl: 2 * time.Second,
+		},
+		{
+			description: "元からキーがあっても上書きする",
+			keyExist:    true,
+			beforeValue: service.NewUserInfo(
+				values.NewTrapMemberID(uuid.New()),
+				values.NewTrapMemberName("mazrean"),
+				values.TrapMemberStatusActive,
+			),
+			session: domain.NewOIDCSession(
+				values.NewOIDCAccessToken("access token2"),
+				now.Add(2*time.Second),
+			),
+			userInfo: service.NewUserInfo(
+				values.NewTrapMemberID(uuid.New()),
+				values.NewTrapMemberName("mazrean"),
+				values.TrapMemberStatusActive,
+			),
+			ttl: 2 * time.Second,
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+		t.Run(testCase.description, func(t *testing.T) {
+			t.Parallel()
+
+			if testCase.keyExist {
+				ok := userCache.meCache.Set(string(testCase.session.GetAccessToken()), testCase.beforeValue, 8)
+				assert.True(t, ok)
+
+				userCache.meCache.Wait()
+			}
+
+			err := userCache.SetMe(ctx, testCase.session, testCase.userInfo)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			// キャッシュが設定されるまで待機
+			userCache.meCache.Wait()
+
+			// OIDCSessionの期限前なのでキャッシュされている
+			value, ok := userCache.meCache.Get(string(testCase.session.GetAccessToken()))
+			assert.True(t, ok)
+			assert.Equal(t, testCase.userInfo, value)
+
+			<-time.NewTimer(testCase.ttl).C
+
+			// OIDCSessionの期限が切れたらキャッシュは削除される
+			_, ok = userCache.meCache.Get(string(testCase.session.GetAccessToken()))
+			assert.False(t, ok)
 		})
 	}
 }
