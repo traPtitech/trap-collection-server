194cd054157766731dd90328aab2633d46beed56
mazrean <shunsuke.w04@gmail.com>
2021-10-28 16:21:21 +0900

    game version周りの値オブジェクト作成

---
+++ src/domain/values/game_version.go
@@ -0,0 +1,43 @@
+package values
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+	"golang.org/x/mod/semver"
+)
+
+type (
+	GameVersionID uuid.UUID
+	// セマンティックバージョニング
+	GameVersionName        string
+	GameVersionDescription string
+)
+
+func NewGameVersionID() GameVersionID {
+	return GameVersionID(uuid.New())
+}
+
+func NewGameVersionIDFromUUID(id uuid.UUID) GameVersionID {
+	return GameVersionID(id)
+}
+
+func NewGameVersionName(name string) GameVersionName {
+	return GameVersionName(name)
+}
+
+var (
+	ErrGameVersionNameInvalidSemanticVersion = errors.New("Invalid semantic version")
+)
+
+func (gvn GameVersionName) Validate() error {
+	if !semver.IsValid(string(gvn)) {
+		return ErrGameVersionNameInvalidSemanticVersion
+	}
+
+	return nil
+}
+
+func NewGameVersionDescription(description string) GameVersionDescription {
+	return GameVersionDescription(description)
+}
