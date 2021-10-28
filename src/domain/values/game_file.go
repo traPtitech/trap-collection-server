307d50b8509aa482e426bcfff52d80e591ebd145
mazrean <shunsuke.w04@gmail.com>
2021-10-28 17:27:59 +0900

    game file周りの値オブジェクト作成

---
+++ src/domain/values/game_file.go
@@ -0,0 +1,63 @@
+package values
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io"
+
+	"github.com/google/uuid"
+)
+
+type (
+	GameFileID   uuid.UUID
+	GameFileType int
+	/*
+		GameFileEntryPoint
+		ファイルのエントリーポイント。
+		ランチャーはFile解凍後にこのパスのファイルを実行する。
+	*/
+	GameFileEntryPoint string
+	/*
+		GameFileHash
+		ファイルのハッシュ値。
+		ランチャーでファイルが壊れていないかの確認に使用する。
+	*/
+	GameFileHash []byte
+)
+
+func NewGameFileID() GameFileID {
+	return GameFileID(uuid.New())
+}
+
+func NewGameFileIDFromUUID(id uuid.UUID) GameVersionID {
+	return GameVersionID(id)
+}
+
+const (
+	GameFileTypeJar GameFileType = iota
+	GameFileTypeWindows
+	GameFileTypeMac
+)
+
+func NewGameFileEntryPoint(entryPoint string) GameFileEntryPoint {
+	return GameFileEntryPoint(entryPoint)
+}
+
+func NewGameFileHash(r io.Reader) (GameFileHash, error) {
+	h := md5.New()
+	_, err := io.Copy(h, r)
+	if err != nil {
+		return nil, fmt.Errorf("failed to calculate hash: %w", err)
+	}
+
+	return GameFileHash(h.Sum(nil)), nil
+}
+
+func NewGameFileHashFromBytes(hash []byte) GameFileHash {
+	return GameFileHash(hash)
+}
+
+func (h GameFileHash) String() string {
+	return hex.EncodeToString(h)
+}
