04ebb2bd72c9fb3759c991ed421c04842ca08c02
mazrean <shunsuke.w04@gmail.com>
2021-10-28 20:51:10 +0900

    GameAuthService抽象化

---
+++ src/service/game_auth.go
@@ -0,0 +1,24 @@
+package service
+
+import (
+	"context"
+
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+)
+
+type GameAuth interface {
+	AddGameCollaborators(ctx context.Context, session *domain.OIDCSession, gameID values.GameID, userIDs []values.TraPMemberID) error
+	UpdateGameManagementRole(ctx context.Context, gameID values.GameID, userID values.TraPMemberID, role values.GameManagementRole) error
+	RemoveGameCollaborator(ctx context.Context, gameID values.GameID, userID values.TraPMemberID) error
+	GetGameManagers(ctx context.Context, session *domain.OIDCSession, gameID values.GameID) ([]*GameManager, error)
+	UpdateGameAuth(ctx context.Context, session *domain.OIDCSession, gameID values.GameID) error
+	UpdateGameManagementRoleAuth(ctx context.Context, session *domain.OIDCSession, gameID values.GameID) error
+}
+
+type GameManager struct {
+	UserID     values.TraPMemberID
+	UserName   values.TraPMemberName
+	UserStatus values.TraPMemberStatus
+	Role       values.GameManagementRole
+}
