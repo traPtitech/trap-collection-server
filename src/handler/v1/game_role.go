d5e634190959d67a2bd7dbae1f963eb2854618f8
mazrean <shunsuke.w04@gmail.com>
2021-11-02 02:54:02 +0900

    GetMaintainer実装

--- src/handler/v1/game_role.go
+++ src/handler/v1/game_role.go
@@ -71,3 +71,58 @@ func (gr *GameRole) PostMaintainer(strGameID string, maintainers *openapi.Mainta
 
 	return nil
 }
+
+func (gr *GameRole) GetMaintainer(strGameID string, c echo.Context) ([]*openapi.Maintainer, error) {
+	session, err := getSession(c)
+	if err != nil {
+		log.Printf("error: failed to get session: %v\n", err)
+		return nil, echo.NewHTTPError(http.StatusInternalServerError, "failed to get session")
+	}
+
+	authSession, err := gr.session.getAuthSession(session)
+	if errors.Is(err, ErrNoValue) {
+		return nil, echo.NewHTTPError(http.StatusBadRequest, "no auth session")
+	}
+	if err != nil {
+		log.Printf("error: failed to get auth session: %v\n", err)
+		return nil, echo.NewHTTPError(http.StatusInternalServerError, "failed to get auth session")
+	}
+
+	uuidGameID, err := uuid.Parse(strGameID)
+	if err != nil {
+		return nil, echo.NewHTTPError(http.StatusBadRequest, "invalid game id")
+	}
+
+	gameID := values.NewGameIDFromUUID(uuidGameID)
+
+	gameManagers, err := gr.gameAuthService.GetGameManagers(c.Request().Context(), authSession, gameID)
+	if errors.Is(err, service.ErrInvalidGameID) {
+		return nil, echo.NewHTTPError(http.StatusBadRequest, "invalid game id")
+	}
+	if err != nil {
+		log.Printf("error: failed to get game managers: %v\n", err)
+		return nil, echo.NewHTTPError(http.StatusInternalServerError, "failed to get game managers")
+	}
+
+	maintainers := make([]*openapi.Maintainer, 0, len(gameManagers))
+	for _, gameManager := range gameManagers {
+		var role int32
+		switch gameManager.Role {
+		case values.GameManagementRoleAdministrator:
+			role = 1
+		case values.GameManagementRoleCollaborator:
+			role = 0
+		default:
+			log.Printf("error: invalid game manager role: %d\n", gameManager.Role)
+			return nil, echo.NewHTTPError(http.StatusInternalServerError, "invalid game manager role")
+		}
+
+		maintainers = append(maintainers, &openapi.Maintainer{
+			Id:   uuid.UUID(gameManager.UserID).String(),
+			Name: string(gameManager.UserName),
+			Role: role,
+		})
+	}
+
+	return maintainers, nil
+}
