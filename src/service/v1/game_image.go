70d5900fbe29a2eb591d0c4181a4491af0dde9a1
mazrean <shunsuke.w04@gmail.com>
2021-11-03 03:52:21 +0900

    SaveGameImage実装

--- src/service/v1/game_image.go
+++ src/service/v1/game_image.go
@@ -1,7 +1,18 @@
 package v1
 
 import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/h2non/filetype"
+	"github.com/h2non/filetype/matchers"
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
 	"github.com/traPtitech/trap-collection-server/src/repository"
+	"github.com/traPtitech/trap-collection-server/src/service"
 	"github.com/traPtitech/trap-collection-server/src/storage"
 )
 
@@ -24,4 +35,57 @@ func NewGameImage(
 		gameImageRepository: gameImageRepository,
 		gameImageStorage:    gameImageStorage,
 	}
+}
+
+func (gi *GameImage) SaveGameImage(ctx context.Context, reader io.Reader, gameID values.GameID) error {
+	err := gi.db.Transaction(ctx, nil, func(ctx context.Context) error {
+		_, err := gi.gameRepository.GetGame(ctx, gameID, repository.LockTypeRecord)
+		if errors.Is(err, repository.ErrRecordNotFound) {
+			return service.ErrInvalidGameID
+		}
+		if err != nil {
+			return fmt.Errorf("failed to get game: %w", err)
+		}
+
+		buf := bytes.NewBuffer(nil)
+		r := io.TeeReader(reader, buf)
+		fType, err := filetype.MatchReader(r)
+		if err != nil {
+			return fmt.Errorf("failed to get file type: %w", err)
+		}
+
+		var imageType values.GameImageType
+		switch fType.Extension {
+		case matchers.TypeJpeg.Extension:
+			imageType = values.GameImageTypeJpeg
+		case matchers.TypePng.Extension:
+			imageType = values.GameImageTypePng
+		case matchers.TypeGif.Extension:
+			imageType = values.GameImageTypeGif
+		default:
+			return service.ErrInvalidFormat
+		}
+
+		image := domain.NewGameImage(
+			values.NewGameImageID(),
+			imageType,
+		)
+
+		err = gi.gameImageRepository.SaveGameImage(ctx, gameID, image)
+		if err != nil {
+			return fmt.Errorf("failed to save game image: %w", err)
+		}
+
+		err = gi.gameImageStorage.SaveGameImage(ctx, buf, image)
+		if err != nil {
+			return fmt.Errorf("failed to save game image file: %w", err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("failed in transaction: %w", err)
+	}
+
+	return nil
 }
