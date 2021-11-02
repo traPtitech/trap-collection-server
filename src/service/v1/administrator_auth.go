96f1f047be3e8ae131c847063ed11672a07733c1
mazrean <shunsuke.w04@gmail.com>
2021-11-02 20:26:43 +0900

    AdministratorAuth実装

--- src/service/v1/administrator_auth.go
+++ src/service/v1/administrator_auth.go
@@ -1,8 +1,13 @@
 package v1
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/traPtitech/trap-collection-server/pkg/common"
+	"github.com/traPtitech/trap-collection-server/src/domain"
 	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/service"
 )
 
 type AdministratorAuth struct {
@@ -15,4 +20,19 @@ func NewAdministratorAuth(administrators common.Administrators, userUtils *UserU
 		administrators: administrators,
 		userUtils:      userUtils,
 	}
+}
+
+func (aa *AdministratorAuth) AdministratorAuth(ctx context.Context, session *domain.OIDCSession) error {
+	user, err := aa.userUtils.getMe(ctx, session)
+	if err != nil {
+		return fmt.Errorf("failed to get user: %w", err)
+	}
+
+	for _, administrator := range aa.administrators {
+		if user.GetName() == administrator {
+			return nil
+		}
+	}
+
+	return service.ErrForbidden
 }
