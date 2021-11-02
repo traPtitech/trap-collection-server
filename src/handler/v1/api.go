c23c7ef41850d8b5a124b224b9751b944545dd0f
mazrean <shunsuke.w04@gmail.com>
2021-11-02 14:02:50 +0900

    APIにGameRole追加

--- src/handler/v1/api.go
+++ src/handler/v1/api.go
@@ -3,17 +3,26 @@ package v1
 type API struct {
 	*Middleware
 	*User
+	*GameRole
 	*LauncherAuth
 	*OAuth2
 	*Session
 }
 
-func NewAPI(user *User, launcherAuth *LauncherAuth, oAuth2 *OAuth2, middleware *Middleware, session *Session) *API {
+func NewAPI(
+	middleware *Middleware,
+	user *User,
+	gameRole *GameRole,
+	launcherAuth *LauncherAuth,
+	oAuth2 *OAuth2,
+	session *Session,
+) *API {
 	return &API{
+		Middleware:   middleware,
 		User:         user,
+		GameRole:     gameRole,
 		LauncherAuth: launcherAuth,
 		OAuth2:       oAuth2,
-		Middleware:   middleware,
 		Session:      session,
 	}
 }
