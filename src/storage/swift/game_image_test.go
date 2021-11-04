132b23c5c71aac86bbe4ff5acaa2eaaebf70f13d
mazrean <shunsuke.w04@gmail.com>
2021-11-05 01:25:12 +0900

    TestGetGameImage追加

--- src/storage/swift/game_image_test.go
+++ src/storage/swift/game_image_test.go
@@ -160,6 +160,133 @@ func TestSaveGameImage(t *testing.T) {
 	}
 }
 
+func TestGetGameImage(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	client, err := newTestClient(
+		ctx,
+		common.SwiftContainer("get_game_image"),
+		common.FilePath("get_game_image"),
+	)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+	defer func() {
+		err := os.RemoveAll("get_game_image")
+		if err != nil {
+			t.Fatalf("failed to remove directory: %v", err)
+		}
+	}()
+
+	gameImageStorage := NewGameImage(client)
+
+	type test struct {
+		description string
+		image       *domain.GameImage
+		isFileExist bool
+		isErr       bool
+		err         error
+	}
+
+	testCases := []test{
+		{
+			description: "特に問題ないのでエラーなし",
+			image: domain.NewGameImage(
+				values.NewGameImageID(),
+				values.GameImageTypeJpeg,
+			),
+			isFileExist: true,
+		},
+		{
+			description: "pngでもエラーなし",
+			image: domain.NewGameImage(
+				values.NewGameImageID(),
+				values.GameImageTypePng,
+			),
+			isFileExist: true,
+		},
+		{
+			description: "gifでもエラーなし",
+			image: domain.NewGameImage(
+				values.NewGameImageID(),
+				values.GameImageTypeGif,
+			),
+			isFileExist: true,
+		},
+		{
+			description: "ファイルが存在しないのでErrNotFound",
+			image: domain.NewGameImage(
+				values.NewGameImageID(),
+				values.GameImageTypeJpeg,
+			),
+			isErr: true,
+			err:   storage.ErrNotFound,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			var expectBytes []byte
+			img := image.NewRGBA(image.Rect(0, 0, 3000, 3000))
+			imgBuf := bytes.NewBuffer(nil)
+
+			switch testCase.image.GetType() {
+			case values.GameImageTypeJpeg:
+				err := jpeg.Encode(imgBuf, img, nil)
+				if err != nil {
+					t.Fatalf("failed to encode image: %v\n", err)
+				}
+			case values.GameImageTypePng:
+				err := png.Encode(imgBuf, img)
+				if err != nil {
+					t.Fatalf("failed to encode image: %v\n", err)
+				}
+			case values.GameImageTypeGif:
+				err := gif.Encode(imgBuf, img, nil)
+				if err != nil {
+					t.Fatalf("failed to encode image: %v\n", err)
+				}
+			default:
+				imgBuf = bytes.NewBufferString("hoge")
+			}
+			expectBytes = imgBuf.Bytes()
+
+			if testCase.isFileExist {
+				err := client.saveFile(
+					ctx,
+					fmt.Sprintf("images/%s", uuid.UUID(testCase.image.GetID()).String()),
+					"",
+					"",
+					imgBuf,
+				)
+				if err != nil {
+					t.Fatalf("failed to create file: %v", err)
+				}
+			}
+
+			buf := bytes.NewBuffer(nil)
+			err := gameImageStorage.GetGameImage(ctx, buf, testCase.image)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, expectBytes, buf.Bytes())
+		})
+	}
+}
+
 func TestImageKey(t *testing.T) {
 	t.Parallel()
 
