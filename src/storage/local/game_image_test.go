04a9b14922523e4950611a3db95103bcfd8a758f
mazrean <shunsuke.w04@gmail.com>
2021-11-04 14:04:06 +0900

    TestGetGameImage実装

--- src/storage/local/game_image_test.go
+++ src/storage/local/game_image_test.go
@@ -112,3 +112,97 @@ func TestSaveGameImage(t *testing.T) {
 		})
 	}
 }
+
+func TestGetGameImage(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	rootPath := common.FilePath("./get_game_image_test")
+
+	directoryManager := NewDirectoryManager(rootPath)
+	defer func() {
+		err := os.RemoveAll(string(rootPath))
+		if err != nil {
+			t.Fatalf("failed to remove directory: %v", err)
+		}
+	}()
+
+	gameImage, err := NewGameImage(directoryManager)
+	if err != nil {
+		t.Fatalf("failed to create game image: %v", err)
+	}
+
+	imageRootPath := filepath.Join(string(rootPath), "images")
+
+	type test struct {
+		description string
+		image       *domain.GameImage
+		isFileExist bool
+		fileContent *bytes.Buffer
+		isErr       bool
+		err         error
+	}
+
+	testCases := []test{
+		{
+			description: "ファイルが存在するので読み込める",
+			image: domain.NewGameImage(
+				values.NewGameImageID(),
+				values.GameImageTypeJpeg,
+			),
+			isFileExist: true,
+			fileContent: bytes.NewBufferString("b"),
+		},
+		{
+			description: "ファイルが存在しないので読み込めない",
+			image: domain.NewGameImage(
+				values.NewGameImageID(),
+				values.GameImageTypeJpeg,
+			),
+			isErr: true,
+			err:   storage.ErrNotFound,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			var expectBytes []byte
+			if testCase.isFileExist {
+				expectBytes = testCase.fileContent.Bytes()
+
+				func() {
+					f, err := os.Create(filepath.Join(imageRootPath, uuid.UUID(testCase.image.GetID()).String()))
+					if err != nil {
+						t.Fatalf("failed to write file: %v", err)
+					}
+					defer f.Close()
+
+					_, err = io.Copy(f, testCase.fileContent)
+					if err != nil {
+						t.Fatalf("failed to write file: %v", err)
+					}
+				}()
+			}
+
+			buf := bytes.NewBuffer(nil)
+
+			err := gameImage.GetGameImage(ctx, buf, testCase.image)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, expectBytes, buf.Bytes())
+		})
+	}
+}
