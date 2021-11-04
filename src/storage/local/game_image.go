2a920b5905030965df7ded78c8e86c3470dc7c31
mazrean <shunsuke.w04@gmail.com>
2021-11-04 13:18:27 +0900

    SaveGameImage実装

--- src/storage/local/game_image.go
+++ src/storage/local/game_image.go
@@ -1,7 +1,17 @@
 package local
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path"
+
+	"github.com/google/uuid"
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/storage"
 )
 
 type GameImage struct {
@@ -19,4 +29,29 @@ func NewGameImage(directoryManager *DirectoryManager) (*GameImage, error) {
 		imageRootPath:    imageRootPath,
 		directoryManager: directoryManager,
 	}, nil
+}
+
+func (gi *GameImage) SaveGameImage(ctx context.Context, reader io.Reader, image *domain.GameImage) error {
+	imagePath := path.Join(gi.imageRootPath, uuid.UUID(image.GetID()).String())
+
+	_, err := os.Stat(imagePath)
+	if err == nil {
+		return storage.ErrAlreadyExists
+	}
+	if err != nil && !errors.Is(err, fs.ErrNotExist) {
+		return fmt.Errorf("failed to stat file: %w", err)
+	}
+
+	f, err := os.Create(imagePath)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, reader)
+	if err != nil {
+		return fmt.Errorf("failed to copy: %w", err)
+	}
+
+	return nil
 }
