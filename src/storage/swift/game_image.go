21fdc1937dd9791c4a6bacae4998e4087371502f
mazrean <shunsuke.w04@gmail.com>
2021-11-04 20:16:51 +0900

    imageKey作成

--- src/storage/swift/game_image.go
+++ src/storage/swift/game_image.go
@@ -1,5 +1,12 @@
 package swift
 
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/traPtitech/trap-collection-server/src/domain"
+)
+
 type GameImage struct {
 	client *Client
 }
@@ -8,4 +15,9 @@ func NewGameImage(client *Client) *GameImage {
 	return &GameImage{
 		client: client,
 	}
+}
+
+// imageKey 変更時にはオブジェクトストレージのキーを変更する必要があるので要注意
+func (gi *GameImage) imageKey(image *domain.GameImage) string {
+	return fmt.Sprintf("images/%s", uuid.UUID(image.GetID()).String())
 }
