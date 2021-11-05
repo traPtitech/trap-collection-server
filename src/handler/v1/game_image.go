7acd24943f6cbb0c09e7630f078d90f2b684d7d8
mazrean <shunsuke.w04@gmail.com>
2021-11-05 15:12:49 +0900

    PostImage実装

--- src/handler/v1/game_image.go
+++ src/handler/v1/game_image.go
@@ -1,6 +1,14 @@
 package v1
 
 import (
+	"context"
+	"errors"
+	"mime/multipart"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
 	"github.com/traPtitech/trap-collection-server/src/service"
 )
 
@@ -12,4 +20,29 @@ func NewGameImage(gameImageService service.GameImage) *GameImage {
 	return &GameImage{
 		gameImageService: gameImageService,
 	}
+}
+
+func (gi *GameImage) PostImage(strGameID string, image multipart.File) error {
+	ctx := context.Background()
+
+	uuidGameID, err := uuid.Parse(strGameID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid game id")
+	}
+
+	gameID := values.NewGameIDFromUUID(uuidGameID)
+
+	err = gi.gameImageService.SaveGameImage(ctx, image, gameID)
+	if errors.Is(err, service.ErrInvalidGameID) {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid game id")
+	}
+	if errors.Is(err, service.ErrInvalidFormat) {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid image format")
+	}
+	if err != nil {
+		log.Printf("error: failed to save image: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to save image")
+	}
+
+	return nil
 }
