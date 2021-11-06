1020ac3616e81ec11a8ffe833929f5e9f68464aa
mazrean <shunsuke.w04@gmail.com>
2021-11-06 22:25:00 +0900

    CreateGameVersion実装

--- src/service/v1/game_version.go
+++ src/service/v1/game_version.go
@@ -1,6 +1,16 @@
 package v1
 
-import "github.com/traPtitech/trap-collection-server/src/repository"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/repository"
+	"github.com/traPtitech/trap-collection-server/src/service"
+)
 
 type GameVersion struct {
 	db                    repository.DB
@@ -18,4 +28,36 @@ func NewGameVersion(
 		gameRepository:        gameRepository,
 		gameVersionRepository: gameVersionRepository,
 	}
+}
+
+func (gv *GameVersion) CreateGameVersion(ctx context.Context, gameID values.GameID, name values.GameVersionName, description values.GameVersionDescription) (*domain.GameVersion, error) {
+	var gameVersion *domain.GameVersion
+	err := gv.db.Transaction(ctx, nil, func(ctx context.Context) error {
+		_, err := gv.gameRepository.GetGame(ctx, gameID, repository.LockTypeRecord)
+		if errors.Is(err, repository.ErrRecordNotFound) {
+			return service.ErrInvalidGameID
+		}
+		if err != nil {
+			return fmt.Errorf("failed to get game: %w", err)
+		}
+
+		gameVersion = domain.NewGameVersion(
+			values.NewGameVersionID(),
+			name,
+			description,
+			time.Now(),
+		)
+
+		err = gv.gameVersionRepository.CreateGameVersion(ctx, gameID, gameVersion)
+		if err != nil {
+			return fmt.Errorf("failed to create game version: %w", err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed in transaction: %w", err)
+	}
+
+	return gameVersion, nil
 }
