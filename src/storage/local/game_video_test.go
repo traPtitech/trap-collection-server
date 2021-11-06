ef6a9a225802db6d02135b2c91ee8d344f761bbf
mazrean <shunsuke.w04@gmail.com>
2021-11-06 16:34:11 +0900

    TestGetGameVideo追加

--- src/storage/local/game_video_test.go
+++ src/storage/local/game_video_test.go
@@ -135,3 +135,119 @@ func TestSaveGameVideo(t *testing.T) {
 		})
 	}
 }
+
+func TestGetGameVideo(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	rootPath := common.FilePath("./get_game_video_test")
+
+	directoryManager := NewDirectoryManager(rootPath)
+	defer func() {
+		err := os.RemoveAll(string(rootPath))
+		if err != nil {
+			t.Fatalf("failed to remove directory: %v", err)
+		}
+	}()
+
+	gameVideo, err := NewGameVideo(directoryManager)
+	if err != nil {
+		t.Fatalf("failed to create game image: %v", err)
+	}
+
+	videoRootPath := filepath.Join(string(rootPath), "videos")
+
+	type test struct {
+		description string
+		video       *domain.GameVideo
+		isFileExist bool
+		isErr       bool
+		err         error
+	}
+
+	testCases := []test{
+		{
+			description: "ファイルが存在するので読み込める",
+			video: domain.NewGameVideo(
+				values.NewGameVideoID(),
+				values.GameVideoTypeMp4,
+			),
+			isFileExist: true,
+		},
+		{
+			description: "ファイルが存在しないので読み込めない",
+			video: domain.NewGameVideo(
+				values.NewGameVideoID(),
+				values.GameVideoTypeMp4,
+			),
+			isErr: true,
+			err:   storage.ErrNotFound,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			var expectBytes []byte
+			if testCase.isFileExist {
+				videoBuf := bytes.NewBuffer(nil)
+
+				switch testCase.video.GetType() {
+				case values.GameVideoTypeMp4:
+					err := func() error {
+						f, err := os.Open("../../../testdata/1.mp4")
+						if err != nil {
+							return fmt.Errorf("failed to open file: %w", err)
+						}
+						defer f.Close()
+
+						_, err = io.Copy(videoBuf, f)
+						if err != nil {
+							return fmt.Errorf("failed to copy file: %w", err)
+						}
+
+						return nil
+					}()
+					if err != nil {
+						t.Fatalf("failed to encode image: %s", err)
+					}
+				default:
+					t.Fatalf("invalid video type: %v\n", testCase.video.GetType())
+				}
+				expectBytes = videoBuf.Bytes()
+
+				func() {
+					f, err := os.Create(filepath.Join(videoRootPath, uuid.UUID(testCase.video.GetID()).String()))
+					if err != nil {
+						t.Fatalf("failed to write file: %v", err)
+					}
+					defer f.Close()
+
+					_, err = io.Copy(f, videoBuf)
+					if err != nil {
+						t.Fatalf("failed to write file: %v", err)
+					}
+				}()
+			}
+
+			buf := bytes.NewBuffer(nil)
+
+			err := gameVideo.GetGameVideo(ctx, buf, testCase.video)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, expectBytes, buf.Bytes())
+		})
+	}
+}
