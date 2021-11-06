1268c774ec15c184ef8f81e2875db67f5b100838
mazrean <shunsuke.w04@gmail.com>
2021-11-06 20:37:03 +0900

    GetVideo実装

--- src/handler/v1/game_video.go
+++ src/handler/v1/game_video.go
@@ -1,8 +1,10 @@
 package v1
 
 import (
+	"bytes"
 	"context"
 	"errors"
+	"io"
 	"log"
 	"mime/multipart"
 	"net/http"
@@ -46,4 +48,37 @@ func (gv *GameVideo) PostVideo(strGameID string, video multipart.File) error {
 	}
 
 	return nil
+}
+
+func (gv *GameVideo) GetVideo(strGameID string) (io.Reader, error) {
+	ctx := context.Background()
+
+	uuidGameID, err := uuid.Parse(strGameID)
+	if err != nil {
+		return nil, echo.NewHTTPError(http.StatusBadRequest, "invalid game id")
+	}
+
+	gameID := values.NewGameIDFromUUID(uuidGameID)
+
+	/*
+		メモリに保持してしまうので、
+		大きい画像を返すとメモリが溶けてしまう。
+		Pipeを使いたいが、openapiでの生成コードからio.Writerが渡されておらず、
+		エラーハンドリングが怪しくなるので一旦これで妥協する。
+	*/
+	buf := bytes.NewBuffer(nil)
+
+	err = gv.gameVideoService.GetGameVideo(ctx, buf, gameID)
+	if errors.Is(err, service.ErrNoGameVideo) {
+		return nil, echo.NewHTTPError(http.StatusNotFound, "no video")
+	}
+	if errors.Is(err, service.ErrInvalidGameID) {
+		return nil, echo.NewHTTPError(http.StatusBadRequest, "invalid game id")
+	}
+	if err != nil {
+		log.Printf("error: failed to get video: %v\n", err)
+		return nil, echo.NewHTTPError(http.StatusInternalServerError, "failed to get video")
+	}
+
+	return buf, nil
 }
