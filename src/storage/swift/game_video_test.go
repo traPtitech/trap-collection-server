229eecedc2f658bcc5380e0cc5fbd1a12f8ffea3
mazrean <shunsuke.w04@gmail.com>
2021-11-06 17:26:17 +0900

    TestGetGameVideo追加

--- src/storage/swift/game_video_test.go
+++ src/storage/swift/game_video_test.go
@@ -144,3 +144,116 @@ func TestSaveGameVideo(t *testing.T) {
 		})
 	}
 }
+
+func TestGetGameVideo(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	client, err := newTestClient(
+		ctx,
+		common.SwiftContainer("get_game_video"),
+		common.FilePath("get_game_video"),
+	)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+	defer func() {
+		err := os.RemoveAll("get_game_video")
+		if err != nil {
+			t.Fatalf("failed to remove directory: %v", err)
+		}
+	}()
+
+	gameVideoStorage := NewGameVideo(client)
+
+	type test struct {
+		description string
+		video       *domain.GameVideo
+		isFileExist bool
+		isErr       bool
+		err         error
+	}
+
+	testCases := []test{
+		{
+			description: "特に問題ないのでエラーなし",
+			video: domain.NewGameVideo(
+				values.NewGameVideoID(),
+				values.GameVideoTypeMp4,
+			),
+			isFileExist: true,
+		},
+		{
+			description: "ファイルが存在しないのでErrNotFound",
+			video: domain.NewGameVideo(
+				values.NewGameVideoID(),
+				values.GameVideoTypeMp4,
+			),
+			isErr: true,
+			err:   storage.ErrNotFound,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			videoBuf := bytes.NewBuffer(nil)
+
+			switch testCase.video.GetType() {
+			case values.GameVideoTypeMp4:
+				err := func() error {
+					f, err := os.Open("../../../testdata/1.mp4")
+					if err != nil {
+						return fmt.Errorf("failed to open file: %w", err)
+					}
+					defer f.Close()
+
+					_, err = io.Copy(videoBuf, f)
+					if err != nil {
+						return fmt.Errorf("failed to copy file: %w", err)
+					}
+
+					return nil
+				}()
+				if err != nil {
+					t.Fatalf("failed to encode image: %s", err)
+				}
+			default:
+				videoBuf = bytes.NewBufferString("hoge")
+			}
+
+			expectBytes := videoBuf.Bytes()
+
+			if testCase.isFileExist {
+				err := client.saveFile(
+					ctx,
+					fmt.Sprintf("videos/%s", uuid.UUID(testCase.video.GetID()).String()),
+					"",
+					"",
+					videoBuf,
+				)
+				if err != nil {
+					t.Fatalf("failed to create file: %v", err)
+				}
+			}
+
+			buf := bytes.NewBuffer(nil)
+			err := gameVideoStorage.GetGameVideo(ctx, buf, testCase.video)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, expectBytes, buf.Bytes())
+		})
+	}
+}
