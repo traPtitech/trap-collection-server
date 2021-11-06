bda169b2a482c41ab07625110c0f46f595e9d442
mazrean <shunsuke.w04@gmail.com>
2021-11-06 16:58:49 +0900

    SaveGameVideo実装

--- src/storage/swift/game_video.go
+++ src/storage/swift/game_video.go
@@ -1,10 +1,15 @@
 package swift
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"io"
 
 	"github.com/google/uuid"
 	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/storage"
 )
 
 type GameVideo struct {
@@ -15,6 +20,34 @@ func NewGameVideo(client *Client) *GameVideo {
 	return &GameVideo{
 		client: client,
 	}
+}
+
+func (gv *GameVideo) SaveGameVideo(ctx context.Context, reader io.Reader, video *domain.GameVideo) error {
+	videoKey := gv.videoKey(video)
+
+	var contentType string
+	switch video.GetType() {
+	case values.GameVideoTypeMp4:
+		contentType = "video/mp4"
+	default:
+		return fmt.Errorf("unsupported video type: %d", video.GetType())
+	}
+
+	err := gv.client.saveFile(
+		ctx,
+		videoKey,
+		contentType,
+		"",
+		reader,
+	)
+	if errors.Is(err, ErrAlreadyExists) {
+		return storage.ErrAlreadyExists
+	}
+	if err != nil {
+		return fmt.Errorf("failed to save video: %w", err)
+	}
+
+	return nil
 }
 
 func (gv *GameVideo) videoKey(video *domain.GameVideo) string {
