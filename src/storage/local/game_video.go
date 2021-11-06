21e886c578c280c740544a440e2e1e2cdb537b0b
mazrean <shunsuke.w04@gmail.com>
2021-11-06 16:09:42 +0900

    SaveGameVideo実装

--- src/storage/local/game_video.go
+++ src/storage/local/game_video.go
@@ -1,7 +1,17 @@
 package local
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path"
+
+	"github.com/google/uuid"
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/storage"
 )
 
 type GameVideo struct {
@@ -19,4 +29,29 @@ func NewGameVideo(directoryManager *DirectoryManager) (*GameVideo, error) {
 		videoRootPath:    videoRootPath,
 		directoryManager: directoryManager,
 	}, nil
+}
+
+func (gv *GameVideo) SaveGameVideo(ctx context.Context, reader io.Reader, video *domain.GameVideo) error {
+	videoPath := path.Join(gv.videoRootPath, uuid.UUID(video.GetID()).String())
+
+	_, err := os.Stat(videoPath)
+	if err == nil {
+		return storage.ErrAlreadyExists
+	}
+	if err != nil && !errors.Is(err, fs.ErrNotExist) {
+		return fmt.Errorf("failed to stat file: %w", err)
+	}
+
+	f, err := os.Create(videoPath)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, reader)
+	if err != nil {
+		return fmt.Errorf("failed to copy: %w", err)
+	}
+
+	return nil
 }
