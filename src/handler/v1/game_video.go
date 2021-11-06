35c6882d52657151954324524920bd121e0cbff8
mazrean <shunsuke.w04@gmail.com>
2021-11-06 20:33:45 +0900

    PostVideo実装

--- src/handler/v1/game_video.go
+++ src/handler/v1/game_video.go
@@ -1,6 +1,17 @@
 package v1
 
-import "github.com/traPtitech/trap-collection-server/src/service"
+import (
+	"context"
+	"errors"
+	"log"
+	"mime/multipart"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/service"
+)
 
 type GameVideo struct {
 	gameVideoService service.GameVideo
@@ -10,4 +21,29 @@ func NewGameVideo(gameVideoService service.GameVideo) *GameVideo {
 	return &GameVideo{
 		gameVideoService: gameVideoService,
 	}
+}
+
+func (gv *GameVideo) PostVideo(strGameID string, video multipart.File) error {
+	ctx := context.Background()
+
+	uuidGameID, err := uuid.Parse(strGameID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid game id")
+	}
+
+	gameID := values.NewGameIDFromUUID(uuidGameID)
+
+	err = gv.gameVideoService.SaveGameVideo(ctx, video, gameID)
+	if errors.Is(err, service.ErrInvalidGameID) {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid game id")
+	}
+	if errors.Is(err, service.ErrInvalidFormat) {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid video format")
+	}
+	if err != nil {
+		log.Printf("error: failed to save video: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to save video")
+	}
+
+	return nil
 }
