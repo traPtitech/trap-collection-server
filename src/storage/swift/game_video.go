5082588c5adacacf6e2b30dced91110f7daf2326
mazrean <shunsuke.w04@gmail.com>
2021-11-06 16:53:20 +0900

    videoKey実装

--- src/storage/swift/game_video.go
+++ src/storage/swift/game_video.go
@@ -1,5 +1,12 @@
 package swift
 
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/traPtitech/trap-collection-server/src/domain"
+)
+
 type GameVideo struct {
 	client *Client
 }
@@ -8,4 +15,8 @@ func NewGameVideo(client *Client) *GameVideo {
 	return &GameVideo{
 		client: client,
 	}
+}
+
+func (gv *GameVideo) videoKey(video *domain.GameVideo) string {
+	return fmt.Sprintf("videos/%s", uuid.UUID(video.GetID()).String())
 }
