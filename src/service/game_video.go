13b97ec52840db76c3d0d3d74faac94b50d9097d
mazrean <shunsuke.w04@gmail.com>
2021-11-06 13:24:24 +0900

    GameVideoサービス抽象化

---
+++ src/service/game_video.go
@@ -0,0 +1,15 @@
+package service
+
+//go:generate mockgen -source=$GOFILE -destination=mock/${GOFILE} -package=mock
+
+import (
+	"context"
+	"io"
+
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+)
+
+type GameVideo interface {
+	SaveGameVideo(ctx context.Context, reader io.Reader, gameID values.GameID) error
+	GetGameVideo(ctx context.Context, writer io.Writer, gameID values.GameID) error
+}
