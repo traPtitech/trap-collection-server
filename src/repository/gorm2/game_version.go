44cded7ff1cecd81e9992aa4f8ad9c1fe74796bb
mazrean <shunsuke.w04@gmail.com>
2021-11-17 15:10:37 +0900

    GetGameVersions実装

--- src/repository/gorm2/game_version.go
+++ src/repository/gorm2/game_version.go
@@ -40,3 +40,35 @@ func (gv *GameVersion) CreateGameVersion(ctx context.Context, gameID values.Game
 
 	return nil
 }
+
+func (gv *GameVersion) GetGameVersions(ctx context.Context, gameID values.GameID) ([]*domain.GameVersion, error) {
+	gormDB, err := gv.db.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get gorm DB: %w", err)
+	}
+
+	var gameVersions []GameVersionTable
+	err = gormDB.
+		Where("game_id = ?", uuid.UUID(gameID)).
+		Order("created_at desc").
+		Select("id", "name", "description", "created_at").
+		Find(&gameVersions).Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to get game versions: %w", err)
+	}
+
+	versions := make([]*domain.GameVersion, 0, len(gameVersions))
+	for _, gameVersion := range gameVersions {
+		gameVersionID := values.NewGameVersionIDFromUUID(gameVersion.ID)
+		gameVersionName := values.NewGameVersionName(gameVersion.Name)
+		gameVersionDescription := values.NewGameVersionDescription(gameVersion.Description)
+		versions = append(versions, domain.NewGameVersion(
+			gameVersionID,
+			gameVersionName,
+			gameVersionDescription,
+			gameVersion.CreatedAt,
+		))
+	}
+
+	return versions, nil
+}
