307247524b2545915ca010f1106b26dfc9499753
mazrean <shunsuke.w04@gmail.com>
2021-11-17 14:58:03 +0900

    CreateGameVersion実装

--- src/repository/gorm2/game_version.go
+++ src/repository/gorm2/game_version.go
@@ -1,5 +1,14 @@
 package gorm2
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+)
+
 type GameVersion struct {
 	db *DB
 }
@@ -8,4 +17,26 @@ func NewGameVersion(db *DB) *GameVersion {
 	return &GameVersion{
 		db: db,
 	}
+}
+
+func (gv *GameVersion) CreateGameVersion(ctx context.Context, gameID values.GameID, version *domain.GameVersion) error {
+	gormDB, err := gv.db.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get gorm DB: %w", err)
+	}
+
+	gameVersion := GameVersionTable{
+		ID:          uuid.UUID(version.GetID()),
+		GameID:      uuid.UUID(gameID),
+		Name:        string(version.GetName()),
+		Description: string(version.GetDescription()),
+		CreatedAt:   version.GetCreatedAt(),
+	}
+
+	err = gormDB.Create(&gameVersion).Error
+	if err != nil {
+		return fmt.Errorf("failed to create game version: %w", err)
+	}
+
+	return nil
 }
