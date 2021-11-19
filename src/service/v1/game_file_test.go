3db085df5f4307a53bee767f22e43832d276afa2
mazrean <shunsuke.w04@gmail.com>
2021-11-19 23:00:42 +0900

    TestGetGameFile追加

--- src/service/v1/game_file_test.go
+++ src/service/v1/game_file_test.go
@@ -280,3 +280,405 @@ func TestSaveGameFile(t *testing.T) {
 		})
 	}
 }
+
+func TestGetGameFile(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockDB := mockRepository.NewMockDB(ctrl)
+	mockGameRepository := mockRepository.NewMockGame(ctrl)
+	mockGameVersionRepository := mockRepository.NewMockGameVersion(ctrl)
+	mockGameFileRepository := mockRepository.NewMockGameFile(ctrl)
+
+	type test struct {
+		description                  string
+		fileContent                  *bytes.Buffer
+		gameID                       values.GameID
+		environment                  *values.LauncherEnvironment
+		GetGameErr                   error
+		executeGetLatestGameVersion  bool
+		gameVersion                  *domain.GameVersion
+		GetLatestGameVersionErr      error
+		executeRepositoryGetGameFile bool
+		gameFiles                    []*domain.GameFile
+		repositoryGetGameFileErr     error
+		gameFile                     *domain.GameFile
+		executeStorageGetGameFile    bool
+		storageGetGameFileErr        error
+		isErr                        bool
+		err                          error
+	}
+
+	gameID := values.NewGameID()
+	gameFileID1 := values.NewGameFileID()
+	gameFileID2 := values.NewGameFileID()
+
+	testCases := []test{
+		{
+			description:                 "特に問題ないのでエラーなし",
+			fileContent:                 bytes.NewBufferString("test"),
+			gameID:                      gameID,
+			environment:                 values.NewLauncherEnvironment(values.LauncherEnvironmentOSWindows),
+			executeGetLatestGameVersion: true,
+			gameVersion: domain.NewGameVersion(
+				values.NewGameVersionID(),
+				"v1.0.0",
+				"リリース",
+				time.Now(),
+			),
+			executeRepositoryGetGameFile: true,
+			gameFiles: []*domain.GameFile{
+				domain.NewGameFile(
+					gameFileID1,
+					values.GameFileTypeJar,
+					"/path/to/game.jar",
+					values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+				),
+			},
+			gameFile: domain.NewGameFile(
+				gameFileID1,
+				values.GameFileTypeJar,
+				"/path/to/game.jar",
+				values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+			),
+			executeStorageGetGameFile: true,
+		},
+		{
+			description: "ゲームが存在しないのでエラー",
+			fileContent: bytes.NewBufferString("test"),
+			gameID:      gameID,
+			environment: values.NewLauncherEnvironment(values.LauncherEnvironmentOSWindows),
+			GetGameErr:  repository.ErrRecordNotFound,
+			isErr:       true,
+			err:         service.ErrInvalidGameID,
+		},
+		{
+			description: "GetGameがエラーなのでエラー",
+			fileContent: bytes.NewBufferString("test"),
+			gameID:      gameID,
+			environment: values.NewLauncherEnvironment(values.LauncherEnvironmentOSWindows),
+			GetGameErr:  errors.New("error"),
+			isErr:       true,
+		},
+		{
+			description:                 "ゲームバージョンが存在しないのでエラー",
+			fileContent:                 bytes.NewBufferString("test"),
+			gameID:                      gameID,
+			environment:                 values.NewLauncherEnvironment(values.LauncherEnvironmentOSWindows),
+			executeGetLatestGameVersion: true,
+			GetLatestGameVersionErr:     repository.ErrRecordNotFound,
+			isErr:                       true,
+			err:                         service.ErrNoGameVersion,
+		},
+		{
+			description:                 "GetLatestGameVersionがエラーなのでエラー",
+			fileContent:                 bytes.NewBufferString("test"),
+			gameID:                      gameID,
+			environment:                 values.NewLauncherEnvironment(values.LauncherEnvironmentOSWindows),
+			executeGetLatestGameVersion: true,
+			GetLatestGameVersionErr:     errors.New("error"),
+			isErr:                       true,
+		},
+		{
+			description:                 "repositoryのGetGameFileがエラーなのでエラー",
+			fileContent:                 bytes.NewBufferString("test"),
+			gameID:                      gameID,
+			environment:                 values.NewLauncherEnvironment(values.LauncherEnvironmentOSWindows),
+			executeGetLatestGameVersion: true,
+			gameVersion: domain.NewGameVersion(
+				values.NewGameVersionID(),
+				"v1.0.0",
+				"リリース",
+				time.Now(),
+			),
+			executeRepositoryGetGameFile: true,
+			repositoryGetGameFileErr:     errors.New("error"),
+			isErr:                        true,
+		},
+		{
+			description:                 "storageのGetGameFileがエラーなのでエラー",
+			fileContent:                 bytes.NewBufferString("test"),
+			gameID:                      gameID,
+			environment:                 values.NewLauncherEnvironment(values.LauncherEnvironmentOSWindows),
+			executeGetLatestGameVersion: true,
+			gameVersion: domain.NewGameVersion(
+				values.NewGameVersionID(),
+				"v1.0.0",
+				"リリース",
+				time.Now(),
+			),
+			executeRepositoryGetGameFile: true,
+			gameFiles: []*domain.GameFile{
+				domain.NewGameFile(
+					gameFileID1,
+					values.GameFileTypeJar,
+					"/path/to/game.jar",
+					values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+				),
+			},
+			gameFile: domain.NewGameFile(
+				gameFileID1,
+				values.GameFileTypeJar,
+				"/path/to/game.jar",
+				values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+			),
+			executeStorageGetGameFile: true,
+			storageGetGameFileErr:     errors.New("error"),
+			isErr:                     true,
+		},
+		{
+			description:                 "ゲームファイルが存在しないのでエラー",
+			fileContent:                 bytes.NewBufferString("test"),
+			gameID:                      gameID,
+			environment:                 values.NewLauncherEnvironment(values.LauncherEnvironmentOSWindows),
+			executeGetLatestGameVersion: true,
+			gameVersion: domain.NewGameVersion(
+				values.NewGameVersionID(),
+				"v1.0.0",
+				"リリース",
+				time.Now(),
+			),
+			executeRepositoryGetGameFile: true,
+			gameFiles:                    []*domain.GameFile{},
+			isErr:                        true,
+			err:                          service.ErrNoGameFile,
+		},
+		{
+			description:                 "windows用のファイルが存在すればjarよりそちらを優先する",
+			fileContent:                 bytes.NewBufferString("test"),
+			gameID:                      gameID,
+			environment:                 values.NewLauncherEnvironment(values.LauncherEnvironmentOSWindows),
+			executeGetLatestGameVersion: true,
+			gameVersion: domain.NewGameVersion(
+				values.NewGameVersionID(),
+				"v1.0.0",
+				"リリース",
+				time.Now(),
+			),
+			executeRepositoryGetGameFile: true,
+			gameFiles: []*domain.GameFile{
+				domain.NewGameFile(
+					gameFileID1,
+					values.GameFileTypeJar,
+					"/path/to/game.jar",
+					values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+				),
+				domain.NewGameFile(
+					gameFileID2,
+					values.GameFileTypeWindows,
+					"/path/to/game.exe",
+					values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+				),
+			},
+			gameFile: domain.NewGameFile(
+				gameFileID2,
+				values.GameFileTypeWindows,
+				"/path/to/game.exe",
+				values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+			),
+			executeStorageGetGameFile: true,
+		},
+		{
+			description:                 "順番に関わらずwindows用のファイルが存在すればjarよりそちらを優先する",
+			fileContent:                 bytes.NewBufferString("test"),
+			gameID:                      gameID,
+			environment:                 values.NewLauncherEnvironment(values.LauncherEnvironmentOSWindows),
+			executeGetLatestGameVersion: true,
+			gameVersion: domain.NewGameVersion(
+				values.NewGameVersionID(),
+				"v1.0.0",
+				"リリース",
+				time.Now(),
+			),
+			executeRepositoryGetGameFile: true,
+			gameFiles: []*domain.GameFile{
+				domain.NewGameFile(
+					gameFileID2,
+					values.GameFileTypeWindows,
+					"/path/to/game.exe",
+					values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+				),
+				domain.NewGameFile(
+					gameFileID1,
+					values.GameFileTypeJar,
+					"/path/to/game.jar",
+					values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+				),
+			},
+			gameFile: domain.NewGameFile(
+				gameFileID2,
+				values.GameFileTypeWindows,
+				"/path/to/game.exe",
+				values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+			),
+			executeStorageGetGameFile: true,
+		},
+		{
+			description:                 "mac用のファイルが存在すればjarよりそちらを優先する",
+			fileContent:                 bytes.NewBufferString("test"),
+			gameID:                      gameID,
+			environment:                 values.NewLauncherEnvironment(values.LauncherEnvironmentOSMac),
+			executeGetLatestGameVersion: true,
+			gameVersion: domain.NewGameVersion(
+				values.NewGameVersionID(),
+				"v1.0.0",
+				"リリース",
+				time.Now(),
+			),
+			executeRepositoryGetGameFile: true,
+			gameFiles: []*domain.GameFile{
+				domain.NewGameFile(
+					gameFileID1,
+					values.GameFileTypeJar,
+					"/path/to/game.jar",
+					values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+				),
+				domain.NewGameFile(
+					gameFileID2,
+					values.GameFileTypeMac,
+					"/path/to/game.app",
+					values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+				),
+			},
+			gameFile: domain.NewGameFile(
+				gameFileID2,
+				values.GameFileTypeMac,
+				"/path/to/game.app",
+				values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+			),
+			executeStorageGetGameFile: true,
+		},
+		{
+			description:                 "順番に関わらずmac用のファイルが存在すればjarよりそちらを優先する",
+			fileContent:                 bytes.NewBufferString("test"),
+			gameID:                      gameID,
+			environment:                 values.NewLauncherEnvironment(values.LauncherEnvironmentOSMac),
+			executeGetLatestGameVersion: true,
+			gameVersion: domain.NewGameVersion(
+				values.NewGameVersionID(),
+				"v1.0.0",
+				"リリース",
+				time.Now(),
+			),
+			executeRepositoryGetGameFile: true,
+			gameFiles: []*domain.GameFile{
+				domain.NewGameFile(
+					gameFileID2,
+					values.GameFileTypeMac,
+					"/path/to/game.app",
+					values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+				),
+				domain.NewGameFile(
+					gameFileID1,
+					values.GameFileTypeJar,
+					"/path/to/game.jar",
+					values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+				),
+			},
+			gameFile: domain.NewGameFile(
+				gameFileID2,
+				values.GameFileTypeMac,
+				"/path/to/game.app",
+				values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+			),
+			executeStorageGetGameFile: true,
+		},
+		{
+			description:                 "ファイルが大きくても問題なし",
+			fileContent:                 bytes.NewBufferString(strings.Repeat("a", 1e7)),
+			gameID:                      gameID,
+			environment:                 values.NewLauncherEnvironment(values.LauncherEnvironmentOSWindows),
+			executeGetLatestGameVersion: true,
+			gameVersion: domain.NewGameVersion(
+				values.NewGameVersionID(),
+				"v1.0.0",
+				"リリース",
+				time.Now(),
+			),
+			executeRepositoryGetGameFile: true,
+			gameFiles: []*domain.GameFile{
+				domain.NewGameFile(
+					gameFileID1,
+					values.GameFileTypeJar,
+					"/path/to/game.jar",
+					values.NewGameFileHashFromBytes([]byte{0x70, 0x95, 0xba, 0xe0, 0x98, 0x25, 0x9e, 0xd, 0xda, 0x4b, 0x7a, 0xcc, 0x62, 0x4d, 0xe4, 0xe2}),
+				),
+			},
+			gameFile: domain.NewGameFile(
+				gameFileID1,
+				values.GameFileTypeJar,
+				"/path/to/game.jar",
+				values.NewGameFileHashFromBytes([]byte{0x70, 0x95, 0xba, 0xe0, 0x98, 0x25, 0x9e, 0xd, 0xda, 0x4b, 0x7a, 0xcc, 0x62, 0x4d, 0xe4, 0xe2}),
+			),
+			executeStorageGetGameFile: true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			mockGameFileStorage := mockStorage.NewGameFile(ctrl, testCase.fileContent)
+
+			gameFileService := NewGameFile(
+				mockDB,
+				mockGameRepository,
+				mockGameVersionRepository,
+				mockGameFileRepository,
+				mockGameFileStorage,
+			)
+
+			mockGameRepository.
+				EXPECT().
+				GetGame(gomock.Any(), gameID, repository.LockTypeNone).
+				Return(nil, testCase.GetGameErr)
+
+			if testCase.executeGetLatestGameVersion {
+				mockGameVersionRepository.
+					EXPECT().
+					GetLatestGameVersion(gomock.Any(), gameID, repository.LockTypeNone).
+					Return(testCase.gameVersion, testCase.GetLatestGameVersionErr)
+			}
+
+			if testCase.executeRepositoryGetGameFile {
+				mockGameFileRepository.
+					EXPECT().
+					GetGameFiles(gomock.Any(), testCase.gameVersion.GetID(), gomock.Any()).
+					Return(testCase.gameFiles, testCase.repositoryGetGameFileErr)
+			}
+
+			if testCase.executeStorageGetGameFile {
+				mockGameFileStorage.
+					EXPECT().
+					GetGameFile(gomock.Any(), gomock.Any()).
+					Return(testCase.storageGetGameFileErr)
+			}
+
+			var expectBytes []byte
+			if testCase.fileContent != nil {
+				expectBytes = testCase.fileContent.Bytes()
+			}
+
+			buf := bytes.NewBuffer(nil)
+			gameFile, err := gameFileService.GetGameFile(ctx, buf, testCase.gameID, testCase.environment)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, *testCase.gameFile, *gameFile)
+			assert.Equal(t, expectBytes, buf.Bytes())
+		})
+	}
+}
