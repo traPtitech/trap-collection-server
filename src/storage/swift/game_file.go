24cddfbc78839a8f42e8ceaea0dbed57d9c5b6ab
mazrean <shunsuke.w04@gmail.com>
2021-11-20 21:35:43 +0900

    fileKey追加

--- src/storage/swift/game_file.go
+++ src/storage/swift/game_file.go
@@ -1,5 +1,12 @@
 package swift
 
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/traPtitech/trap-collection-server/src/domain"
+)
+
 type GameFile struct {
 	client *Client
 }
@@ -8,4 +15,8 @@ func NewGameFile(client *Client) *GameFile {
 	return &GameFile{
 		client: client,
 	}
+}
+
+func (gf *GameFile) fileKey(file *domain.GameFile) string {
+	return fmt.Sprintf("files/%s", uuid.UUID(file.GetID()).String())
 }
