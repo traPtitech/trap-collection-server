37e368de5b74dbf63650b61b7a44c5d07f03dd56
mazrean <shunsuke.w04@gmail.com>
2021-11-20 21:21:32 +0900

    TestGetGameFile追加

--- src/storage/local/game_file_test.go
+++ src/storage/local/game_file_test.go
@@ -116,3 +116,101 @@ func TestSaveGameFile(t *testing.T) {
 		})
 	}
 }
+
+func TestGetGameFile(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	rootPath := common.FilePath("./get_game_file_test")
+
+	directoryManager := NewDirectoryManager(rootPath)
+	defer func() {
+		err := os.RemoveAll(string(rootPath))
+		if err != nil {
+			t.Fatalf("failed to remove directory: %v", err)
+		}
+	}()
+
+	gameFile, err := NewGameFile(directoryManager)
+	if err != nil {
+		t.Fatalf("failed to create game file: %v", err)
+	}
+
+	fileRootPath := filepath.Join(string(rootPath), "files")
+
+	type test struct {
+		description string
+		file        *domain.GameFile
+		isFileExist bool
+		fileContent *bytes.Buffer
+		isErr       bool
+		err         error
+	}
+
+	testCases := []test{
+		{
+			description: "ファイルが存在するので読み込める",
+			file: domain.NewGameFile(
+				values.NewGameFileID(),
+				values.GameFileTypeJar,
+				"/path/to/game.jar",
+				values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+			),
+			isFileExist: true,
+			fileContent: bytes.NewBufferString("test"),
+		},
+		{
+			description: "ファイルが存在しないので読み込めない",
+			file: domain.NewGameFile(
+				values.NewGameFileID(),
+				values.GameFileTypeJar,
+				"/path/to/game.jar",
+				values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+			),
+			isErr: true,
+			err:   storage.ErrNotFound,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			var expectBytes []byte
+			if testCase.isFileExist {
+				expectBytes = testCase.fileContent.Bytes()
+
+				func() {
+					f, err := os.Create(filepath.Join(fileRootPath, uuid.UUID(testCase.file.GetID()).String()))
+					if err != nil {
+						t.Fatalf("failed to write file: %v", err)
+					}
+					defer f.Close()
+
+					_, err = io.Copy(f, testCase.fileContent)
+					if err != nil {
+						t.Fatalf("failed to write file: %v", err)
+					}
+				}()
+			}
+
+			buf := bytes.NewBuffer(nil)
+
+			err := gameFile.GetGameFile(ctx, buf, testCase.file)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, expectBytes, buf.Bytes())
+		})
+	}
+}
