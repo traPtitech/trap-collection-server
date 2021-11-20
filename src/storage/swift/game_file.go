3bb5ce05a329e506a7e1a9a41f0940057c4230d9
mazrean <shunsuke.w04@gmail.com>
2021-11-20 21:40:25 +0900

    SaveGameFile実装

--- src/storage/swift/game_file.go
+++ src/storage/swift/game_file.go
@@ -1,10 +1,15 @@
 package swift
 
 import (
+	"context"
+	"encoding/hex"
+	"errors"
 	"fmt"
+	"io"
 
 	"github.com/google/uuid"
 	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/storage"
 )
 
 type GameFile struct {
@@ -15,6 +20,28 @@ func NewGameFile(client *Client) *GameFile {
 	return &GameFile{
 		client: client,
 	}
+}
+
+func (gf *GameFile) SaveGameFile(ctx context.Context, reader io.Reader, file *domain.GameFile) error {
+	fileKey := gf.fileKey(file)
+
+	contentType := "application/zip"
+
+	err := gf.client.saveFile(
+		ctx,
+		fileKey,
+		contentType,
+		hex.EncodeToString([]byte(file.GetHash())),
+		reader,
+	)
+	if errors.Is(err, ErrAlreadyExists) {
+		return storage.ErrAlreadyExists
+	}
+	if err != nil {
+		return fmt.Errorf("failed to save file: %w", err)
+	}
+
+	return nil
 }
 
 func (gf *GameFile) fileKey(file *domain.GameFile) string {
