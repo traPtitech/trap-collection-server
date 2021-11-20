ae92f8e0b901e1a2373c6d9f6d532ab18c75eaff
mazrean <shunsuke.w04@gmail.com>
2021-11-20 21:54:39 +0900

    TestSaveGameFile追加

--- src/storage/swift/game_file_test.go
+++ src/storage/swift/game_file_test.go
@@ -1,15 +1,122 @@
 package swift
 
 import (
+	"bytes"
+	"context"
+	"errors"
 	"fmt"
 	"math/rand"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/traPtitech/trap-collection-server/pkg/common"
 	"github.com/traPtitech/trap-collection-server/src/domain"
 	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/storage"
 )
+
+func TestSaveGameFile(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	client, err := newTestClient(
+		ctx,
+		common.SwiftContainer("save_game_file"),
+		common.FilePath("save_game_file"),
+	)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+	defer func() {
+		err := os.RemoveAll("save_game_file")
+		if err != nil {
+			t.Fatalf("failed to remove directory: %v", err)
+		}
+	}()
+
+	type test struct {
+		description string
+		file        *domain.GameFile
+		reader      *bytes.Buffer
+		isFileExist bool
+		isErr       bool
+		err         error
+	}
+
+	testCases := []test{
+		{
+			description: "特に問題ないのでエラーなし",
+			file: domain.NewGameFile(
+				values.NewGameFileID(),
+				values.GameFileTypeJar,
+				"path/to/game.jar",
+				values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+			),
+			reader: bytes.NewBufferString("test"),
+		},
+		{
+			description: "ファイルが存在するのでErrAlreadyExists",
+			file: domain.NewGameFile(
+				values.NewGameFileID(),
+				values.GameFileTypeJar,
+				"path/to/game.jar",
+				values.NewGameFileHashFromBytes([]byte{0x09, 0x8f, 0x6b, 0xcd, 0x46, 0x21, 0xd3, 0x73, 0xca, 0xde, 0x4e, 0x83, 0x26, 0x27, 0xb4, 0xf6}),
+			),
+			reader:      bytes.NewBufferString("test"),
+			isFileExist: true,
+			isErr:       true,
+			err:         storage.ErrAlreadyExists,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			gameFileStorage := NewGameFile(client)
+
+			if testCase.isFileExist {
+				err := client.saveFile(
+					ctx,
+					fmt.Sprintf("files/%s", uuid.UUID(testCase.file.GetID()).String()),
+					"text/plain",
+					"",
+					strings.NewReader(""),
+				)
+				if err != nil {
+					t.Fatalf("failed to create file: %v", err)
+				}
+			}
+
+			expectBytes := testCase.reader.Bytes()
+
+			err := gameFileStorage.SaveGameFile(ctx, testCase.reader, testCase.file)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			buf := bytes.NewBuffer(nil)
+			err = client.loadFile(ctx, fmt.Sprintf("files/%s", uuid.UUID(testCase.file.GetID()).String()), buf)
+			if err != nil {
+				t.Fatalf("failed to load file: %v", err)
+			}
+
+			assert.Equal(t, expectBytes, buf.Bytes())
+		})
+	}
+}
 
 func TestFileKey(t *testing.T) {
 	t.Parallel()
