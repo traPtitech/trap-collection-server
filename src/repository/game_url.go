137bc4667add9e7a3492c1f4f10145a262100ce1
mazrean <shunsuke.w04@gmail.com>
2021-12-18 01:15:02 +0900

    GameURLリポジトリ抽象化

---
+++ src/repository/game_url.go
@@ -0,0 +1,15 @@
+package repository
+
+//go:generate mockgen -source=$GOFILE -destination=mock/${GOFILE} -package=mock
+
+import (
+	"context"
+
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+)
+
+type GameURL interface {
+	SaveGameURL(ctx context.Context, gameVersionID values.GameVersionID, url *domain.GameURL) error
+	GetGameURL(ctx context.Context, gameVersionID values.GameVersionID) (*domain.GameURL, error)
+}
