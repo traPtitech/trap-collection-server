f858afa6560d203f263e8cc2753b5fbf0725288e
mazrean <shunsuke.w04@gmail.com>
2021-12-18 02:30:40 +0900

    TestGetGameURL追加

--- src/service/v1/game_url_test.go
+++ src/service/v1/game_url_test.go
@@ -198,3 +198,155 @@ func TestSaveGameURL(t *testing.T) {
 		})
 	}
 }
+
+func TestGetGameURL(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockDB := mockRepository.NewMockDB(ctrl)
+	mockGameRepository := mockRepository.NewMockGame(ctrl)
+	mockGameVersionRepository := mockRepository.NewMockGameVersion(ctrl)
+	mockGameURLRepository := mockRepository.NewMockGameURL(ctrl)
+
+	gameURLService := NewGameURL(mockDB, mockGameRepository, mockGameVersionRepository, mockGameURLRepository)
+
+	type test struct {
+		description                 string
+		gameID                      values.GameID
+		GetGameErr                  error
+		executeGetLatestGameVersion bool
+		gameVersion                 *domain.GameVersion
+		GetLatestGameVersionErr     error
+		executeGetGameURL           bool
+		gameURL                     *domain.GameURL
+		GetGameURLErr               error
+		isErr                       bool
+		err                         error
+	}
+
+	gameID := values.NewGameID()
+
+	urlLink, err := url.Parse("https://example.com")
+	if err != nil {
+		t.Fatalf("failed to encode image: %v", err)
+	}
+	link := values.NewGameURLLink(urlLink)
+
+	testCases := []test{
+		{
+			description:                 "特に問題ないのでエラーなし",
+			gameID:                      gameID,
+			executeGetLatestGameVersion: true,
+			gameVersion: domain.NewGameVersion(
+				values.NewGameVersionID(),
+				"v1.0.0",
+				"リリース",
+				time.Now(),
+			),
+			executeGetGameURL: true,
+			gameURL:           domain.NewGameURL(values.NewGameURLID(), link),
+		},
+		{
+			description: "ゲームが存在しないのでエラー",
+			gameID:      gameID,
+			GetGameErr:  repository.ErrRecordNotFound,
+			isErr:       true,
+			err:         service.ErrInvalidGameID,
+		},
+		{
+			description: "GetGameがエラーなのでエラー",
+			gameID:      gameID,
+			GetGameErr:  errors.New("error"),
+			isErr:       true,
+		},
+		{
+			description:                 "ゲームバージョンが存在しないのでエラー",
+			gameID:                      gameID,
+			executeGetLatestGameVersion: true,
+			GetLatestGameVersionErr:     repository.ErrRecordNotFound,
+			isErr:                       true,
+			err:                         service.ErrNoGameVersion,
+		},
+		{
+			description:                 "GetLatestGameVersionがエラーなのでエラー",
+			gameID:                      gameID,
+			executeGetLatestGameVersion: true,
+			GetLatestGameVersionErr:     errors.New("error"),
+			isErr:                       true,
+		},
+		{
+			description:                 "GetGameURLがErrRecordNotFoundなのでエラー",
+			gameID:                      gameID,
+			executeGetLatestGameVersion: true,
+			gameVersion: domain.NewGameVersion(
+				values.NewGameVersionID(),
+				"v1.0.0",
+				"リリース",
+				time.Now(),
+			),
+			executeGetGameURL: true,
+			GetGameURLErr:     repository.ErrRecordNotFound,
+			isErr:             true,
+			err:               service.ErrNoGameURL,
+		},
+		{
+			description:                 "GetGameURLがエラーなのでエラー",
+			gameID:                      gameID,
+			executeGetLatestGameVersion: true,
+			gameVersion: domain.NewGameVersion(
+				values.NewGameVersionID(),
+				"v1.0.0",
+				"リリース",
+				time.Now(),
+			),
+			executeGetGameURL: true,
+			GetGameURLErr:     errors.New("error"),
+			isErr:             true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			mockGameRepository.
+				EXPECT().
+				GetGame(gomock.Any(), testCase.gameID, repository.LockTypeNone).
+				Return(nil, testCase.GetGameErr)
+
+			if testCase.executeGetLatestGameVersion {
+				mockGameVersionRepository.
+					EXPECT().
+					GetLatestGameVersion(gomock.Any(), testCase.gameID, repository.LockTypeNone).
+					Return(testCase.gameVersion, testCase.GetLatestGameVersionErr)
+			}
+
+			if testCase.executeGetGameURL {
+				mockGameURLRepository.
+					EXPECT().
+					GetGameURL(gomock.Any(), testCase.gameVersion.GetID()).
+					Return(testCase.gameURL, testCase.GetGameURLErr)
+			}
+
+			gameURL, err := gameURLService.GetGameURL(ctx, testCase.gameID)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, testCase.gameURL.GetID(), gameURL.GetID())
+			assert.Equal(t, testCase.gameURL.GetLink(), gameURL.GetLink())
+		})
+	}
+}
