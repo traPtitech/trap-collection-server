6e054b0ddd381f0453edd14c8da6b23f4458e682
mazrean <shunsuke.w04@gmail.com>
2021-12-18 03:18:44 +0900

    SaveGameURL実装

--- src/repository/gorm2/game_url.go
+++ src/repository/gorm2/game_url.go
@@ -1,5 +1,15 @@
 package gorm2
 
+import (
+	"context"
+	"fmt"
+	"net/url"
+
+	"github.com/google/uuid"
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+)
+
 type GameURL struct {
 	db *DB
 }
@@ -8,4 +18,22 @@ func NewGameURL(db *DB) *GameURL {
 	return &GameURL{
 		db: db,
 	}
+}
+
+func (gu *GameURL) SaveGameURL(ctx context.Context, gameVersionID values.GameVersionID, gameURL *domain.GameURL) error {
+	db, err := gu.db.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db: %w", err)
+	}
+
+	err = db.Create(&GameURLTable{
+		ID:            uuid.UUID(gameURL.GetID()),
+		GameVersionID: uuid.UUID(gameVersionID),
+		URL:           (*url.URL)(gameURL.GetLink()).String(),
+	}).Error
+	if err != nil {
+		return fmt.Errorf("failed to create game url: %w", err)
+	}
+
+	return nil
 }
