5702477922dafebeb8c73f50a5d779910f528f5c
mazrean <shunsuke.w04@gmail.com>
2021-12-18 03:24:53 +0900

    GetGameURL実装

--- src/repository/gorm2/game_url.go
+++ src/repository/gorm2/game_url.go
@@ -2,12 +2,15 @@ package gorm2
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/url"
 
 	"github.com/google/uuid"
 	"github.com/traPtitech/trap-collection-server/src/domain"
 	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/repository"
+	"gorm.io/gorm"
 )
 
 type GameURL struct {
@@ -36,4 +39,32 @@ func (gu *GameURL) SaveGameURL(ctx context.Context, gameVersionID values.GameVer
 	}
 
 	return nil
+}
+
+func (gu *GameURL) GetGameURL(ctx context.Context, gameVersionID values.GameVersionID) (*domain.GameURL, error) {
+	db, err := gu.db.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get db: %w", err)
+	}
+
+	var gameURLTable GameURLTable
+	err = db.
+		Where("game_version_id = ?", uuid.UUID(gameVersionID)).
+		Take(&gameURLTable).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, repository.ErrRecordNotFound
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to get game url: %w", err)
+	}
+
+	urlGameLink, err := url.Parse(gameURLTable.URL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse game url: %w", err)
+	}
+
+	return domain.NewGameURL(
+		values.NewGameURLIDFromUUID(gameURLTable.ID),
+		values.NewGameURLLink(urlGameLink),
+	), nil
 }
