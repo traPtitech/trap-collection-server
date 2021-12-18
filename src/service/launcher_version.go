231a16694b6d4a02a4dfe5a89164b01aa3c4f9c3
mazrean <shunsuke.w04@gmail.com>
2021-12-18 17:52:44 +0900

    LauncherVersionサービス抽象化

---
+++ src/service/launcher_version.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"context"
+
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+)
+
+type LauncherVersion interface {
+	CreateLauncherVersion(ctx context.Context, name values.LauncherVersionName, questionnaireURL values.LauncherVersionQuestionnaireURL) (*domain.LauncherVersion, error)
+	GetLauncherVersions(ctx context.Context) ([]*domain.LauncherVersion, error)
+	GetLauncherVersion(ctx context.Context, id values.LauncherVersionID) (*domain.LauncherVersion, []*domain.Game, error)
+	AddGamesToLauncherVersion(ctx context.Context, id values.LauncherVersionID, gameIDs []values.GameID) error
+	GetLauncherVersionCheckList(ctx context.Context, launcherVersionID values.LauncherVersionID, env *values.LauncherEnvironment) ([]*CheckListItem, error)
+}
+
+type CheckListItem struct {
+	*domain.Game
+	LatestVersion *domain.GameVersion
+	/*
+		LatestURL
+		最新のゲームバージョンのURL
+		nullableなことに注意!
+	*/
+	LatestURL *domain.GameURL
+	/*
+		LatestFile
+		最新のゲームバージョンのファイル
+		nullableなことに注意!
+	*/
+	LatestFile *domain.GameFile
+	// LatestImage nullableなことに注意!
+	LatestImage *domain.GameImage
+	// LatestVideo nullableなことに注意!
+	LatestVideo *domain.GameVideo
+}
