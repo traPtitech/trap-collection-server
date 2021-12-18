360bbf93ccbc1a586feaa537162f6681d8252a52
mazrean <shunsuke.w04@gmail.com>
2021-12-18 17:06:12 +0900

    TestGetGameURL追加

--- src/handler/v1/game_url_test.go
+++ src/handler/v1/game_url_test.go
@@ -159,3 +159,127 @@ func TestPostURL(t *testing.T) {
 		})
 	}
 }
+
+func TestGetGameURL(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockGameURLService := mock.NewMockGameURL(ctrl)
+
+	gameURLHandler := NewGameURL(mockGameURLService)
+
+	type test struct {
+		description       string
+		strGameID         string
+		executeGetGameURL bool
+		gameID            values.GameID
+		gameURL           *domain.GameURL
+		GetGameURLErr     error
+		apiGameURL        string
+		isErr             bool
+		err               error
+		statusCode        int
+	}
+
+	gameID := values.NewGameID()
+	gameURLID := values.NewGameURLID()
+
+	urlLink, err := url.Parse("https://example.com")
+	if err != nil {
+		t.Fatalf("failed to encode image: %v", err)
+	}
+	link := values.NewGameURLLink(urlLink)
+
+	testCases := []test{
+		{
+			description:       "特に問題ないのでエラーなし",
+			strGameID:         uuid.UUID(gameID).String(),
+			executeGetGameURL: true,
+			gameID:            gameID,
+			gameURL: domain.NewGameURL(
+				gameURLID,
+				link,
+			),
+			apiGameURL: (*url.URL)(link).String(),
+		},
+		{
+			description: "gameIDが不正なので400",
+			strGameID:   "invalid",
+			isErr:       true,
+			statusCode:  http.StatusBadRequest,
+		},
+		{
+			description:       "GetGameURLがErrInvalidGameIDなので400",
+			strGameID:         uuid.UUID(gameID).String(),
+			executeGetGameURL: true,
+			gameID:            gameID,
+			GetGameURLErr:     service.ErrInvalidGameID,
+			isErr:             true,
+			statusCode:        http.StatusBadRequest,
+		},
+		{
+			description:       "GetGameURLがErrNoGameVersionなので400",
+			strGameID:         uuid.UUID(gameID).String(),
+			executeGetGameURL: true,
+			gameID:            gameID,
+			GetGameURLErr:     service.ErrNoGameVersion,
+			isErr:             true,
+			statusCode:        http.StatusBadRequest,
+		},
+		{
+			description:       "GetGameURLがErrNoGameURLなので400",
+			strGameID:         uuid.UUID(gameID).String(),
+			executeGetGameURL: true,
+			gameID:            gameID,
+			GetGameURLErr:     service.ErrNoGameURL,
+			isErr:             true,
+			statusCode:        http.StatusBadRequest,
+		},
+		{
+			description:       "GetGameURLがエラーなので500",
+			strGameID:         uuid.UUID(gameID).String(),
+			executeGetGameURL: true,
+			gameID:            gameID,
+			GetGameURLErr:     errors.New("error"),
+			isErr:             true,
+			statusCode:        http.StatusInternalServerError,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			if testCase.executeGetGameURL {
+				mockGameURLService.
+					EXPECT().
+					GetGameURL(gomock.Any(), testCase.gameID).
+					Return(testCase.gameURL, testCase.GetGameURLErr)
+			}
+
+			gameURL, err := gameURLHandler.GetGameURL(testCase.strGameID)
+
+			if testCase.isErr {
+				if testCase.statusCode != 0 {
+					var httpError *echo.HTTPError
+					if errors.As(err, &httpError) {
+						assert.Equal(t, testCase.statusCode, httpError.Code)
+					} else {
+						t.Errorf("error is not *echo.HTTPError")
+					}
+				} else if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, testCase.apiGameURL, gameURL)
+		})
+	}
+}
