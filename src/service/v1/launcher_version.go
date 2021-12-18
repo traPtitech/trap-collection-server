6306696c5c82618e7e2fd824e8819cafced951eb
mazrean <shunsuke.w04@gmail.com>
2021-12-18 20:47:37 +0900

    CreateLauncherVersion実装

--- src/service/v1/launcher_version.go
+++ src/service/v1/launcher_version.go
@@ -1,6 +1,14 @@
 package v1
 
-import "github.com/traPtitech/trap-collection-server/src/repository"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/repository"
+)
 
 type LauncherVersion struct {
 	db                        repository.DB
@@ -18,4 +26,29 @@ func NewLauncherVersion(
 		launcherVersionRepository: launcherVersionRepository,
 		gameRepository:            gameRepository,
 	}
+}
+
+func (lv *LauncherVersion) CreateLauncherVersion(ctx context.Context, name values.LauncherVersionName, questionnaireURL values.LauncherVersionQuestionnaireURL) (*domain.LauncherVersion, error) {
+	var launcherVersion *domain.LauncherVersion
+	if questionnaireURL == nil {
+		launcherVersion = domain.NewLauncherVersionWithoutQuestionnaire(
+			values.NewLauncherVersionID(),
+			name,
+			time.Now(),
+		)
+	} else {
+		launcherVersion = domain.NewLauncherVersionWithQuestionnaire(
+			values.NewLauncherVersionID(),
+			name,
+			questionnaireURL,
+			time.Now(),
+		)
+	}
+
+	err := lv.launcherVersionRepository.CreateLauncherVersion(ctx, launcherVersion)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create launcher version: %w", err)
+	}
+
+	return launcherVersion, nil
 }
