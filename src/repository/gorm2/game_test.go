49f6cdcf3a79e7c99919aca43d609157ff0c35de
mazrean <shunsuke.w04@gmail.com>
2021-12-20 03:06:41 +0900

    TestGetGamesByIDs追加

--- src/repository/gorm2/game_test.go
+++ src/repository/gorm2/game_test.go
@@ -172,3 +172,188 @@ func TestGetGame(t *testing.T) {
 		})
 	}
 }
+
+func TestGetGamesByIDs(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	db, err := testDB.getDB(ctx)
+	if err != nil {
+		t.Fatalf("failed to get db: %+v\n", err)
+	}
+
+	gameRepository := NewGame(testDB)
+
+	type test struct {
+		description     string
+		beforeGameTable []GameTable
+		gameIDs         []values.GameID
+		lockType        repository.LockType
+		games           []*domain.Game
+		isErr           bool
+		err             error
+	}
+
+	gameID1 := values.NewGameID()
+	gameID2 := values.NewGameID()
+	gameID3 := values.NewGameID()
+	gameID4 := values.NewGameID()
+	gameID5 := values.NewGameID()
+	gameID6 := values.NewGameID()
+	gameID7 := values.NewGameID()
+
+	now := time.Now()
+
+	testCases := []test{
+		{
+			description: "特に問題ないのでエラーなし",
+			beforeGameTable: []GameTable{
+				{
+					ID:          uuid.UUID(gameID1),
+					Name:        "test",
+					Description: "test",
+					CreatedAt:   now,
+				},
+			},
+			gameIDs:  []values.GameID{gameID1},
+			lockType: repository.LockTypeNone,
+			games: []*domain.Game{
+				domain.NewGame(
+					gameID1,
+					"test",
+					"test",
+					now,
+				),
+			},
+		},
+		{
+			description:     "ゲームが存在しないので含まない",
+			beforeGameTable: []GameTable{},
+			gameIDs:         []values.GameID{gameID2},
+			lockType:        repository.LockTypeNone,
+			games:           []*domain.Game{},
+		},
+		{
+			description: "ゲームが削除済みなので含まない",
+			beforeGameTable: []GameTable{
+				{
+					ID:          uuid.UUID(gameID3),
+					Name:        "test",
+					Description: "test",
+					CreatedAt:   now,
+					DeletedAt: gorm.DeletedAt{
+						Valid: true,
+						Time:  now,
+					},
+				},
+			},
+			gameIDs:  []values.GameID{gameID3},
+			lockType: repository.LockTypeNone,
+			games:    []*domain.Game{},
+		},
+		{
+			description: "ゲームが複数でも問題なし",
+			beforeGameTable: []GameTable{
+				{
+					ID:          uuid.UUID(gameID4),
+					Name:        "test",
+					Description: "test",
+					CreatedAt:   now,
+				},
+				{
+					ID:          uuid.UUID(gameID5),
+					Name:        "test",
+					Description: "test",
+					CreatedAt:   now,
+				},
+			},
+			gameIDs:  []values.GameID{gameID4, gameID5},
+			lockType: repository.LockTypeNone,
+			games: []*domain.Game{
+				domain.NewGame(
+					gameID4,
+					"test",
+					"test",
+					now,
+				),
+				domain.NewGame(
+					gameID5,
+					"test",
+					"test",
+					now,
+				),
+			},
+		},
+		{
+			description: "含まないゲームが存在してもエラーなし",
+			beforeGameTable: []GameTable{
+				{
+					ID:          uuid.UUID(gameID6),
+					Name:        "test",
+					Description: "test",
+					CreatedAt:   now,
+				},
+				{
+					ID:          uuid.UUID(gameID7),
+					Name:        "test",
+					Description: "test",
+					CreatedAt:   now,
+				},
+			},
+			gameIDs:  []values.GameID{gameID6},
+			lockType: repository.LockTypeNone,
+			games: []*domain.Game{
+				domain.NewGame(
+					gameID6,
+					"test",
+					"test",
+					now,
+				),
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			if testCase.beforeGameTable != nil && len(testCase.beforeGameTable) != 0 {
+				err := db.Create(&testCase.beforeGameTable).Error
+				if err != nil {
+					t.Fatalf("failed to create game table: %+v\n", err)
+				}
+			}
+
+			games, err := gameRepository.GetGamesByIDs(ctx, testCase.gameIDs, testCase.lockType)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			assert.Len(t, games, len(testCase.games))
+
+			gameMap := make(map[values.GameID]*domain.Game, len(testCase.games))
+			for _, game := range games {
+				gameMap[game.GetID()] = game
+			}
+
+			for _, game := range testCase.games {
+				actualGame, ok := gameMap[game.GetID()]
+				assert.True(t, ok)
+
+				assert.Equal(t, game.GetID(), actualGame.GetID())
+				assert.Equal(t, game.GetName(), actualGame.GetName())
+				assert.Equal(t, game.GetDescription(), actualGame.GetDescription())
+				assert.WithinDuration(t, game.GetCreatedAt(), actualGame.GetCreatedAt(), time.Second)
+			}
+		})
+	}
+}
