13221e81941d00e8b3de0332ca1ae52867c23987
mazrean <shunsuke.w04@gmail.com>
2021-12-20 22:42:44 +0900

    TestGetCheckList追加

--- src/handler/v1/launcher_version_test.go
+++ src/handler/v1/launcher_version_test.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"errors"
 	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"testing"
 	"time"
@@ -803,6 +804,550 @@ func TestPostGameToVersion(t *testing.T) {
 
 			for i, expect := range testCase.expect.Games {
 				assert.Equal(t, expect, launcherVersion.Games[i])
+			}
+		})
+	}
+}
+
+func TestGetCheckList(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockLauncherVersionService := mock.NewMockLauncherVersion(ctrl)
+
+	launcherVersionHandler := NewLauncherVersion(mockLauncherVersionService)
+
+	type test struct {
+		description                        string
+		operatingSystem                    string
+		launcherVersion                    *domain.LauncherVersion
+		executeGetLauncherVersionCheckList bool
+		checkListItems                     []*service.CheckListItem
+		GetLauncherVersionCheckListErr     error
+		expect                             []*openapi.CheckItem
+		isErr                              bool
+		err                                error
+		statusCode                         int
+	}
+
+	launcherVersionID1 := values.NewLauncherVersionID()
+
+	gameID1 := values.NewGameID()
+	gameID2 := values.NewGameID()
+
+	urlLink, err := url.Parse("https://example.com")
+	if err != nil {
+		t.Fatalf("failed to encode image: %v", err)
+	}
+
+	now := time.Now()
+
+	testCases := []test{
+		{
+			description:     "エラーなしなので問題なし",
+			operatingSystem: "win32",
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				now,
+			),
+			executeGetLauncherVersionCheckList: true,
+			checkListItems: []*service.CheckListItem{
+				{
+					Game: domain.NewGame(
+						gameID1,
+						values.NewGameName("game1"),
+						values.NewGameDescription("description1"),
+						now,
+					),
+					LatestFile: domain.NewGameFile(
+						values.NewGameFileID(),
+						values.GameFileTypeJar,
+						values.NewGameFileEntryPoint("main.jar"),
+						values.NewGameFileHashFromBytes([]byte("hash1")),
+						now,
+					),
+					LatestImage: domain.NewGameImage(
+						values.NewGameImageID(),
+						values.GameImageTypePng,
+						now,
+					),
+					LatestVideo: domain.NewGameVideo(
+						values.NewGameVideoID(),
+						values.GameVideoTypeMp4,
+						now,
+					),
+				},
+			},
+			expect: []*openapi.CheckItem{
+				{
+					Id:             uuid.UUID(gameID1).String(),
+					Md5:            "6861736831",
+					Type:           "jar",
+					BodyUpdatedAt:  now,
+					ImgUpdatedAt:   now,
+					MovieUpdatedAt: now,
+				},
+			},
+		},
+		{
+			description:     "macでも問題なし",
+			operatingSystem: "darwin",
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				now,
+			),
+			executeGetLauncherVersionCheckList: true,
+			checkListItems: []*service.CheckListItem{
+				{
+					Game: domain.NewGame(
+						gameID1,
+						values.NewGameName("game1"),
+						values.NewGameDescription("description1"),
+						now,
+					),
+					LatestFile: domain.NewGameFile(
+						values.NewGameFileID(),
+						values.GameFileTypeJar,
+						values.NewGameFileEntryPoint("main.jar"),
+						values.NewGameFileHashFromBytes([]byte("hash1")),
+						now,
+					),
+					LatestImage: domain.NewGameImage(
+						values.NewGameImageID(),
+						values.GameImageTypePng,
+						now,
+					),
+					LatestVideo: domain.NewGameVideo(
+						values.NewGameVideoID(),
+						values.GameVideoTypeMp4,
+						now,
+					),
+				},
+			},
+			expect: []*openapi.CheckItem{
+				{
+					Id:             uuid.UUID(gameID1).String(),
+					Md5:            "6861736831",
+					Type:           "jar",
+					BodyUpdatedAt:  now,
+					ImgUpdatedAt:   now,
+					MovieUpdatedAt: now,
+				},
+			},
+		},
+		{
+			description:     "macでもwindowsでもないので400",
+			operatingSystem: "invalid",
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				now,
+			),
+			isErr:      true,
+			statusCode: http.StatusBadRequest,
+		},
+		{
+			description:     "ランチャーバージョンが設定されていないので500",
+			operatingSystem: "win32",
+			isErr:           true,
+			statusCode:      http.StatusInternalServerError,
+		},
+		{
+			description:     "ランチャーバージョンが存在しないので400",
+			operatingSystem: "win32",
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				now,
+			),
+			executeGetLauncherVersionCheckList: true,
+			GetLauncherVersionCheckListErr:     service.ErrNoLauncherVersion,
+			isErr:                              true,
+			statusCode:                         http.StatusBadRequest,
+		},
+		{
+			description:     "GetLauncherVersionCheckListがエラーなので500",
+			operatingSystem: "win32",
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				now,
+			),
+			executeGetLauncherVersionCheckList: true,
+			GetLauncherVersionCheckListErr:     errors.New("error"),
+			isErr:                              true,
+			statusCode:                         http.StatusInternalServerError,
+		},
+		{
+			description:     "windows用ファイルでも問題なし",
+			operatingSystem: "win32",
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				now,
+			),
+			executeGetLauncherVersionCheckList: true,
+			checkListItems: []*service.CheckListItem{
+				{
+					Game: domain.NewGame(
+						gameID1,
+						values.NewGameName("game1"),
+						values.NewGameDescription("description1"),
+						now,
+					),
+					LatestFile: domain.NewGameFile(
+						values.NewGameFileID(),
+						values.GameFileTypeWindows,
+						values.NewGameFileEntryPoint("main.exe"),
+						values.NewGameFileHashFromBytes([]byte("hash1")),
+						now,
+					),
+					LatestImage: domain.NewGameImage(
+						values.NewGameImageID(),
+						values.GameImageTypePng,
+						now,
+					),
+					LatestVideo: domain.NewGameVideo(
+						values.NewGameVideoID(),
+						values.GameVideoTypeMp4,
+						now,
+					),
+				},
+			},
+			expect: []*openapi.CheckItem{
+				{
+					Id:             uuid.UUID(gameID1).String(),
+					Md5:            "6861736831",
+					Type:           "windows",
+					BodyUpdatedAt:  now,
+					ImgUpdatedAt:   now,
+					MovieUpdatedAt: now,
+				},
+			},
+		},
+		{
+			description:     "mac用ファイルでも問題なし",
+			operatingSystem: "darwin",
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				now,
+			),
+			executeGetLauncherVersionCheckList: true,
+			checkListItems: []*service.CheckListItem{
+				{
+					Game: domain.NewGame(
+						gameID1,
+						values.NewGameName("game1"),
+						values.NewGameDescription("description1"),
+						now,
+					),
+					LatestFile: domain.NewGameFile(
+						values.NewGameFileID(),
+						values.GameFileTypeMac,
+						values.NewGameFileEntryPoint("main.app"),
+						values.NewGameFileHashFromBytes([]byte("hash1")),
+						now,
+					),
+					LatestImage: domain.NewGameImage(
+						values.NewGameImageID(),
+						values.GameImageTypePng,
+						now,
+					),
+					LatestVideo: domain.NewGameVideo(
+						values.NewGameVideoID(),
+						values.GameVideoTypeMp4,
+						now,
+					),
+				},
+			},
+			expect: []*openapi.CheckItem{
+				{
+					Id:             uuid.UUID(gameID1).String(),
+					Md5:            "6861736831",
+					Type:           "mac",
+					BodyUpdatedAt:  now,
+					ImgUpdatedAt:   now,
+					MovieUpdatedAt: now,
+				},
+			},
+		},
+		{
+			description:     "urlでも問題なし",
+			operatingSystem: "win32",
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				now,
+			),
+			executeGetLauncherVersionCheckList: true,
+			checkListItems: []*service.CheckListItem{
+				{
+					Game: domain.NewGame(
+						gameID1,
+						values.NewGameName("game1"),
+						values.NewGameDescription("description1"),
+						now,
+					),
+					LatestURL: domain.NewGameURL(
+						values.NewGameURLID(),
+						values.NewGameURLLink(urlLink),
+						now,
+					),
+					LatestImage: domain.NewGameImage(
+						values.NewGameImageID(),
+						values.GameImageTypePng,
+						now,
+					),
+					LatestVideo: domain.NewGameVideo(
+						values.NewGameVideoID(),
+						values.GameVideoTypeMp4,
+						now,
+					),
+				},
+			},
+			expect: []*openapi.CheckItem{
+				{
+					Id:             uuid.UUID(gameID1).String(),
+					Md5:            "",
+					Type:           "url",
+					BodyUpdatedAt:  now,
+					ImgUpdatedAt:   now,
+					MovieUpdatedAt: now,
+				},
+			},
+		},
+		{
+			description:     "ファイルの種類が誤っている場合は無視",
+			operatingSystem: "win32",
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				now,
+			),
+			executeGetLauncherVersionCheckList: true,
+			checkListItems: []*service.CheckListItem{
+				{
+					Game: domain.NewGame(
+						gameID1,
+						values.NewGameName("game1"),
+						values.NewGameDescription("description1"),
+						now,
+					),
+					LatestFile: domain.NewGameFile(
+						values.NewGameFileID(),
+						100,
+						values.NewGameFileEntryPoint("main.jar"),
+						values.NewGameFileHashFromBytes([]byte("hash1")),
+						now,
+					),
+					LatestImage: domain.NewGameImage(
+						values.NewGameImageID(),
+						values.GameImageTypePng,
+						now,
+					),
+					LatestVideo: domain.NewGameVideo(
+						values.NewGameVideoID(),
+						values.GameVideoTypeMp4,
+						now,
+					),
+				},
+			},
+			expect: []*openapi.CheckItem{},
+		},
+		{
+			description:     "エラーなしなので問題なし",
+			operatingSystem: "win32",
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				now,
+			),
+			executeGetLauncherVersionCheckList: true,
+			checkListItems: []*service.CheckListItem{
+				{
+					Game: domain.NewGame(
+						gameID1,
+						values.NewGameName("game1"),
+						values.NewGameDescription("description1"),
+						now,
+					),
+					LatestFile: domain.NewGameFile(
+						values.NewGameFileID(),
+						values.GameFileTypeJar,
+						values.NewGameFileEntryPoint("main.jar"),
+						values.NewGameFileHashFromBytes([]byte("hash1")),
+						now,
+					),
+					LatestImage: domain.NewGameImage(
+						values.NewGameImageID(),
+						values.GameImageTypePng,
+						now,
+					),
+					LatestVideo: domain.NewGameVideo(
+						values.NewGameVideoID(),
+						values.GameVideoTypeMp4,
+						now,
+					),
+				},
+			},
+			expect: []*openapi.CheckItem{
+				{
+					Id:             uuid.UUID(gameID1).String(),
+					Md5:            "6861736831",
+					Type:           "jar",
+					BodyUpdatedAt:  now,
+					ImgUpdatedAt:   now,
+					MovieUpdatedAt: now,
+				},
+			},
+		},
+		{
+			description:     "ゲームが存在しなくてもエラーなし",
+			operatingSystem: "win32",
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				now,
+			),
+			executeGetLauncherVersionCheckList: true,
+			checkListItems:                     []*service.CheckListItem{},
+			expect:                             []*openapi.CheckItem{},
+		},
+		{
+			description:     "ゲームが複数でもエラーなし",
+			operatingSystem: "win32",
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				now,
+			),
+			executeGetLauncherVersionCheckList: true,
+			checkListItems: []*service.CheckListItem{
+				{
+					Game: domain.NewGame(
+						gameID1,
+						values.NewGameName("game1"),
+						values.NewGameDescription("description1"),
+						now,
+					),
+					LatestFile: domain.NewGameFile(
+						values.NewGameFileID(),
+						values.GameFileTypeJar,
+						values.NewGameFileEntryPoint("main.jar"),
+						values.NewGameFileHashFromBytes([]byte("hash1")),
+						now,
+					),
+					LatestImage: domain.NewGameImage(
+						values.NewGameImageID(),
+						values.GameImageTypePng,
+						now,
+					),
+					LatestVideo: domain.NewGameVideo(
+						values.NewGameVideoID(),
+						values.GameVideoTypeMp4,
+						now,
+					),
+				},
+				{
+					Game: domain.NewGame(
+						gameID2,
+						values.NewGameName("game2"),
+						values.NewGameDescription("description2"),
+						now,
+					),
+					LatestFile: domain.NewGameFile(
+						values.NewGameFileID(),
+						values.GameFileTypeJar,
+						values.NewGameFileEntryPoint("main.jar"),
+						values.NewGameFileHashFromBytes([]byte("hash1")),
+						now,
+					),
+					LatestImage: domain.NewGameImage(
+						values.NewGameImageID(),
+						values.GameImageTypePng,
+						now,
+					),
+					LatestVideo: domain.NewGameVideo(
+						values.NewGameVideoID(),
+						values.GameVideoTypeMp4,
+						now,
+					),
+				},
+			},
+			expect: []*openapi.CheckItem{
+				{
+					Id:             uuid.UUID(gameID1).String(),
+					Md5:            "6861736831",
+					Type:           "jar",
+					BodyUpdatedAt:  now,
+					ImgUpdatedAt:   now,
+					MovieUpdatedAt: now,
+				},
+				{
+					Id:             uuid.UUID(gameID2).String(),
+					Md5:            "6861736831",
+					Type:           "jar",
+					BodyUpdatedAt:  now,
+					ImgUpdatedAt:   now,
+					MovieUpdatedAt: now,
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, "/", nil)
+			rec := httptest.NewRecorder()
+			c := echo.New().NewContext(req, rec)
+
+			if testCase.launcherVersion != nil {
+				c.Set(launcherVersionKey, testCase.launcherVersion)
+			}
+
+			if testCase.executeGetLauncherVersionCheckList {
+				mockLauncherVersionService.
+					EXPECT().
+					GetLauncherVersionCheckList(gomock.Any(), testCase.launcherVersion.GetID(), gomock.Any()).
+					Return(testCase.checkListItems, testCase.GetLauncherVersionCheckListErr)
+			}
+
+			checkList, err := launcherVersionHandler.GetCheckList(testCase.operatingSystem, c)
+
+			if testCase.isErr {
+				if testCase.statusCode != 0 {
+					var httpError *echo.HTTPError
+					if errors.As(err, &httpError) {
+						assert.Equal(t, testCase.statusCode, httpError.Code)
+					} else {
+						t.Errorf("error is not *echo.HTTPError")
+					}
+				} else if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			assert.Len(t, checkList, len(testCase.expect))
+
+			for i, expect := range testCase.expect {
+				assert.Equal(t, expect.Id, checkList[i].Id)
+				assert.Equal(t, expect.Md5, checkList[i].Md5)
+				assert.Equal(t, expect.Type, checkList[i].Type)
+				assert.Equal(t, expect.BodyUpdatedAt, checkList[i].BodyUpdatedAt)
+				assert.Equal(t, expect.ImgUpdatedAt, checkList[i].ImgUpdatedAt)
+				assert.Equal(t, expect.MovieUpdatedAt, checkList[i].MovieUpdatedAt)
 			}
 		})
 	}
