b5a025a78c17a6042a85126268883a1f43e4d174
mazrean <shunsuke.w04@gmail.com>
2021-12-20 21:30:57 +0900

    TestGetVersion追加

--- src/handler/v1/launcher_version_test.go
+++ src/handler/v1/launcher_version_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/traPtitech/trap-collection-server/openapi"
 	"github.com/traPtitech/trap-collection-server/src/domain"
 	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/service"
 	"github.com/traPtitech/trap-collection-server/src/service/mock"
 )
 
@@ -308,6 +309,237 @@ func TestPostVersion(t *testing.T) {
 			}
 
 			assert.Equal(t, *testCase.expect, *launcherVersion)
+		})
+	}
+}
+
+func TestGetVersion(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockLauncherVersionService := mock.NewMockLauncherVersion(ctrl)
+
+	launcherVersionHandler := NewLauncherVersion(mockLauncherVersionService)
+
+	type test struct {
+		description               string
+		strLauncherVersionID      string
+		executeGetLauncherVersion bool
+		launcherVersionID         values.LauncherVersionID
+		launcherVersion           *domain.LauncherVersion
+		games                     []*domain.Game
+		GetLauncherVersionErr     error
+		expect                    *openapi.VersionDetails
+		isErr                     bool
+		err                       error
+		statusCode                int
+	}
+
+	launcherVersionID1 := values.NewLauncherVersionID()
+
+	gameID1 := values.NewGameID()
+	gameID2 := values.NewGameID()
+
+	urlLink, err := url.Parse("https://example.com")
+	if err != nil {
+		t.Fatalf("failed to encode image: %v", err)
+	}
+
+	now := time.Now()
+
+	testCases := []test{
+		{
+			description:               "エラーなしなので問題なし",
+			strLauncherVersionID:      uuid.UUID(launcherVersionID1).String(),
+			executeGetLauncherVersion: true,
+			launcherVersionID:         launcherVersionID1,
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				now,
+			),
+			games: []*domain.Game{
+				domain.NewGame(
+					gameID1,
+					values.NewGameName("game1"),
+					values.NewGameDescription("description1"),
+					now,
+				),
+			},
+			expect: &openapi.VersionDetails{
+				Id:        uuid.UUID(launcherVersionID1).String(),
+				Name:      "2020.1.1",
+				AnkeTo:    "",
+				CreatedAt: now,
+				Games: []openapi.GameMeta{
+					{
+						Id:   uuid.UUID(gameID1).String(),
+						Name: "game1",
+					},
+				},
+			},
+		},
+		{
+			description:          "ランチャーバージョンIDが不正なので400",
+			strLauncherVersionID: "invalid",
+			isErr:                true,
+			statusCode:           http.StatusBadRequest,
+		},
+		{
+			description:               "ランチャーバージョンが存在しないので400",
+			strLauncherVersionID:      uuid.UUID(launcherVersionID1).String(),
+			executeGetLauncherVersion: true,
+			launcherVersionID:         launcherVersionID1,
+			GetLauncherVersionErr:     service.ErrNoLauncherVersion,
+			isErr:                     true,
+			statusCode:                http.StatusBadRequest,
+		},
+		{
+			description:               "GetLauncherVersionがエラーなので500",
+			strLauncherVersionID:      uuid.UUID(launcherVersionID1).String(),
+			executeGetLauncherVersion: true,
+			launcherVersionID:         launcherVersionID1,
+			GetLauncherVersionErr:     errors.New("error"),
+			isErr:                     true,
+			statusCode:                http.StatusInternalServerError,
+		},
+		{
+			description:               "アンケートありでも問題なし",
+			strLauncherVersionID:      uuid.UUID(launcherVersionID1).String(),
+			executeGetLauncherVersion: true,
+			launcherVersionID:         launcherVersionID1,
+			launcherVersion: domain.NewLauncherVersionWithQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				values.NewLauncherVersionQuestionnaireURL(urlLink),
+				now,
+			),
+			games: []*domain.Game{
+				domain.NewGame(
+					gameID1,
+					values.NewGameName("game1"),
+					values.NewGameDescription("description1"),
+					now,
+				),
+			},
+			expect: &openapi.VersionDetails{
+				Id:        uuid.UUID(launcherVersionID1).String(),
+				Name:      "2020.1.1",
+				AnkeTo:    "https://example.com",
+				CreatedAt: now,
+				Games: []openapi.GameMeta{
+					{
+						Id:   uuid.UUID(gameID1).String(),
+						Name: "game1",
+					},
+				},
+			},
+		},
+		{
+			description:               "ゲームがなくても問題なし",
+			strLauncherVersionID:      uuid.UUID(launcherVersionID1).String(),
+			executeGetLauncherVersion: true,
+			launcherVersionID:         launcherVersionID1,
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				now,
+			),
+			games: []*domain.Game{},
+			expect: &openapi.VersionDetails{
+				Id:        uuid.UUID(launcherVersionID1).String(),
+				Name:      "2020.1.1",
+				AnkeTo:    "",
+				CreatedAt: now,
+				Games:     []openapi.GameMeta{},
+			},
+		},
+		{
+			description:               "ゲームが複数でも問題なし",
+			strLauncherVersionID:      uuid.UUID(launcherVersionID1).String(),
+			executeGetLauncherVersion: true,
+			launcherVersionID:         launcherVersionID1,
+			launcherVersion: domain.NewLauncherVersionWithoutQuestionnaire(
+				launcherVersionID1,
+				values.NewLauncherVersionName("2020.1.1"),
+				now,
+			),
+			games: []*domain.Game{
+				domain.NewGame(
+					gameID1,
+					values.NewGameName("game1"),
+					values.NewGameDescription("description1"),
+					now,
+				),
+				domain.NewGame(
+					gameID2,
+					values.NewGameName("game2"),
+					values.NewGameDescription("description2"),
+					now,
+				),
+			},
+			expect: &openapi.VersionDetails{
+				Id:        uuid.UUID(launcherVersionID1).String(),
+				Name:      "2020.1.1",
+				AnkeTo:    "",
+				CreatedAt: now,
+				Games: []openapi.GameMeta{
+					{
+						Id:   uuid.UUID(gameID1).String(),
+						Name: "game1",
+					},
+					{
+						Id:   uuid.UUID(gameID2).String(),
+						Name: "game2",
+					},
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			if testCase.executeGetLauncherVersion {
+				mockLauncherVersionService.
+					EXPECT().
+					GetLauncherVersion(gomock.Any(), testCase.launcherVersionID).
+					Return(testCase.launcherVersion, testCase.games, testCase.GetLauncherVersionErr)
+			}
+
+			launcherVersion, err := launcherVersionHandler.GetVersion(testCase.strLauncherVersionID)
+
+			if testCase.isErr {
+				if testCase.statusCode != 0 {
+					var httpError *echo.HTTPError
+					if errors.As(err, &httpError) {
+						assert.Equal(t, testCase.statusCode, httpError.Code)
+					} else {
+						t.Errorf("error is not *echo.HTTPError")
+					}
+				} else if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, testCase.expect.Id, launcherVersion.Id)
+			assert.Equal(t, testCase.expect.Name, launcherVersion.Name)
+			assert.Equal(t, testCase.expect.AnkeTo, launcherVersion.AnkeTo)
+			assert.WithinDuration(t, testCase.expect.CreatedAt, launcherVersion.CreatedAt, time.Second)
+
+			assert.Len(t, launcherVersion.Games, len(testCase.expect.Games))
+
+			for i, expect := range testCase.expect.Games {
+				assert.Equal(t, expect, launcherVersion.Games[i])
+			}
 		})
 	}
 }
