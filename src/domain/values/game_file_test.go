981956c02d7a721c80ae3a91eb4b38ad85b125e8
mazrean <shunsuke.w04@gmail.com>
2021-12-24 17:16:39 +0900

    entryPointにvalidate追加

--- src/domain/values/game_file_test.go
+++ src/domain/values/game_file_test.go
@@ -1,6 +1,7 @@
 package values
 
 import (
+	"errors"
 	"strings"
 	"testing"
 
@@ -17,4 +18,44 @@ func TestNewGameFileHash(t *testing.T) {
 	}
 
 	assert.Equal(t, "a32354ed11d6d65a78cbedac5d55e35f", hash.String())
+}
+
+func TestGameFileEntryPointValidate(t *testing.T) {
+	t.Parallel()
+
+	type test struct {
+		description string
+		entryPoint  GameFileEntryPoint
+		isErr       bool
+		err         error
+	}
+
+	testCases := []test{
+		{
+			description: "特に問題ないのでエラーなし",
+			entryPoint:  "main.jar",
+		},
+		{
+			description: "空文字列はエラー",
+			entryPoint:  "",
+			isErr:       true,
+			err:         ErrGameFileEntryPointEmpty,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			err := testCase.entryPoint.Validate()
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
