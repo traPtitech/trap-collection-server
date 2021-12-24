7a7433673c0faea941d52021b3998d10835a672d
mazrean <shunsuke.w04@gmail.com>
2021-12-24 20:41:57 +0900

    gormにメトリクス設定

--- src/repository/gorm2/db.go
+++ src/repository/gorm2/db.go
@@ -14,6 +14,7 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 	"gorm.io/gorm/logger"
+	"gorm.io/plugin/prometheus"
 )
 
 type DB struct {
@@ -71,6 +72,17 @@ func NewDB(isProduction common.IsProduction) (*DB, error) {
 	err = db.AutoMigrate(tables...)
 	if err != nil {
 		return nil, fmt.Errorf("failed to auto migrate: %w", err)
+	}
+
+	err = db.Use(prometheus.New(prometheus.Config{
+		DBName:          "trap_collection",
+		RefreshInterval: 15,
+		MetricsCollector: []prometheus.MetricsCollector{
+			&MetricsCollector{},
+		},
+	}))
+	if err != nil {
+		return nil, fmt.Errorf("failed to use prometheus plugin: %w", err)
 	}
 
 	return &DB{
