7d2a803aa6d3fefb168aff75ecc5d0baee55f313
mazrean <shunsuke.w04@gmail.com>
2022-03-29 03:39:27 +0900

    gorm2対応

--- src/repository/gorm2/db_test.go
+++ src/repository/gorm2/db_test.go
@@ -3,13 +3,15 @@ package gorm2
 import (
 	"os"
 	"testing"
+
+	v1 "github.com/traPtitech/trap-collection-server/src/config/v1"
 )
 
 var testDB *DB
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = NewDB(true)
+	testDB, err = NewDB(v1.NewApp(), v1.NewRepositoryGorm2())
 	if err != nil {
 		panic(err)
 	}
