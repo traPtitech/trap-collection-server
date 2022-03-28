4ed928d8752d8e390fca21c92c822ca39cd07797
mazrean <shunsuke.w04@gmail.com>
2022-03-29 00:27:11 +0900

    cache向け設定v1

---
+++ src/config/v1/cache.go
@@ -0,0 +1,13 @@
+package v1
+
+import "time"
+
+type CacheRistretto struct{}
+
+func NewCacheRistretto() *CacheRistretto {
+	return &CacheRistretto{}
+}
+
+func (*CacheRistretto) ActiveUsersTTL() (time.Duration, error) {
+	return time.Hour, nil
+}
