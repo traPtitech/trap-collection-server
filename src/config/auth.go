79a090b63fd11eeb385f3418d68b89f9675336a5
mazrean <shunsuke.w04@gmail.com>
2022-03-28 23:51:23 +0900

    auth向け設定抽象化

---
+++ src/config/auth.go
@@ -0,0 +1,13 @@
+package config
+
+//go:generate go run github.com/golang/mock/mockgen@latest -source=$GOFILE -destination=mock/${GOFILE} -package=mock
+
+import (
+	"net/http"
+	"net/url"
+)
+
+type AuthTraQ interface {
+	HTTPClient() (*http.Client, error)
+	BaseURL() (*url.URL, error)
+}
