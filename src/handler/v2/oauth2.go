69bf4018e926a596ff65b0641292470c93d4693f
mazrean <shunsuke.w04@gmail.com>
2022-09-24 07:09:59 +0900

    GetCallback実装

--- src/handler/v2/oauth2.go
+++ src/handler/v2/oauth2.go
@@ -1,11 +1,17 @@
 package v2
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"net/http"
 	"net/url"
 
 	"github.com/labstack/echo/v4"
 	"github.com/traPtitech/trap-collection-server/src/config"
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/handler/v2/openapi"
 	"github.com/traPtitech/trap-collection-server/src/service"
 )
 
@@ -33,13 +39,56 @@ func NewOAuth2(conf config.Handler, session *Session, oidcService service.OIDCV2
 // メソッドとして実装予定だが、未実装のもの
 // TODO: 実装
 type oauth2Unimplemented interface {
-	// traQのOAuth 2.0のコールバック
-	// (GET /oauth2/callback)
-	GetCallback(ctx echo.Context, params openapi.GetCallbackParams) error
 	// OAuth 2.0のCode Verifierなどのセッションへの設定とtraQへのリダイレクト
 	// (GET /oauth2/code)
 	GetCode(ctx echo.Context) error
 	// traP Collectionの管理画面からのログアウト
 	// (POST /oauth2/logout)
 	PostLogout(ctx echo.Context) error
+}
+
+// traQのOAuth 2.0のコールバック
+// (GET /oauth2/callback)
+func (oauth2 *OAuth2) GetCallback(c echo.Context, params openapi.GetCallbackParams) error {
+	code := values.NewOIDCAuthorizationCode(params.Code)
+
+	session, err := oauth2.session.get(c)
+	if err != nil {
+		log.Printf("error: failed to get session: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get session")
+	}
+
+	strCodeVerifier, err := oauth2.session.getCodeVerifier(session)
+	if errors.Is(err, ErrNoValue) {
+		return echo.NewHTTPError(http.StatusBadRequest, "no code verifier")
+	}
+	if err != nil {
+		log.Printf("error: failed to get code verifier: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get code verifier")
+	}
+	codeVerifier := values.NewOIDCCodeVerifierFromString(strCodeVerifier)
+
+	authState := domain.NewOIDCAuthState(
+		values.OIDCCodeChallengeMethodSha256,
+		codeVerifier,
+	)
+
+	authSession, err := oauth2.oidcService.Callback(c.Request().Context(), authState, code)
+	if errors.Is(err, service.ErrInvalidAuthStateOrCode) {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid auth state or code")
+	}
+	if err != nil {
+		log.Printf("error: failed to callback: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to callback")
+	}
+
+	oauth2.session.setAuthSession(session, authSession)
+
+	err = oauth2.session.save(c, session)
+	if err != nil {
+		log.Printf("error: failed to save session: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to save session")
+	}
+
+	return c.NoContent(http.StatusOK)
 }
