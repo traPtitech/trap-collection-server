0132792c23c693c63ba6c4eb14dcc66fee33fe07
mazrean <shunsuke.w04@gmail.com>
2022-09-24 07:21:13 +0900

    GetCodeのテスト追加

--- src/handler/v2/oauth2_test.go
+++ src/handler/v2/oauth2_test.go
@@ -198,3 +198,209 @@ func TestGetCallback(t *testing.T) {
 		})
 	}
 }
+
+func TestGetCode(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockOIDCService := mock.NewMockOIDCV2(ctrl)
+
+	baseURL, err := url.Parse("https://q.trap.jp/api/v3")
+	if err != nil {
+		t.Errorf("Error parsing base URL: %v", err)
+	}
+
+	mockConf := mockConfig.NewMockHandler(ctrl)
+	mockConf.
+		EXPECT().
+		TraqBaseURL().
+		Return(baseURL, nil)
+	mockConf.
+		EXPECT().
+		SessionKey().
+		Return("key", nil)
+	mockConf.
+		EXPECT().
+		SessionSecret().
+		Return("secret", nil)
+	sess, err := common.NewSession(mockConf)
+	if err != nil {
+		t.Fatalf("failed to create session: %v", err)
+		return
+	}
+	session, err := NewSession(sess)
+	if err != nil {
+		t.Fatalf("failed to create session: %v", err)
+		return
+	}
+
+	oauth, err := NewOAuth2(mockConf, session, mockOIDCService)
+	if err != nil {
+		t.Fatalf("failed to create oauth: %v", err)
+	}
+
+	type test struct {
+		description         string
+		client              *domain.OIDCClient
+		authState           *domain.OIDCAuthState
+		AuthorizeErr        error
+		sessionExist        bool
+		scheme              string
+		host                string
+		path                string
+		clientID            string
+		codeChallenge       string
+		codeChallengeMethod string
+		responseType        string
+		isErr               bool
+		err                 error
+		statusCode          int
+	}
+
+	codeVerifier, err := values.NewOIDCCodeVerifier()
+	if err != nil {
+		t.Fatalf("failed to create codeVerifier: %v", err)
+	}
+	codeChallenge, err := codeVerifier.GetCodeChallenge(values.OIDCCodeChallengeMethodSha256)
+	if err != nil {
+		t.Fatalf("failed to get codeChallenge:%v", err)
+	}
+
+	testCases := []test{
+		{
+			description: "特に問題ないのでエラーなし",
+			client: domain.NewOIDCClient(
+				"clientID",
+			),
+			authState: domain.NewOIDCAuthState(
+				values.OIDCCodeChallengeMethodSha256,
+				codeVerifier,
+			),
+			sessionExist:        true,
+			isErr:               true,
+			statusCode:          http.StatusSeeOther,
+			scheme:              "https",
+			host:                "q.trap.jp",
+			path:                "/api/v3/oauth2/authorize",
+			clientID:            "clientID",
+			codeChallenge:       string(codeChallenge),
+			codeChallengeMethod: "S256",
+			responseType:        "code",
+		},
+		{
+			description:  "Authorizeがエラーなので500",
+			AuthorizeErr: errors.New("error"),
+			isErr:        true,
+			statusCode:   http.StatusInternalServerError,
+		},
+		{
+			// 実際には発生し得ないが念の為テスト項目に入れている
+			description: "CodeChallengeMethodがSHA256以外なので500",
+			client: domain.NewOIDCClient(
+				"clientID",
+			),
+			authState: domain.NewOIDCAuthState(
+				100,
+				codeVerifier,
+			),
+			isErr:      true,
+			statusCode: http.StatusInternalServerError,
+		},
+		{
+			description: "sessionが存在しなくてもエラーなし",
+			client: domain.NewOIDCClient(
+				"clientID",
+			),
+			authState: domain.NewOIDCAuthState(
+				values.OIDCCodeChallengeMethodSha256,
+				codeVerifier,
+			),
+			sessionExist:        false,
+			isErr:               true,
+			statusCode:          http.StatusSeeOther,
+			scheme:              "https",
+			host:                "q.trap.jp",
+			path:                "/api/v3/oauth2/authorize",
+			clientID:            "clientID",
+			codeChallenge:       string(codeChallenge),
+			codeChallengeMethod: "S256",
+			responseType:        "code",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			e := echo.New()
+			req := httptest.NewRequest(http.MethodPost, "/oauth2/callback", nil)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+
+			if testCase.sessionExist {
+				sess, err := session.New(req)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				err = sess.Save(req, rec)
+				if err != nil {
+					t.Fatalf("failed to save session: %v", err)
+				}
+
+				setCookieHeader(c)
+			}
+
+			mockOIDCService.
+				EXPECT().
+				GenerateAuthState(gomock.Any()).
+				Return(testCase.client, testCase.authState, testCase.AuthorizeErr)
+
+			err := oauth.GetCode(c)
+
+			if testCase.isErr {
+				if testCase.statusCode != 0 {
+					var httpError *echo.HTTPError
+					if errors.As(err, &httpError) {
+						assert.Equal(t, testCase.statusCode, httpError.Code)
+
+						strRedirectURL := c.Response().Header().Get("Location")
+						redirectURL, err := url.Parse(strRedirectURL)
+						if err != nil {
+							t.Fatalf("failed to parse redirectURL: %v", err)
+						}
+
+						assert.Equal(t, testCase.scheme, redirectURL.Scheme)
+						assert.Equal(t, testCase.host, redirectURL.Host)
+						assert.Equal(t, testCase.path, redirectURL.Path)
+						assert.Equal(t, testCase.clientID, redirectURL.Query().Get("client_id"))
+						assert.Equal(t, testCase.codeChallenge, redirectURL.Query().Get("code_challenge"))
+						assert.Equal(t, testCase.codeChallengeMethod, redirectURL.Query().Get("code_challenge_method"))
+						assert.Equal(t, testCase.responseType, redirectURL.Query().Get("response_type"))
+					} else {
+						t.Errorf("error is not *echo.HTTPError")
+					}
+				} else if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			setCookieHeader(c)
+
+			sess, err := session.get(c)
+			if err != nil {
+				t.Fatalf("failed to get session: %v", err)
+			}
+
+			_, err = session.getCodeVerifier(sess)
+			assert.NoError(t, err)
+		})
+	}
+}
