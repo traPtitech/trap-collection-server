5801ee0e729aa418d198c517ba007563effe81ee
mazrean <shunsuke.w04@gmail.com>
2022-09-24 08:23:41 +0900

    service周りのv2設定

--- src/wire/service.go
+++ src/wire/service.go
@@ -5,45 +5,58 @@ package wire
 import (
 	"github.com/google/wire"
 	"github.com/traPtitech/trap-collection-server/src/service"
-	v1 "github.com/traPtitech/trap-collection-server/src/service/v1"
+	"github.com/traPtitech/trap-collection-server/src/service/v1"
+	"github.com/traPtitech/trap-collection-server/src/service/v2"
 )
 
-var serviceSet = wire.NewSet(
-	wire.Bind(new(service.AdministratorAuth), new(*v1.AdministratorAuth)),
-	v1.NewAdministratorAuth,
+var (
+	serviceSet = wire.NewSet(
+		serviceV1Set,
+		serviceV2Set,
+	)
+	serviceV1Set = wire.NewSet(
+		wire.Bind(new(service.AdministratorAuth), new(*v1.AdministratorAuth)),
+		v1.NewAdministratorAuth,
 
-	wire.Bind(new(service.GameAuth), new(*v1.GameAuth)),
-	v1.NewGameAuth,
+		wire.Bind(new(service.GameAuth), new(*v1.GameAuth)),
+		v1.NewGameAuth,
 
-	wire.Bind(new(service.Game), new(*v1.Game)),
-	v1.NewGame,
+		wire.Bind(new(service.Game), new(*v1.Game)),
+		v1.NewGame,
 
-	wire.Bind(new(service.GameVersion), new(*v1.GameVersion)),
-	v1.NewGameVersion,
+		wire.Bind(new(service.GameVersion), new(*v1.GameVersion)),
+		v1.NewGameVersion,
 
-	wire.Bind(new(service.GameImage), new(*v1.GameImage)),
-	v1.NewGameImage,
+		wire.Bind(new(service.GameImage), new(*v1.GameImage)),
+		v1.NewGameImage,
 
-	wire.Bind(new(service.GameVideo), new(*v1.GameVideo)),
-	v1.NewGameVideo,
+		wire.Bind(new(service.GameVideo), new(*v1.GameVideo)),
+		v1.NewGameVideo,
 
-	wire.Bind(new(service.GameFile), new(*v1.GameFile)),
-	v1.NewGameFile,
+		wire.Bind(new(service.GameFile), new(*v1.GameFile)),
+		v1.NewGameFile,
 
-	wire.Bind(new(service.GameURL), new(*v1.GameURL)),
-	v1.NewGameURL,
+		wire.Bind(new(service.GameURL), new(*v1.GameURL)),
+		v1.NewGameURL,
 
-	wire.Bind(new(service.LauncherAuth), new(*v1.LauncherAuth)),
-	v1.NewLauncherAuth,
+		wire.Bind(new(service.LauncherAuth), new(*v1.LauncherAuth)),
+		v1.NewLauncherAuth,
 
-	wire.Bind(new(service.LauncherVersion), new(*v1.LauncherVersion)),
-	v1.NewLauncherVersion,
+		wire.Bind(new(service.LauncherVersion), new(*v1.LauncherVersion)),
+		v1.NewLauncherVersion,
 
-	wire.Bind(new(service.OIDC), new(*v1.OIDC)),
-	v1.NewOIDC,
+		wire.Bind(new(service.OIDC), new(*v1.OIDC)),
+		v1.NewOIDC,
 
-	wire.Bind(new(service.User), new(*v1.User)),
-	v1.NewUser,
+		wire.Bind(new(service.User), new(*v1.User)),
+		v1.NewUser,
 
-	v1.NewUserUtils,
+		v1.NewUserUtils,
+	)
+	serviceV2Set = wire.NewSet(
+		wire.Bind(new(service.OIDCV2), new(*v2.OIDC)),
+		v2.NewOIDC,
+
+		v2.NewUser,
+	)
 )
