612564be5522b238d709a1011c9f5d2aa907fb69
mazrean <shunsuke.w04@gmail.com>
2022-09-24 17:23:16 +0900

    TestSaveGameImage追加

---
+++ src/service/v2/game_image_test.go
@@ -0,0 +1,210 @@
+package v2
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/repository"
+	mockRepository "github.com/traPtitech/trap-collection-server/src/repository/mock"
+	"github.com/traPtitech/trap-collection-server/src/service"
+	mockStorage "github.com/traPtitech/trap-collection-server/src/storage/mock"
+	"github.com/traPtitech/trap-collection-server/testdata"
+)
+
+func TestSaveGameImage(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockDB := mockRepository.NewMockDB(ctrl)
+	mockGameRepository := mockRepository.NewMockGame(ctrl)
+	mockGameImageRepository := mockRepository.NewMockGameImageV2(ctrl)
+
+	type test struct {
+		description                    string
+		gameID                         values.GameID
+		isValidFile                    bool
+		imageType                      values.GameImageType
+		GetGameErr                     error
+		executeRepositorySaveGameImage bool
+		RepositorySaveGameImageErr     error
+		executeStorageSaveGameImage    bool
+		StorageSaveGameImageErr        error
+		isErr                          bool
+		err                            error
+	}
+
+	testCases := []test{
+		{
+			description:                    "特に問題ないのでエラーなし",
+			gameID:                         values.NewGameID(),
+			isValidFile:                    true,
+			imageType:                      values.GameImageTypeJpeg,
+			executeRepositorySaveGameImage: true,
+			executeStorageSaveGameImage:    true,
+		},
+		{
+			description: "GetGameがErrRecordNotFoundなのでErrInvalidGameID",
+			gameID:      values.NewGameID(),
+			isValidFile: true,
+			imageType:   values.GameImageTypeJpeg,
+			GetGameErr:  repository.ErrRecordNotFound,
+			isErr:       true,
+			err:         service.ErrInvalidGameID,
+		},
+		{
+			description: "GetGameがエラーなのでエラー",
+			gameID:      values.NewGameID(),
+			isValidFile: true,
+			imageType:   values.GameImageTypeJpeg,
+			GetGameErr:  errors.New("error"),
+			isErr:       true,
+		},
+		{
+			description:                    "画像がpngでもエラーなし",
+			gameID:                         values.NewGameID(),
+			isValidFile:                    true,
+			imageType:                      values.GameImageTypePng,
+			executeRepositorySaveGameImage: true,
+			executeStorageSaveGameImage:    true,
+		},
+		{
+			description:                    "画像がgifでもエラーなし",
+			gameID:                         values.NewGameID(),
+			isValidFile:                    true,
+			imageType:                      values.GameImageTypeGif,
+			executeRepositorySaveGameImage: true,
+			executeStorageSaveGameImage:    true,
+		},
+		{
+			description:                 "画像が不正なのでエラー",
+			gameID:                      values.NewGameID(),
+			executeStorageSaveGameImage: true,
+			isValidFile:                 false,
+			isErr:                       true,
+			err:                         service.ErrInvalidFormat,
+		},
+		{
+			description:                    "repository.SaveGameImageがエラーなのでエラー",
+			gameID:                         values.NewGameID(),
+			isValidFile:                    true,
+			imageType:                      values.GameImageTypeJpeg,
+			executeRepositorySaveGameImage: true,
+			executeStorageSaveGameImage:    true,
+			RepositorySaveGameImageErr:     errors.New("error"),
+			isErr:                          true,
+		},
+		{
+			description:                    "storage.SaveGameImageがエラーなのでエラー",
+			gameID:                         values.NewGameID(),
+			isValidFile:                    true,
+			imageType:                      values.GameImageTypeJpeg,
+			executeRepositorySaveGameImage: true,
+			executeStorageSaveGameImage:    true,
+			StorageSaveGameImageErr:        errors.New("error"),
+			isErr:                          true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
+
+			mockGameImageStorage := mockStorage.NewGameImage(ctrl, buf)
+
+			gameImageService := NewGameImage(
+				mockDB,
+				mockGameRepository,
+				mockGameImageRepository,
+				mockGameImageStorage,
+			)
+
+			var file io.Reader
+			var expectBytes []byte
+			if testCase.isValidFile {
+				imgBuf := bytes.NewBuffer(nil)
+
+				err := func() error {
+					var path string
+					switch testCase.imageType {
+					case values.GameImageTypeJpeg:
+						path = "1.jpg"
+					case values.GameImageTypePng:
+						path = "1.png"
+					case values.GameImageTypeGif:
+						path = "1.gif"
+					default:
+						t.Fatalf("invalid image type: %v\n", testCase.imageType)
+					}
+					f, err := testdata.FS.Open(path)
+					if err != nil {
+						return fmt.Errorf("failed to open file: %w", err)
+					}
+					defer f.Close()
+
+					_, err = io.Copy(imgBuf, f)
+					if err != nil {
+						return fmt.Errorf("failed to copy file: %w", err)
+					}
+
+					return nil
+				}()
+				if err != nil {
+					t.Fatalf("failed to encode image: %s", err)
+				}
+
+				file = imgBuf
+				expectBytes = imgBuf.Bytes()
+			} else {
+				file = strings.NewReader("invalid file")
+			}
+
+			mockGameRepository.
+				EXPECT().
+				GetGame(gomock.Any(), testCase.gameID, repository.LockTypeRecord).
+				Return(nil, testCase.GetGameErr)
+
+			if testCase.executeRepositorySaveGameImage {
+				mockGameImageRepository.
+					EXPECT().
+					SaveGameImage(gomock.Any(), testCase.gameID, gomock.Any()).
+					Return(testCase.RepositorySaveGameImageErr)
+			}
+
+			if testCase.executeStorageSaveGameImage {
+				mockGameImageStorage.
+					EXPECT().
+					SaveGameImage(gomock.Any(), gomock.Any()).
+					Return(testCase.StorageSaveGameImageErr)
+			}
+
+			err := gameImageService.SaveGameImage(ctx, file, testCase.gameID)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil || testCase.isErr {
+				return
+			}
+
+			assert.Equal(t, expectBytes, buf.Bytes())
+		})
+	}
+}
