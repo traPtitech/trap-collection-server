8bc7b7d308dcc53d6e79bf29020806f773d96b7c
mazrean <shunsuke.w04@gmail.com>
2022-09-27 03:14:26 +0900

    GetGameImage実装

--- src/repository/gorm2/v2_game_image.go
+++ src/repository/gorm2/v2_game_image.go
@@ -2,12 +2,15 @@ package gorm2
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/google/uuid"
 	"github.com/traPtitech/trap-collection-server/src/domain"
 	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/repository"
 	"github.com/traPtitech/trap-collection-server/src/repository/gorm2/migrate"
+	"gorm.io/gorm"
 )
 
 type GameImageV2 struct {
@@ -60,4 +63,49 @@ func (gameImage *GameImageV2) SaveGameImage(ctx context.Context, gameID values.G
 	}
 
 	return nil
+}
+
+func (gameImage *GameImageV2) GetGameImage(ctx context.Context, gameImageID values.GameImageID, lockType repository.LockType) (*repository.GameImageInfo, error) {
+	db, err := gameImage.db.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get db: %w", err)
+	}
+
+	db, err = gameImage.db.setLock(db, lockType)
+	if err != nil {
+		return nil, fmt.Errorf("failed to set lock: %w", err)
+	}
+
+	var image migrate.GameImageTable2
+	err = db.
+		Joins("GameImageType").
+		Where("v2_game_images.id = ?", uuid.UUID(gameImageID)).
+		Take(&image).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, repository.ErrRecordNotFound
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to get game image: %w", err)
+	}
+
+	var imageType values.GameImageType
+	switch image.GameImageType.Name {
+	case migrate.GameImageTypeJpeg:
+		imageType = values.GameImageTypeJpeg
+	case migrate.GameImageTypePng:
+		imageType = values.GameImageTypePng
+	case migrate.GameImageTypeGif:
+		imageType = values.GameImageTypeGif
+	default:
+		return nil, fmt.Errorf("invalid image type: %s", image.GameImageType.Name)
+	}
+
+	return &repository.GameImageInfo{
+		GameImage: domain.NewGameImage(
+			values.GameImageIDFromUUID(image.ID),
+			imageType,
+			image.CreatedAt,
+		),
+		GameID: values.NewGameIDFromUUID(image.GameID),
+	}, nil
 }
