7ecef716b6d3b0ec3834030b5b9f3bb7fe61bd84
mazrean <shunsuke.w04@gmail.com>
2022-09-28 01:32:16 +0900

    GetGameImages実装

--- src/handler/v2/game_image.go
+++ src/handler/v2/game_image.go
@@ -1,7 +1,12 @@
 package v2
 
 import (
+	"errors"
+	"log"
+	"net/http"
+
 	"github.com/labstack/echo/v4"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
 	"github.com/traPtitech/trap-collection-server/src/handler/v2/openapi"
 	"github.com/traPtitech/trap-collection-server/src/service"
 )
@@ -21,9 +26,6 @@ func NewGameImage(gameImageService service.GameImageV2) *GameImage {
 // メソッドとして実装予定だが、未実装のもの
 // TODO: 実装
 type gameImageUnimplemented interface {
-	// ゲーム画像の作成
-	// (GET /games/{gameID}/images)
-	GetGameImages(ctx echo.Context, gameID openapi.GameIDInPath) error
 	// ゲーム画像一覧の取得
 	// (POST /games/{gameID}/images)
 	PostGameImage(ctx echo.Context, gameID openapi.GameIDInPath) error
@@ -33,4 +35,41 @@ type gameImageUnimplemented interface {
 	// ゲーム画像のメタ情報の取得
 	// (GET /games/{gameID}/images/{gameImageID}/meta)
 	GetGameImageMeta(ctx echo.Context, gameID openapi.GameIDInPath, gameImageID openapi.GameImageIDInPath) error
+}
+
+// ゲーム画像一覧の取得
+// (GET /games/{gameID}/images)
+func (gameImage *GameImage) GetGameImages(c echo.Context, gameID openapi.GameIDInPath) error {
+	images, err := gameImage.gameImageService.GetGameImages(c.Request().Context(), values.NewGameIDFromUUID(gameID))
+	if errors.Is(err, service.ErrInvalidGameID) {
+		return echo.NewHTTPError(http.StatusNotFound, "invalid gameID")
+	}
+	if err != nil {
+		log.Printf("error: failed to get game images: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get game images")
+	}
+
+	resImages := make([]openapi.GameImage, 0, len(images))
+	for _, image := range images {
+		var mime openapi.GameImageMime
+		switch image.GetType() {
+		case values.GameImageTypeJpeg:
+			mime = openapi.Imagejpeg
+		case values.GameImageTypePng:
+			mime = openapi.Imagepng
+		case values.GameImageTypeGif:
+			mime = openapi.Imagegif
+		default:
+			log.Printf("error: unknown game image type: %v\n", image.GetType())
+			return echo.NewHTTPError(http.StatusInternalServerError, "unknown game image type")
+		}
+
+		resImages = append(resImages, openapi.GameImage{
+			Id:        openapi.GameImageID(image.GetID()),
+			Mime:      mime,
+			CreatedAt: image.GetCreatedAt(),
+		})
+	}
+
+	return c.JSON(http.StatusOK, resImages)
 }
