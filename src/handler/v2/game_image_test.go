1b2b3615b2e6db4a8bf187d4939ab487cd858ce0
mazrean <shunsuke.w04@gmail.com>
2022-10-03 03:37:41 +0900

    TestGetGameImages追加

---
+++ src/handler/v2/game_image_test.go
@@ -0,0 +1,217 @@
+package v2
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/handler/v2/openapi"
+	"github.com/traPtitech/trap-collection-server/src/service"
+	"github.com/traPtitech/trap-collection-server/src/service/mock"
+)
+
+func TestGetGameImages(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockGameImageService := mock.NewMockGameImageV2(ctrl)
+
+	gameImage := NewGameImage(mockGameImageService)
+
+	type test struct {
+		description      string
+		gameID           openapi.GameIDInPath
+		images           []*domain.GameImage
+		getGameImagesErr error
+		resImages        []openapi.GameImage
+		isErr            bool
+		err              error
+		statusCode       int
+	}
+
+	gameImageID1 := values.NewGameImageID()
+	gameImageID2 := values.NewGameImageID()
+	gameImageID3 := values.NewGameImageID()
+	gameImageID4 := values.NewGameImageID()
+	gameImageID5 := values.NewGameImageID()
+
+	now := time.Now()
+	testCases := []test{
+		{
+			description: "特に問題ないのでエラーなし",
+			gameID:      uuid.UUID(values.NewGameID()),
+			images: []*domain.GameImage{
+				domain.NewGameImage(
+					gameImageID1,
+					values.GameImageTypeJpeg,
+					now,
+				),
+			},
+			resImages: []openapi.GameImage{
+				{
+					Id:        uuid.UUID(gameImageID1),
+					Mime:      openapi.Imagejpeg,
+					CreatedAt: now,
+				},
+			},
+		},
+		{
+			description: "pngでもエラーなし",
+			gameID:      uuid.UUID(values.NewGameID()),
+			images: []*domain.GameImage{
+				domain.NewGameImage(
+					gameImageID2,
+					values.GameImageTypePng,
+					now,
+				),
+			},
+			resImages: []openapi.GameImage{
+				{
+					Id:        uuid.UUID(gameImageID2),
+					Mime:      openapi.Imagepng,
+					CreatedAt: now,
+				},
+			},
+		},
+		{
+			description: "gifでもエラーなし",
+			gameID:      uuid.UUID(values.NewGameID()),
+			images: []*domain.GameImage{
+				domain.NewGameImage(
+					gameImageID3,
+					values.GameImageTypeGif,
+					now,
+				),
+			},
+			resImages: []openapi.GameImage{
+				{
+					Id:        uuid.UUID(gameImageID3),
+					Mime:      openapi.Imagegif,
+					CreatedAt: now,
+				},
+			},
+		},
+		{
+			description: "jpeg,png,gifのいずれでもないので500",
+			gameID:      uuid.UUID(values.NewGameID()),
+			images: []*domain.GameImage{
+				domain.NewGameImage(
+					values.NewGameImageID(),
+					values.GameImageType(100),
+					now,
+				),
+			},
+			isErr:      true,
+			statusCode: http.StatusInternalServerError,
+		},
+		{
+			description:      "GetGameImagesがErrInvalidGameIDなので404",
+			gameID:           uuid.UUID(values.NewGameID()),
+			getGameImagesErr: service.ErrInvalidGameID,
+			isErr:            true,
+			statusCode:       http.StatusNotFound,
+		},
+		{
+			description:      "GetGameImagesがエラーなので500",
+			gameID:           uuid.UUID(values.NewGameID()),
+			getGameImagesErr: errors.New("error"),
+			isErr:            true,
+			statusCode:       http.StatusInternalServerError,
+		},
+		{
+			description: "画像がなくても問題なし",
+			gameID:      uuid.UUID(values.NewGameID()),
+			images:      []*domain.GameImage{},
+			resImages:   []openapi.GameImage{},
+		},
+		{
+			description: "画像が複数あっても問題なし",
+			gameID:      uuid.UUID(values.NewGameID()),
+			images: []*domain.GameImage{
+				domain.NewGameImage(
+					gameImageID4,
+					values.GameImageTypeJpeg,
+					now,
+				),
+				domain.NewGameImage(
+					gameImageID5,
+					values.GameImageTypePng,
+					now.Add(-10*time.Hour),
+				),
+			},
+			resImages: []openapi.GameImage{
+				{
+					Id:        uuid.UUID(gameImageID4),
+					Mime:      openapi.Imagejpeg,
+					CreatedAt: now,
+				},
+				{
+					Id:        uuid.UUID(gameImageID5),
+					Mime:      openapi.Imagepng,
+					CreatedAt: now.Add(-10 * time.Hour),
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			e := echo.New()
+			req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/api/v2/games/%s/images", testCase.gameID), nil)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+
+			mockGameImageService.
+				EXPECT().
+				GetGameImages(gomock.Any(), gomock.Any()).
+				Return(testCase.images, testCase.getGameImagesErr)
+
+			err := gameImage.GetGameImages(c, testCase.gameID)
+
+			if testCase.isErr {
+				if testCase.statusCode != 0 {
+					var httpError *echo.HTTPError
+					if errors.As(err, &httpError) {
+						assert.Equal(t, testCase.statusCode, httpError.Code)
+					} else {
+						t.Errorf("error is not *echo.HTTPError")
+					}
+				} else if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil || testCase.isErr {
+				return
+			}
+
+			assert.Equal(t, http.StatusOK, rec.Code)
+
+			var resImages []openapi.GameImage
+			err = json.NewDecoder(rec.Body).Decode(&resImages)
+			if err != nil {
+				t.Fatalf("failed to decode response body: %v", err)
+			}
+			for i, resImage := range resImages {
+				assert.Equal(t, testCase.resImages[i].Id, resImage.Id)
+				assert.Equal(t, testCase.resImages[i].Mime, resImage.Mime)
+				assert.WithinDuration(t, testCase.resImages[i].CreatedAt, resImage.CreatedAt, time.Second)
+			}
+		})
+	}
+}
