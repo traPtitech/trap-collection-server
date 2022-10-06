4b83ba4c815fdcf2a89c48e5f8eb17c8c58ed25c
ikura-hamu <harukikainuma@gmail.com>
2022-10-06 11:19:59 +0900

    UpdateGame実装

--- src/repository/gorm2/v2_game.go
+++ src/repository/gorm2/v2_game.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/repository"
 	"github.com/traPtitech/trap-collection-server/src/repository/gorm2/migrate"
 )
 
@@ -35,6 +36,32 @@ func (g *GameV2) SaveGame(ctx context.Context, game *domain.Game) error {
 	err = db.Create(&gameTable).Error
 	if err != nil {
 		return fmt.Errorf("failed to save game: %w", err)
+	}
+
+	return nil
+}
+
+func (g *GameV2) UpdateGame(ctx context.Context, game *domain.Game) error {
+	db, err := g.db.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db: %w", err)
+	}
+
+	gameTable := migrate.GameTable{
+		Name:        string(game.GetName()),
+		Description: string(game.GetDescription()),
+	}
+
+	result := db.
+		Where("id = ?", uuid.UUID(game.GetID())).
+		Updates(gameTable)
+	err = result.Error
+	if err != nil {
+		return fmt.Errorf("failed to update game: %w", err)
+	}
+
+	if result.RowsAffected == 0 {
+		return repository.ErrNoRecordUpdated
 	}
 
 	return nil
