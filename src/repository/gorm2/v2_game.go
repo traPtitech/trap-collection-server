55a83486183a506fa5138b8be7ece4e310cb181f
ikura-hamu <harukikainuma@gmail.com>
2022-10-06 11:16:09 +0900

    SaveGame実装

--- src/repository/gorm2/v2_game.go
+++ src/repository/gorm2/v2_game.go
@@ -1,5 +1,14 @@
 package gorm2
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/repository/gorm2/migrate"
+)
+
 type GameV2 struct {
 	db *DB
 }
@@ -8,4 +17,25 @@ func NewGameV2(db *DB) *GameV2 {
 	return &GameV2{
 		db: db,
 	}
+}
+
+func (g *GameV2) SaveGame(ctx context.Context, game *domain.Game) error {
+	db, err := g.db.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db: %w", err)
+	}
+
+	gameTable := migrate.GameTable{
+		ID:          uuid.UUID(game.GetID()),
+		Name:        string(game.GetName()),
+		Description: string(game.GetDescription()),
+		CreatedAt:   game.GetCreatedAt(),
+	}
+
+	err = db.Create(&gameTable).Error
+	if err != nil {
+		return fmt.Errorf("failed to save game: %w", err)
+	}
+
+	return nil
 }
