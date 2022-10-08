6f96175ddee195f2db341f0483887e1fdfbf87aa
ikura-hamu <harukikainuma@gmail.com>
2022-10-08 12:23:14 +0900

    TestGetGame実装

--- src/repository/gorm2/v2_game_test.go
+++ src/repository/gorm2/v2_game_test.go
@@ -454,3 +454,162 @@ func TestRemoveGameV2(t *testing.T) {
 		})
 	}
 }
+
+func TestGetGameV2(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	db, err := testDB.getDB(ctx)
+	if err != nil {
+		t.Fatalf("failed to get db: %+v\n", err)
+	}
+
+	gameRepository := NewGameV2(testDB)
+
+	type test struct {
+		description string
+		gameID      values.GameID
+		lockType    repository.LockType
+		GameTable   []migrate.GameTable
+		game        *domain.Game
+		isErr       bool
+		err         error
+	}
+
+	gameID1 := values.NewGameID()
+	gameID2 := values.NewGameID()
+	gameID3 := values.NewGameID()
+	gameID4 := values.NewGameID()
+	gameID5 := values.NewGameID()
+
+	now := time.Now()
+
+	testCases := []test{
+		{
+			description: "特に問題ないのでエラーなし",
+			gameID:      gameID1,
+			lockType:    repository.LockTypeNone,
+			GameTable: []migrate.GameTable{
+				{
+					ID:          uuid.UUID(gameID1),
+					Name:        "test",
+					Description: "test",
+					CreatedAt:   now,
+				},
+			},
+			game: domain.NewGame(
+				gameID1,
+				"test",
+				"test",
+				now,
+			),
+		},
+		{
+			description: "行ロックでもエラーなし",
+			gameID:      gameID2,
+			lockType:    repository.LockTypeRecord,
+			GameTable: []migrate.GameTable{
+				{
+					ID:          uuid.UUID(gameID2),
+					Name:        "test",
+					Description: "test",
+					CreatedAt:   now,
+				},
+			},
+			game: domain.NewGame(
+				gameID2,
+				"test",
+				"test",
+				now,
+			),
+		},
+		{
+			description: "ロックの種類が不正なのでエラー",
+			gameID:      gameID5,
+			lockType:    100,
+			GameTable: []migrate.GameTable{
+				{
+					ID:          uuid.UUID(gameID5),
+					Name:        "test",
+					Description: "test",
+					CreatedAt:   now,
+				},
+			},
+			game: domain.NewGame(
+				gameID5,
+				"test",
+				"test",
+				now,
+			),
+			isErr: true,
+		},
+		{
+			description: "ゲームが存在しないのでErrRecordNotFound",
+			gameID:      gameID3,
+			lockType:    repository.LockTypeNone,
+			GameTable:   []migrate.GameTable{},
+			isErr:       true,
+			err:         repository.ErrRecordNotFound,
+		},
+		{
+			description: "ゲームが削除済みなのでErrRecordNotFound",
+			gameID:      gameID4,
+			lockType:    repository.LockTypeNone,
+			GameTable: []migrate.GameTable{
+				{
+					ID:          uuid.UUID(gameID4),
+					Name:        "test",
+					Description: "test",
+					CreatedAt:   now,
+					DeletedAt: gorm.DeletedAt{
+						Time:  now,
+						Valid: true,
+					},
+				},
+			},
+			isErr: true,
+			err:   repository.ErrRecordNotFound,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			if len(testCase.GameTable) != 0 {
+				err := db.Create(&testCase.GameTable).Error
+				if err != nil {
+					t.Fatalf("failed to create test data: %+v\n", err)
+				}
+
+				for _, game := range testCase.GameTable {
+					if game.DeletedAt.Valid {
+						err = db.Delete(&game).Error
+						if err != nil {
+							t.Fatalf("failed to delete test data: %+v\n", err)
+						}
+					}
+				}
+			}
+
+			game, err := gameRepository.GetGameV2(ctx, testCase.gameID, testCase.lockType)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, testCase.game.GetID(), game.GetID())
+			assert.Equal(t, testCase.game.GetName(), game.GetName())
+			assert.Equal(t, testCase.game.GetDescription(), game.GetDescription())
+			assert.WithinDuration(t, testCase.game.GetCreatedAt(), game.GetCreatedAt(), 2*time.Second)
+		})
+	}
+}
