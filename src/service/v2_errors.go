82def4612267085048111f81a80d262e7272a189
ikura-hamu <harukikainuma@gmail.com>
2022-10-13 16:09:34 +0900

    serviceでGetGamesのoffset対応

--- src/service/v2_errors.go
+++ src/service/v2_errors.go
@@ -6,4 +6,5 @@ var (
 	ErrOverlapBetweenOwnersAndMaintainers = errors.New("overlap between owners and maintainers")
 	ErrOverlapInOwners                    = errors.New("overlap (in owners/between login user and owners)")
 	ErrOverlapInMaintainers               = errors.New("overlap in maintainers")
+	ErrOffsetWithoutLimit                 = errors.New("there is offset but no limit")
 )
