4e1c662279d84fcfae11efdebd901fe689e05f09
ikura-hamu <harukikainuma@gmail.com>
2022-10-14 13:41:26 +0900

    SQL文周りを整理

--- src/repository/errors.go
+++ src/repository/errors.go
@@ -3,10 +3,9 @@ package repository
 import "errors"
 
 var (
-	ErrRecordNotFound     = errors.New("record not found")
-	ErrNoRecordDeleted    = errors.New("no record deleted")
-	ErrNoRecordUpdated    = errors.New("no record updated")
-	ErrNegativeLimit      = errors.New("limit is negative")
-	ErrOffsetWithoutLimit = errors.New("there is offset but no limit")
-	ErrBadLimitAndOffset  = errors.New("Limit and Offset are invalid")
+	ErrRecordNotFound    = errors.New("record not found")
+	ErrNoRecordDeleted   = errors.New("no record deleted")
+	ErrNoRecordUpdated   = errors.New("no record updated")
+	ErrNegativeLimit     = errors.New("limit is negative")
+	ErrBadLimitAndOffset = errors.New("Limit and Offset are invalid")
 )
