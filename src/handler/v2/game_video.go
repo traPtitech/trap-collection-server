52e67d269160641acbb83b7bfc97f2787ac3ff0a
ikura-hamu <harukikainuma@gmail.com>
2022-10-18 16:50:57 +0900

    PostGameVideo実装

--- src/handler/v2/game_video.go
+++ src/handler/v2/game_video.go
@@ -39,7 +39,7 @@ type gameVideoUnimplemented interface {
 	GetGameVideoMeta(ctx echo.Context, gameID openapi.GameIDInPath, gameVideoID openapi.GameVideoIDInPath) error
 }
 
-// ゲーム動画の作成
+// ゲーム動画一覧の取得
 // (GET /games/{gameID}/videos)
 func (gameVideo *GameVideo) GetGameVideos(c echo.Context, gameID openapi.GameIDInPath) error {
 	videos, err := gameVideo.gameVideoService.GetGameVideos(c.Request().Context(), values.NewGameIDFromUUID(gameID))
@@ -69,4 +69,42 @@ func (gameVideo *GameVideo) GetGameVideos(c echo.Context, gameID openapi.GameIDI
 	}
 
 	return c.JSON(http.StatusOK, resVideos)
+}
+
+// ゲーム動画の作成
+// (POST /games/{gameID}/videos)
+func (gameVideo *GameVideo) PostGameVideo(c echo.Context, gameID openapi.GameIDInPath) error {
+	header, err := c.FormFile("content")
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid file")
+	}
+	file, err := header.Open()
+	if err != nil {
+		log.Printf("error: failed to open file: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to open file")
+	}
+	defer file.Close()
+
+	video, err := gameVideo.gameVideoService.SaveGameVideo(c.Request().Context(), file, values.NewGameIDFromUUID(gameID))
+	if errors.Is(err, service.ErrInvalidGameID) {
+		return echo.NewHTTPError(http.StatusNotFound, "invalid gameID")
+	}
+	if err != nil {
+		log.Printf("error: failed to save game video: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to save game video")
+	}
+
+	var mime openapi.GameVideoMime
+	if video.GetType() == values.GameVideoTypeMp4 {
+		mime = openapi.Videomp4
+	} else {
+		log.Printf("error: unknown game video type: %v\n", video.GetType())
+		return echo.NewHTTPError(http.StatusInternalServerError, "unknown game video type")
+	}
+
+	return c.JSON(http.StatusCreated, openapi.GameVideo{
+		Id:        openapi.GameVideoID(video.GetID()),
+		Mime:      mime,
+		CreatedAt: video.GetCreatedAt(),
+	})
 }
