d3229abdedef3bbd8a500fbe0ce92e063cc58098
ikura-hamu <harukikainuma@gmail.com>
2022-10-18 17:15:17 +0900

    TestGetGameVideo追加

--- src/handler/v2/game_video_test.go
+++ src/handler/v2/game_video_test.go
@@ -9,6 +9,7 @@ import (
 	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 	"time"
 
@@ -335,6 +336,108 @@ func TestPostGameVideo(t *testing.T) {
 			assert.Equal(t, testCase.resVideo.Id, resVideo.Id)
 			assert.Equal(t, testCase.resVideo.Mime, resVideo.Mime)
 			assert.WithinDuration(t, testCase.resVideo.CreatedAt, resVideo.CreatedAt, time.Second)
+		})
+	}
+}
+
+func TestGetGameVideo(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockGameVideoService := mock.NewMockGameVideoV2(ctrl)
+
+	gameVideo := NewGameVideo(mockGameVideoService)
+
+	type test struct {
+		description     string
+		gameID          openapi.GameIDInPath
+		gameVideoID     openapi.GameVideoIDInPath
+		tmpURL          values.GameVideoTmpURL
+		getGameVideoErr error
+		resLocation     string
+		isErr           bool
+		err             error
+		statusCode      int
+	}
+
+	urlLink, err := url.Parse("https://example.com")
+	if err != nil {
+		t.Fatalf("failed to encode video: %v", err)
+	}
+
+	testCases := []test{
+		{
+			description: "特に問題ないのでエラーなし",
+			gameID:      uuid.UUID(values.NewGameID()),
+			gameVideoID: uuid.UUID(values.NewGameVideoID()),
+			tmpURL:      values.NewGameVideoTmpURL(urlLink),
+			resLocation: "https://example.com",
+		},
+		{
+			description:     "GetGameVideoがErrInvalidGameIDなので404",
+			gameID:          uuid.UUID(values.NewGameID()),
+			gameVideoID:     uuid.UUID(values.NewGameVideoID()),
+			getGameVideoErr: service.ErrInvalidGameID,
+			isErr:           true,
+			statusCode:      http.StatusNotFound,
+		},
+		{
+			description:     "GetGameVideoがErrInvalidGameVideoIDなので404",
+			gameID:          uuid.UUID(values.NewGameID()),
+			gameVideoID:     uuid.UUID(values.NewGameVideoID()),
+			getGameVideoErr: service.ErrInvalidGameVideoID,
+			isErr:           true,
+			statusCode:      http.StatusNotFound,
+		},
+		{
+			description:     "GetGameVideoがエラーなので500",
+			gameID:          uuid.UUID(values.NewGameID()),
+			gameVideoID:     uuid.UUID(values.NewGameVideoID()),
+			getGameVideoErr: errors.New("error"),
+			isErr:           true,
+			statusCode:      http.StatusInternalServerError,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			e := echo.New()
+			req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/api/v2/games/%s/videos", testCase.gameID), nil)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+
+			mockGameVideoService.
+				EXPECT().
+				GetGameVideo(gomock.Any(), gomock.Any(), gomock.Any()).
+				Return(testCase.tmpURL, testCase.getGameVideoErr)
+
+			err := gameVideo.GetGameVideo(c, testCase.gameID, testCase.gameVideoID)
+
+			if testCase.isErr {
+				if testCase.statusCode != 0 {
+					var httpError *echo.HTTPError
+					if errors.As(err, &httpError) {
+						assert.Equal(t, testCase.statusCode, httpError.Code)
+					} else {
+						t.Errorf("error is not *echo.HTTPError")
+					}
+				} else if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil || testCase.isErr {
+				return
+			}
+
+			assert.Equal(t, http.StatusSeeOther, rec.Code)
+
+			assert.Equal(t, testCase.resLocation, rec.Header().Get("Location"))
 		})
 	}
 }
