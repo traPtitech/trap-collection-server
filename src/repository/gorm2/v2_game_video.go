5112d985bee532bca10a9b0480c2eaac2f7c3efd
ikura-hamu <harukikainuma@gmail.com>
2022-10-18 13:26:03 +0900

    GetGameVideo実装

--- src/repository/gorm2/v2_game_video.go
+++ src/repository/gorm2/v2_game_video.go
@@ -2,12 +2,15 @@ package gorm2
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/google/uuid"
 	"github.com/traPtitech/trap-collection-server/src/domain"
 	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/repository"
 	"github.com/traPtitech/trap-collection-server/src/repository/gorm2/migrate"
+	"gorm.io/gorm"
 )
 
 type GameVideoV2 struct {
@@ -55,4 +58,44 @@ func (gameVideo *GameVideoV2) SaveGameVideo(ctx context.Context, gameID values.G
 	}
 
 	return nil
+}
+
+func (gameVideo *GameVideoV2) GetGameVideo(ctx context.Context, gameVideoID values.GameVideoID, lockType repository.LockType) (*repository.GameVideoInfo, error) {
+	db, err := gameVideo.db.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get db: %w", err)
+	}
+
+	db, err = gameVideo.db.setLock(db, lockType)
+	if err != nil {
+		return nil, fmt.Errorf("failed to set lock: %w", err)
+	}
+
+	var video migrate.GameVideoTable2
+	err = db.
+		Joins("GameVideoType").
+		Where("v2_game_video.id = ?", uuid.UUID(gameVideoID)).
+		Take(&video).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, repository.ErrRecordNotFound
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to get game video: %w", err)
+	}
+
+	var videoType values.GameVideoType
+	if video.GameVideoType.Name == migrate.GameVideoTypeMp4 {
+		videoType = values.GameVideoTypeMp4
+	} else {
+		return nil, fmt.Errorf("invalid video type: %s", video.GameVideoType.Name)
+	}
+
+	return &repository.GameVideoInfo{
+		GameVideo: domain.NewGameVideo(
+			values.NewGameVideoIDFromUUID(video.ID),
+			videoType,
+			video.CreatedAt,
+		),
+		GameID: values.NewGameIDFromUUID(video.GameID),
+	}, nil
 }
