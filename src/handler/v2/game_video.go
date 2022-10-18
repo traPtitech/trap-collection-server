1a58211a92f00301f65bee9cf9aa14399c9255be
ikura-hamu <harukikainuma@gmail.com>
2022-10-18 17:23:42 +0900

    GetGameVideoMeta実装

--- src/handler/v2/game_video.go
+++ src/handler/v2/game_video.go
@@ -127,3 +127,33 @@ func (gameVideo *GameVideo) GetGameVideo(c echo.Context, gameID openapi.GameIDIn
 
 	return c.Redirect(http.StatusSeeOther, (*url.URL)(tmpURL).String())
 }
+
+// ゲーム動画のメタ情報の取得
+// (GET /games/{gameID}/videos/{gameVideoID}/meta)
+func (gameVideo *GameVideo) GetGameVideoMeta(ctx echo.Context, gameID openapi.GameIDInPath, gameVideoID openapi.GameVideoIDInPath) error {
+	video, err := gameVideo.gameVideoService.GetGameVideoMeta(ctx.Request().Context(), values.NewGameIDFromUUID(gameID), values.NewGameVideoIDFromUUID(gameVideoID))
+	if errors.Is(err, service.ErrInvalidGameID) {
+		return echo.NewHTTPError(http.StatusNotFound, "invalid gameID")
+	}
+	if errors.Is(err, service.ErrInvalidGameVideoID) {
+		return echo.NewHTTPError(http.StatusNotFound, "invalid gameVideoID")
+	}
+	if err != nil {
+		log.Printf("error: failed to get game video meta: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get game video meta")
+	}
+
+	var mime openapi.GameVideoMime
+	if video.GetType() == values.GameVideoTypeMp4 {
+		mime = openapi.Videomp4
+	} else {
+		log.Printf("error: unknown game video type: %v\n", video.GetType())
+		return echo.NewHTTPError(http.StatusInternalServerError, "unknown game video type")
+	}
+
+	return ctx.JSON(http.StatusOK, openapi.GameVideo{
+		Id:        openapi.GameVideoID(video.GetID()),
+		Mime:      mime,
+		CreatedAt: video.GetCreatedAt(),
+	})
+}
