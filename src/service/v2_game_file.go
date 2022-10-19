b2ed4d7343522e2809473ca4060676f62f8e14b9
ikura-hamu <harukikainuma@gmail.com>
2022-10-19 13:55:43 +0900

    GameFileV2追加

---
+++ src/service/v2_game_file.go
@@ -0,0 +1,33 @@
+package service
+
+//go:generate go run github.com/golang/mock/mockgen -source=$GOFILE -destination=mock/${GOFILE} -package=mock
+
+import (
+	"context"
+	"io"
+
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+)
+
+type GameFileV2 interface {
+	// SaveGameFile
+	// ゲームファイルの保存。
+	// ゲームIDに対応するゲームが存在しない場合、ErrInvalidGameIDを返す。
+	SaveGameFiel(ctx context.Context, reader io.Reader, gameID values.GameID) (*domain.GameFile, error)
+	// GetGameFile
+	// ゲームファイル一覧の取得。
+	// ゲームIDに対応するゲームが存在しない場合、ErrInvalidGameIDを返す。
+	GetGameFiles(ctx context.Context, gameID values.GameID) ([]*domain.GameFile, error)
+	// GetGameFile
+	// ゲームファイルの一時的(1分間)に有効なurlを返す。
+	// ゲームIDに対応するゲームが存在しない場合、ErrInvalidGameIDを返す。
+	// ゲームファイルIDに対応するゲームファイルが存在しない、
+	// もしくは存在しても紐づくゲームのゲームIDが異なる場合、ErrInvalidGameFileIDを返す。
+	GetGameFile(ctx context.Context, gameID values.GameID, fileID values.GameFileID) (values.GameFileTmpURL, error)
+	// GetGameFileMeta
+	// ゲームファイルのメタデータの取得。
+	// ゲームIDに対応するゲームが存在しない場合、ErrInvalidGameIDを返す。
+	// ゲームファイルIDに対応するゲームファイルが存在しない場合、ErrInvalidGameFileIDを返す。
+	GetGameFileMeta(ctx context.Context, gameID values.GameID, fileID values.GameFileID) (*domain.GameFile, error)
+}
