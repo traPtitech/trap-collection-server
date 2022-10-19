db5c73fdb86b562fd0b26355c215658633462373
ikura-hamu <harukikainuma@gmail.com>
2022-10-19 14:04:36 +0900

    repositoryのGameFileV2追加

--- src/repository/v2_game_file.go
+++ src/repository/v2_game_file.go
@@ -1,5 +1,7 @@
 package repository
 
+//go:generate go run github.com/golang/mock/mockgen -source=$GOFILE -destination=mock/${GOFILE} -package=mock
+
 import (
 	"context"
 
@@ -7,14 +9,23 @@ import (
 	"github.com/traPtitech/trap-collection-server/src/domain/values"
 )
 
-//go:generate go run github.com/golang/mock/mockgen -source=$GOFILE -destination=mock/${GOFILE} -package=mock
-
+// GameFileV2
+// ゲームファイルのメタデータの保存・取得。
+// ストレージに保存済みのファイルのメタデータのみが見えるように使う。
 type GameFileV2 interface {
+	// SaveGameFile
+	// ゲームファイルのメタデータの保存。
+	// 他のスレッドからはストレージに保存済みのファイルのみが見えるようにトランザクションをとるように注意する。
+	SaveGameFile(ctx context.Context, gameID values.GameID, file *domain.GameFile) error
+	// GetGameFile
+	// ゲームファイルのメタデータの取得。
+	// 既にストレージに保存済みのファイルのみが取得できる。
+	GetGameFile(ctx context.Context, gameFileID values.GameFileID, lockType LockType) (*GameFileInfo, error)
 	// GetGameFiles
-	// ゲームファイルのメタデータ一覧の取得。
+	// ゲームに対応するゲームファイルのメタデータ一覧の取得。
 	// 既にストレージに保存済みのファイルのみが取得できる。
 	// ファイルの並び順はCreateAtの降順。
-	GetGameFiles(ctx context.Context, fileIDs []values.GameFileID, lockType LockType) ([]*GameFileInfo, error)
+	GetGameFiles(ctx context.Context, gameID values.GameID, lockType LockType) ([]*domain.GameFile, error)
 }
 
 type GameFileInfo struct {
