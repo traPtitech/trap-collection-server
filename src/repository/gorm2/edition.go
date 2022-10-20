6fc005d476fa9b00d6b98458e069402ec585d809
mazrean <shunsuke.w04@gmail.com>
2022-10-21 01:03:29 +0900

    UpdateEdition実装

--- src/repository/gorm2/edition.go
+++ src/repository/gorm2/edition.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/repository"
 	"github.com/traPtitech/trap-collection-server/src/repository/gorm2/migrate"
 )
 
@@ -54,6 +55,46 @@ func (e *Edition) SaveEdition(ctx context.Context, edition *domain.LauncherVersi
 		}).Error
 	if err != nil {
 		return fmt.Errorf("failed to save edition: %w", err)
+	}
+
+	return nil
+}
+
+func (e *Edition) UpdateEdition(ctx context.Context, edition *domain.LauncherVersion) error {
+	db, err := e.db.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db: %w", err)
+	}
+
+	questionnaireURL, err := edition.GetQuestionnaireURL()
+	if err != nil && !errors.Is(err, domain.ErrNoQuestionnaire) {
+		return fmt.Errorf("failed to get questionnaire url: %w", err)
+	}
+
+	var strQuestionnaireURL sql.NullString
+	if errors.Is(err, domain.ErrNoQuestionnaire) {
+		strQuestionnaireURL = sql.NullString{
+			Valid: false,
+		}
+	} else {
+		strQuestionnaireURL = sql.NullString{
+			String: (*url.URL)(questionnaireURL).String(),
+			Valid:  true,
+		}
+	}
+
+	result := db.
+		Where("id = ?", uuid.UUID(edition.GetID())).
+		Updates(migrate.EditionTable2{
+			Name:             string(edition.GetName()),
+			QuestionnaireURL: strQuestionnaireURL,
+		})
+	if result.Error != nil {
+		return fmt.Errorf("failed to update edition: %w", result.Error)
+	}
+
+	if result.RowsAffected == 0 {
+		return repository.ErrNoRecordUpdated
 	}
 
 	return nil
