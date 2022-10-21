c1b4ab13a51d5ca3b868ae82159d801f1b551f41
ikura-hamu <harukikainuma@gmail.com>
2022-10-21 22:47:57 +0900

    gameFileUnimpremented削除

--- src/handler/v2/game_file.go
+++ src/handler/v2/game_file.go
@@ -22,24 +22,6 @@ func NewGameFile(gameFileService service.GameFileV2) *GameFile {
 	}
 }
 
-// gameFileUnimplemented
-// メソッドとして実装予定だが、未実装のもの
-// TODO: 実装
-type gameFileUnimplemented interface {
-	// ゲームファイルの作成
-	// (GET /games/{gameID}/files)
-	GetGameFiles(ctx echo.Context, gameID openapi.GameIDInPath) error
-	// ゲームファイル一覧の取得
-	// (POST /games/{gameID}/files)
-	PostGameFile(ctx echo.Context, gameID openapi.GameIDInPath) error
-	// ゲームファイルのバイナリの取得
-	// (GET /games/{gameID}/files/{gameFileID})
-	GetGameFile(ctx echo.Context, gameID openapi.GameIDInPath, gameFileID openapi.GameFileIDInPath) error
-	// ゲームファイルのメタ情報の取得
-	// (GET /games/{gameID}/files/{gameFileID}/meta)
-	GetGameFileMeta(ctx echo.Context, gameID openapi.GameIDInPath, gameFileID openapi.GameFileIDInPath) error
-}
-
 // ゲームファイル一覧の取得
 // (GET /games/{gameID}/files)
 func (gameFile GameFile) GetGameFiles(c echo.Context, gameID openapi.GameIDInPath) error {
@@ -76,7 +58,7 @@ func (gameFile GameFile) GetGameFiles(c echo.Context, gameID openapi.GameIDInPat
 		})
 	}
 
-	return c.JSON(http.StatusOK, files)
+	return c.JSON(http.StatusOK, resFiles)
 }
 
 // ゲームファイルの作成
