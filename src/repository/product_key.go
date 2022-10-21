49b17927cfda9ea8e81f274e74a8b20e7257a477
mazrean <shunsuke.w04@gmail.com>
2022-10-22 04:09:42 +0900

    product keyリポジトリ修正

--- src/repository/product_key.go
+++ src/repository/product_key.go
@@ -18,11 +18,14 @@ type ProductKey interface {
 	UpdateProductKey(ctx context.Context, productKey *domain.LauncherUser) error
 	// GetProductKeys
 	// プロダクトキー一覧の取得。
+	// ステータスに関わらず取得可能。
 	GetProductKeys(ctx context.Context, editionID values.LauncherVersionID, lockType LockType) ([]*domain.LauncherUser, error)
 	// GetProductKey
 	// プロダクトキーの取得。
+	// ステータスに関わらず取得可能。
 	GetProductKey(ctx context.Context, productKeyID values.LauncherUserID, lockType LockType) (*domain.LauncherUser, error)
 	// GetProductKeyByKey
 	// キーからのプロダクトキーの取得。
+	// ステータスに関わらず取得可能。
 	GetProductKeyByKey(ctx context.Context, key values.LauncherUserProductKey) (*domain.LauncherUser, error)
 }
