562415912e43aef343864fd9df118800d2443926
ikura-hamu <harukikainuma@gmail.com>
2022-10-22 15:14:27 +0900

    interface修正

--- src/service/v2_game_role.go
+++ src/service/v2_game_role.go
@@ -10,8 +10,7 @@ import (
 //go:generate go run github.com/golang/mock/mockgen -source=$GOFILE -destination=mock/${GOFILE} -package=mock
 
 type GameRoleV2 interface {
-	AddGameManagementRole(ctx context.Context, session *domain.OIDCSession, gameID values.GameID, userID values.TraPMemberID, role values.GameManagementRole) error
-	UpdateGameManagementRole(ctx context.Context, gameID values.GameID, userID values.TraPMemberID, role values.GameManagementRole) error
+	EditGameManagementRole(ctx context.Context, session *domain.OIDCSession, gameID values.GameID, userID values.TraPMemberID, role values.GameManagementRole) error
 	RemoveGameManagementRole(ctx context.Context, gameID values.GameID, userID values.TraPMemberID) error
 	UpdateGameAuth(ctx context.Context, session *domain.OIDCSession, gameID values.GameID) error
 	UpdateGameManagementRoleAuth(ctx context.Context, session *domain.OIDCSession, gameID values.GameID) error
