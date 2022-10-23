7cc13d81b9065966d583c938fee1b04917e48579
ikura-hamu <harukikainuma@gmail.com>
2022-10-23 23:52:25 +0900

    repositoryのAdminAuthV2にコメント追加

--- src/repository/v2_admin_auth.go
+++ src/repository/v2_admin_auth.go
@@ -9,7 +9,14 @@ import (
 //go:generate go run github.com/golang/mock/mockgen -source=$GOFILE -destination=mock/${GOFILE} -package=mock
 
 type AdminAuthV2 interface {
-	AddAdmin(ctx context.Context, userID values.TraPMemberID) ([]*values.TraPMemberID, error)
-	GetAdmins(ctx context.Context) ([]*values.TraPMemberID, error)
-	Delete(ctx context.Context, userID values.TraPMemberID) ([]*values.TraPMemberID, error)
+	//AddAdmin
+	//adminを追加
+	AddAdmin(ctx context.Context, userID values.TraPMemberID) error
+	//GetAdmins
+	//adminを全員取得してuserIDを返す。
+	GetAdmins(ctx context.Context) ([]values.TraPMemberID, error)
+	//DeleteAdmin
+	//adminを削除
+	//ユーザーが存在しない場合、ErrNoRecordDeletedを返す。
+	DeleteAdmin(ctx context.Context, userID values.TraPMemberID) error
 }
