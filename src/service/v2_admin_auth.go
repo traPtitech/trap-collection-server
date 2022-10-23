8926711b9395a8ffd9c3a8bb2a9d4c1dd26984c8
ikura-hamu <harukikainuma@gmail.com>
2022-10-23 23:19:03 +0900

    AdminAuthV2追加

---
+++ src/service/v2_admin_auth.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	"context"
+
+	"github.com/traPtitech/trap-collection-server/src/domain"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+)
+
+type AdminAuthV2 interface {
+	//AddAdmin
+	//adminの追加
+	AddAdmin(ctx context.Context, session *domain.OIDCSession, userID values.TraPMemberID) (*UserInfo, error)
+	//GetAdmins
+	//adminを全取得
+	GetAdmins(ctx context.Context, session *domain.OIDCSession) (*UserInfo, error)
+	//DeleteAdmin
+	//adminを削除
+	DeleteAdmin(ctx context.Context, session *domain.OIDCSession, userID values.TraPMemberID) (*UserInfo, error)
+}
