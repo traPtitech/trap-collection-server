d6c4882650e445f210bf77e00ff7adf67e518730
ikura-hamu <harukikainuma@gmail.com>
2022-10-24 13:37:13 +0900

    AddAdminにtransaction

--- src/service/v2/admin_auth.go
+++ src/service/v2/admin_auth.go
@@ -31,50 +31,56 @@ func NewAdminAuth(
 }
 
 func (aa *AdminAuth) AddAdmin(ctx context.Context, session *domain.OIDCSession, userID values.TraPMemberID) ([]*service.UserInfo, error) {
-	activeUsers, err := aa.user.getActiveUsers(ctx, session)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get active users: %w", err)
-	}
-	activeUsersMap := make(map[values.TraPMemberID]values.TraPMemberName, len(activeUsers))
-	for _, activeUser := range activeUsers {
-		activeUsersMap[activeUser.GetID()] = activeUser.GetName()
-	}
-	if _, ok := activeUsersMap[userID]; !ok {
-		return nil, service.ErrInvalidUserID
-	}
+	var adminInfos []*service.UserInfo
+	err := aa.db.Transaction(ctx, nil, func(ctx context.Context) error {
+		activeUsers, err := aa.user.getActiveUsers(ctx, session)
+		if err != nil {
+			return fmt.Errorf("failed to get active users: %w", err)
+		}
+		activeUsersMap := make(map[values.TraPMemberID]values.TraPMemberName, len(activeUsers))
+		for _, activeUser := range activeUsers {
+			activeUsersMap[activeUser.GetID()] = activeUser.GetName()
+		}
+		if _, ok := activeUsersMap[userID]; !ok {
+			return service.ErrInvalidUserID
+		}
 
-	adminIDs, err := aa.adminAuthRepository.GetAdmins(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get admins: %v", err)
-	}
+		adminIDs, err := aa.adminAuthRepository.GetAdmins(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to get admins: %v", err)
+		}
 
-	adminsMap := make(map[values.TraPMemberID]struct{})
-	for _, adminID := range adminIDs {
-		adminsMap[adminID] = struct{}{}
-	}
-	if _, ok := adminsMap[userID]; ok { //ユーザーがすでにadmin
-		return nil, service.ErrNoAdminsUpdated
-	}
+		adminsMap := make(map[values.TraPMemberID]struct{})
+		for _, adminID := range adminIDs {
+			adminsMap[adminID] = struct{}{}
+		}
+		if _, ok := adminsMap[userID]; ok { //ユーザーがすでにadmin
+			return service.ErrNoAdminsUpdated
+		}
 
-	err = aa.adminAuthRepository.AddAdmin(ctx, userID)
-	if err != nil {
-		return nil, fmt.Errorf("failed to add admin: %v", err)
-	}
+		err = aa.adminAuthRepository.AddAdmin(ctx, userID)
+		if err != nil {
+			return fmt.Errorf("failed to add admin: %v", err)
+		}
 
-	adminInfos := make([]*service.UserInfo, len(adminIDs))
-	for _, adminID := range adminIDs {
+		for _, adminID := range adminIDs {
+			adminInfos = append(adminInfos, service.NewUserInfo(
+				adminID,
+				activeUsersMap[adminID],
+				values.TrapMemberStatusActive,
+			))
+		}
 		adminInfos = append(adminInfos, service.NewUserInfo(
-			adminID,
-			activeUsersMap[adminID],
+			userID,
+			activeUsersMap[userID],
 			values.TrapMemberStatusActive,
 		))
-	}
-	adminInfos = append(adminInfos, service.NewUserInfo(
-		userID,
-		activeUsersMap[userID],
-		values.TrapMemberStatusActive,
-	))
+		return nil
+	})
 
+	if err != nil {
+		return nil, fmt.Errorf("failed in transaction: %v", err)
+	}
 	return adminInfos, nil
 }
 
