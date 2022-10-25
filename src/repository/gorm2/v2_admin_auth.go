8ddfaea08bf24900c6ed2e07da89252d53a4d195
ikura-hamu <harukikainuma@gmail.com>
2022-10-25 15:02:34 +0900

    AddAdmin実装

--- src/repository/gorm2/v2_admin_auth.go
+++ src/repository/gorm2/v2_admin_auth.go
@@ -1,5 +1,14 @@
 package gorm2
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/repository/gorm2/migrate"
+)
+
 type AdminAuth struct {
 	db *DB
 }
@@ -8,4 +17,21 @@ func NewAdminAuth(db *DB) *AdminAuth {
 	return &AdminAuth{
 		db: db,
 	}
+}
+
+func (aa *AdminAuth) AddAdmin(ctx context.Context, userID values.TraPMemberID) error {
+	db, err := aa.db.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db: %w", err)
+	}
+
+	adminTable := migrate.AdminTable{
+		UserID: uuid.UUID(userID),
+	}
+
+	err = db.Create(&adminTable).Error
+	if err != nil {
+		return fmt.Errorf("failed to add admin: %w", err)
+	}
+	return nil
 }
