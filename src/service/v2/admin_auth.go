42ab30694bb400dd8f674de8d63405d58f28aefa
ikura-hamu <harukikainuma@gmail.com>
2022-10-25 13:26:08 +0900

    sessionの有効期限を確認

--- src/service/v2/admin_auth.go
+++ src/service/v2/admin_auth.go
@@ -137,11 +137,15 @@ func (aa *AdminAuth) DeleteAdmin(ctx context.Context, session *domain.OIDCSessio
 }
 
 func (aa *AdminAuth) AdminAuthorize(ctx context.Context, session *domain.OIDCSession) error {
+	if session.IsExpired() {
+		return service.ErrOIDCSessionExpired
+	}
+
 	userInfo, err := aa.user.getMe(ctx, session)
 	if err != nil {
 		return fmt.Errorf("failed to get user: %w", err)
 	}
-	//TODO:session期限確認
+
 	adminsID, err := aa.adminAuthRepository.GetAdmins(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to get admins: %w", err)
