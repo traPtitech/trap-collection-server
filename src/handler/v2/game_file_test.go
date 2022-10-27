049008cd4a0fd752d57adaf72a322951597a4331
ikura-hamu <harukikainuma@gmail.com>
2022-10-27 10:46:23 +0900

    TestGetGameFile追加

--- src/handler/v2/game_file_test.go
+++ src/handler/v2/game_file_test.go
@@ -10,6 +10,7 @@ import (
 	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 	"time"
 
@@ -529,6 +530,108 @@ func TestPostGameFile(t *testing.T) {
 			assert.Equal(t, testCase.resFile.EntryPoint, resFile.EntryPoint)
 			assert.Equal(t, testCase.resFile.Md5, resFile.Md5)
 			assert.WithinDuration(t, testCase.resFile.CreatedAt, resFile.CreatedAt, time.Second)
+		})
+	}
+}
+
+func TestGetGameFile(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockGameFileService := mock.NewMockGameFileV2(ctrl)
+
+	gameFile := NewGameFile(mockGameFileService)
+
+	type test struct {
+		description    string
+		gameID         openapi.GameIDInPath
+		gameFileID     openapi.GameFileIDInPath
+		tmpURL         values.GameFileTmpURL
+		getGameFileErr error
+		resLocation    string
+		isErr          bool
+		err            error
+		statusCode     int
+	}
+
+	urlLink, err := url.Parse("https://example.com")
+	if err != nil {
+		t.Fatalf("failed to encode file: %v", err)
+	}
+
+	testCases := []test{
+		{
+			description: "特に問題ないのでエラーなし",
+			gameID:      uuid.UUID(values.NewGameID()),
+			gameFileID:  uuid.UUID(values.NewGameFileID()),
+			tmpURL:      values.NewGameFileTmpURL(urlLink),
+			resLocation: "https://example.com",
+		},
+		{
+			description:    "GetGameFileがErrInvalidGameIDなので404",
+			gameID:         uuid.UUID(values.NewGameID()),
+			gameFileID:     uuid.UUID(values.NewGameFileID()),
+			getGameFileErr: service.ErrInvalidGameID,
+			isErr:          true,
+			statusCode:     http.StatusNotFound,
+		},
+		{
+			description:    "GetGameFileがErrInvalidGameFileIDなので404",
+			gameID:         uuid.UUID(values.NewGameID()),
+			gameFileID:     uuid.UUID(values.NewGameFileID()),
+			getGameFileErr: service.ErrInvalidGameFileID,
+			isErr:          true,
+			statusCode:     http.StatusNotFound,
+		},
+		{
+			description:    "GetGameFileがエラーなので500",
+			gameID:         uuid.UUID(values.NewGameID()),
+			gameFileID:     uuid.UUID(values.NewGameFileID()),
+			getGameFileErr: errors.New("error"),
+			isErr:          true,
+			statusCode:     http.StatusInternalServerError,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			e := echo.New()
+			req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/api/v2/games/%s/files", testCase.gameID), nil)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+
+			mockGameFileService.
+				EXPECT().
+				GetGameFile(gomock.Any(), gomock.Any(), gomock.Any()).
+				Return(testCase.tmpURL, testCase.getGameFileErr)
+
+			err := gameFile.GetGameFile(c, testCase.gameID, testCase.gameFileID)
+
+			if testCase.isErr {
+				if testCase.statusCode != 0 {
+					var httpError *echo.HTTPError
+					if errors.As(err, &httpError) {
+						assert.Equal(t, testCase.statusCode, httpError.Code)
+					} else {
+						t.Errorf("error is not *echo.HTTPError")
+					}
+				} else if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil || testCase.isErr {
+				return
+			}
+
+			assert.Equal(t, http.StatusSeeOther, rec.Code)
+
+			assert.Equal(t, testCase.resLocation, rec.Header().Get("Location"))
 		})
 	}
 }
