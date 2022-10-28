fe17f5f9b6e27004d9edaf3f3cdb16d00fc11c8d
ikura-hamu <harukikainuma@gmail.com>
2022-10-28 19:42:47 +0900

    TestUpdateEdition追加

--- src/repository/gorm2/edition_test.go
+++ src/repository/gorm2/edition_test.go
@@ -12,8 +12,10 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/traPtitech/trap-collection-server/src/domain"
 	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/repository"
 	"github.com/traPtitech/trap-collection-server/src/repository/gorm2/migrate"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func TestSaveEdition(t *testing.T) {
@@ -141,4 +143,285 @@ func TestSaveEdition(t *testing.T) {
 		assert.Equal(t, questionnaireURL, values.LauncherVersionQuestionnaireURL(savedURL))
 		assert.WithinDuration(t, testCase.edition.GetCreatedAt(), edition.CreatedAt, time.Second)
 	}
+}
+
+func TestUpdateEdition(t *testing.T) {
+
+	ctx := context.Background()
+
+	db, err := testDB.getDB(ctx)
+	if err != nil {
+		t.Fatalf("failed to get db: %+v\n", err)
+	}
+
+	editionRepository := NewEdition(testDB)
+
+	now := time.Now()
+	urlLink1, err := url.Parse("https://example.com")
+	if err != nil {
+		t.Fatalf("failed to encode url: %v", err)
+	}
+
+	urlLink2, err := url.Parse("https://example2.com")
+	if err != nil {
+		t.Fatalf("failed to encode url: %v", err)
+	}
+
+	editionID1 := values.NewLauncherVersionID()
+	editionID2 := values.NewLauncherVersionID()
+
+	type test struct {
+		description    string
+		edition        *domain.LauncherVersion
+		beforeEditions []migrate.EditionTable2
+		afterEditions  []migrate.EditionTable2
+		isErr          bool
+		err            error
+	}
+
+	testCases := []test{
+		{
+			description: "特に問題ないのでエラーなし",
+			edition: domain.NewLauncherVersionWithQuestionnaire(
+				editionID1,
+				"test2",
+				urlLink2,
+				now,
+			),
+			beforeEditions: []migrate.EditionTable2{
+				{
+					ID:   uuid.UUID(editionID1),
+					Name: "test1",
+					QuestionnaireURL: sql.NullString{
+						String: urlLink1.String(),
+						Valid:  true,
+					},
+					CreatedAt: now,
+				},
+			},
+			afterEditions: []migrate.EditionTable2{
+				{
+					ID:   uuid.UUID(editionID1),
+					Name: "test2",
+					QuestionnaireURL: sql.NullString{
+						String: urlLink2.String(),
+						Valid:  true,
+					},
+					CreatedAt: now,
+				},
+			},
+		},
+		{
+			description: "別のエディションが存在してもエラーなし",
+			edition: domain.NewLauncherVersionWithQuestionnaire(
+				editionID1,
+				"test3",
+				urlLink2,
+				now,
+			),
+			beforeEditions: []migrate.EditionTable2{
+				{
+					ID:   uuid.UUID(editionID1),
+					Name: "test1",
+					QuestionnaireURL: sql.NullString{
+						String: urlLink1.String(),
+						Valid:  true,
+					},
+					CreatedAt: now,
+				},
+				{
+					ID:   uuid.UUID(editionID2),
+					Name: "test2",
+					QuestionnaireURL: sql.NullString{
+						String: urlLink1.String(),
+						Valid:  true,
+					},
+					CreatedAt: now.Add(-time.Hour),
+				},
+			},
+			afterEditions: []migrate.EditionTable2{
+				{
+					ID:   uuid.UUID(editionID1),
+					Name: "test3",
+					QuestionnaireURL: sql.NullString{
+						String: urlLink2.String(),
+						Valid:  true,
+					},
+					CreatedAt: now,
+				},
+				{
+					ID:   uuid.UUID(editionID2),
+					Name: "test2",
+					QuestionnaireURL: sql.NullString{
+						String: urlLink1.String(),
+						Valid:  true,
+					},
+					CreatedAt: now.Add(-time.Hour),
+				},
+			},
+		},
+		{
+			description: "アンケートURLが存在しなくなってもエラーなし",
+			edition: domain.NewLauncherVersionWithoutQuestionnaire(
+				editionID1,
+				"test2",
+				now,
+			),
+			beforeEditions: []migrate.EditionTable2{
+				{
+					ID:   uuid.UUID(editionID1),
+					Name: "test1",
+					QuestionnaireURL: sql.NullString{
+						String: urlLink1.String(),
+						Valid:  true,
+					},
+					CreatedAt: now,
+				},
+			},
+			afterEditions: []migrate.EditionTable2{
+				{
+					ID:   uuid.UUID(editionID1),
+					Name: "test2",
+					QuestionnaireURL: sql.NullString{
+						Valid: false,
+					},
+					CreatedAt: now,
+				},
+			},
+		},
+		{
+			description: "アンケートURLが存在するようになってもエラーなし",
+			edition: domain.NewLauncherVersionWithQuestionnaire(
+				editionID1,
+				"test2",
+				urlLink1,
+				now,
+			),
+			beforeEditions: []migrate.EditionTable2{
+				{
+					ID:   uuid.UUID(editionID1),
+					Name: "test1",
+					QuestionnaireURL: sql.NullString{
+						Valid: false,
+					},
+					CreatedAt: now,
+				},
+			},
+			afterEditions: []migrate.EditionTable2{
+				{
+					ID:   uuid.UUID(editionID1),
+					Name: "test2",
+					QuestionnaireURL: sql.NullString{
+						String: urlLink1.String(),
+						Valid:  true,
+					},
+					CreatedAt: now,
+				},
+			},
+		},
+		{
+			description: "アンケートURLが変わってもエラーなし",
+			edition: domain.NewLauncherVersionWithQuestionnaire(
+				editionID1,
+				"test2",
+				urlLink2,
+				now,
+			),
+			beforeEditions: []migrate.EditionTable2{
+				{
+					ID:   uuid.UUID(editionID1),
+					Name: "test1",
+					QuestionnaireURL: sql.NullString{
+						String: urlLink1.String(),
+						Valid:  true,
+					},
+					CreatedAt: now,
+				},
+			},
+			afterEditions: []migrate.EditionTable2{
+				{
+					ID:   uuid.UUID(editionID1),
+					Name: "test2",
+					QuestionnaireURL: sql.NullString{
+						String: urlLink2.String(),
+						Valid:  true,
+					},
+					CreatedAt: now,
+				},
+			},
+		},
+		{
+			description: "エディションが無いのでErrNoRecordUpdated",
+			edition: domain.NewLauncherVersionWithQuestionnaire(
+				editionID1,
+				"test2",
+				urlLink1,
+				now,
+			),
+			beforeEditions: []migrate.EditionTable2{},
+			afterEditions:  []migrate.EditionTable2{},
+			isErr:          true,
+			err:            repository.ErrNoRecordUpdated,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			defer func() {
+				err := db.
+					Unscoped().
+					Session(&gorm.Session{
+						AllowGlobalUpdate: true,
+					}).
+					Delete(&migrate.EditionTable2{}).Error
+				if err != nil {
+					t.Fatalf("failed to delete edition: %+v\n", err)
+				}
+			}()
+
+			if testCase.beforeEditions != nil && len(testCase.beforeEditions) != 0 {
+				err := db.
+					Session(&gorm.Session{
+						Logger: logger.Default.LogMode(logger.Info),
+					}).
+					Create(&testCase.beforeEditions).Error
+				if err != nil {
+					t.Fatalf("failed to create edition: %+v\n", err)
+				}
+			}
+
+			err := editionRepository.UpdateEdition(ctx, testCase.edition)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			var editions []migrate.EditionTable2
+			err = db.
+				Session(&gorm.Session{}).
+				Order("created_at desc").
+				Find(&editions).Error
+			if err != nil {
+				t.Fatalf("failed to get editions: %+v", err)
+			}
+
+			assert.Len(t, editions, len(testCase.afterEditions))
+			for i, edition := range editions {
+				assert.Equal(t, testCase.afterEditions[i].ID, edition.ID)
+				assert.Equal(t, testCase.afterEditions[i].Name, edition.Name)
+				assert.Equal(t, testCase.afterEditions[i].QuestionnaireURL.String, edition.QuestionnaireURL.String)
+				assert.WithinDuration(t, testCase.afterEditions[i].CreatedAt, edition.CreatedAt, time.Second)
+			}
+		})
+	}
+
 }
