d885f60a11f45ce601ade94d1c95459803fa0cfd
ikura-hamu <harukikainuma@gmail.com>
2022-12-22 21:51:09 +0900

    PostAdmin実装

--- src/handler/v2/admin.go
+++ src/handler/v2/admin.go
@@ -1,11 +1,13 @@
 package v2
 
 import (
+	"errors"
 	"log"
 	"net/http"
 
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
 	"github.com/traPtitech/trap-collection-server/src/handler/v2/openapi"
 	"github.com/traPtitech/trap-collection-server/src/service"
 )
@@ -64,4 +66,46 @@ func (a *Admin) GetAdmins(ctx echo.Context) error {
 		})
 	}
 	return ctx.JSON(http.StatusOK, responseAdmins)
+}
+
+// traP Collection全体の管理者追加
+// (POST /admins)
+func (a *Admin) PostAdmin(ctx echo.Context) error {
+	session, err := a.session.get(ctx)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusUnauthorized, "no session")
+	}
+
+	authSession, err := a.session.getAuthSession(session)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusUnauthorized, "no auth session")
+	}
+
+	req := &openapi.PostAdminJSONRequestBody{}
+	err = ctx.Bind(req)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "bad request body")
+	}
+	newAdminID := values.NewTrapMemberID(req.Id)
+
+	adminInfos, err := a.adminService.AddAdmin(ctx.Request().Context(), authSession, values.TraPMemberID(newAdminID))
+	if errors.Is(err, service.ErrInvalidUserID) {
+		return echo.NewHTTPError(http.StatusBadRequest, "not active user")
+	}
+	if errors.Is(err, service.ErrNoAdminsUpdated) {
+		return echo.NewHTTPError(http.StatusBadRequest, "already admin")
+	}
+	if err != nil {
+		log.Printf("error: failed to add admin: %v", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to add admin")
+	}
+
+	res := make([]openapi.User, 0, len(adminInfos))
+	for _, adminInfo := range adminInfos {
+		res = append(res,
+			openapi.User{Id: uuid.UUID(adminInfo.GetID()), Name: string(adminInfo.GetName())},
+		)
+	}
+
+	return ctx.JSON(http.StatusOK, res)
 }
