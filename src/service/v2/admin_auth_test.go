9404bd3dbb6a8be58d6aa72d5a7d84a87f27867e
ikura-hamu <harukikainuma@gmail.com>
2023-01-03 18:52:59 +0900

    TestDeleteAdmin追加

--- src/service/v2/admin_auth_test.go
+++ src/service/v2/admin_auth_test.go
@@ -13,6 +13,7 @@ import (
 	mockCache "github.com/traPtitech/trap-collection-server/src/cache/mock"
 	"github.com/traPtitech/trap-collection-server/src/domain"
 	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/repository"
 	mockRepository "github.com/traPtitech/trap-collection-server/src/repository/mock"
 	"github.com/traPtitech/trap-collection-server/src/service"
 )
@@ -284,6 +285,228 @@ func TestGetAdmins(t *testing.T) {
 			}
 
 			adminInfos, err := adminAuthService.GetAdmins(ctx, testCase.authSession)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			assert.Len(t, adminInfos, len(testCase.expectedAdminInfos))
+			for i, adminInfo := range adminInfos {
+				assert.Equal(t, testCase.expectedAdminInfos[i], adminInfo)
+			}
+		})
+
+	}
+}
+
+func TestDeleteAdmin(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockDB := mockRepository.NewMockDB(ctrl)
+	mockAdminAuthRepository := mockRepository.NewMockAdminAuthV2(ctrl)
+
+	mockUserCache := mockCache.NewMockUser(ctrl)
+	mockUserAuth := mockAuth.NewMockUser(ctrl)
+
+	user := NewUser(mockUserAuth, mockUserCache)
+
+	adminAuthService := NewAdminAuth(mockDB, mockAdminAuthRepository, user)
+
+	type test struct {
+		description           string
+		authSession           *domain.OIDCSession
+		getMeErr              error
+		myInfo                *service.UserInfo
+		executeGetActiveUsers bool
+		getActiveUsersErr     error
+		userID                values.TraPMemberID
+		executeDeleteAdmin    bool
+		DeleteAdminsErr       error
+		executeGetAdmins      bool
+		afterAdminIDs         []values.TraPMemberID
+		GetAdminsErr          error
+		expectedAdminInfos    []*service.UserInfo
+		isErr                 bool
+		err                   error
+	}
+
+	userID1 := values.NewTrapMemberID(uuid.New())
+	userID2 := values.NewTrapMemberID(uuid.New())
+	userID3 := values.NewTrapMemberID(uuid.New())
+
+	userInfo1 := service.NewUserInfo(userID1, "ikura-hamu", values.TrapMemberStatusActive)
+	userInfo2 := service.NewUserInfo(userID2, "mazrean", values.TrapMemberStatusActive)
+	userInfo3 := service.NewUserInfo(userID3, "pikachu", values.TrapMemberStatusActive)
+
+	activeUsers := []*service.UserInfo{userInfo1, userInfo2, userInfo3}
+
+	testCases := []test{
+		{
+			description: "特に問題ないのでエラー無し",
+			authSession: domain.NewOIDCSession(
+				"access token",
+				time.Now().Add(time.Hour),
+			),
+			myInfo:                userInfo1,
+			executeGetActiveUsers: true,
+			userID:                userID2,
+			executeDeleteAdmin:    true,
+			executeGetAdmins:      true,
+			afterAdminIDs:         []values.TraPMemberID{userID1},
+			expectedAdminInfos:    []*service.UserInfo{userInfo1},
+		},
+		{
+			description: "getMeがエラーなのでエラー",
+			authSession: domain.NewOIDCSession(
+				"access token",
+				time.Now().Add(time.Hour),
+			),
+			getMeErr: errors.New("test"),
+			isErr:    true,
+		},
+		{
+			description: "自分を削除しようとしているのでErrCannotDeleteMeFromAdmins",
+			authSession: domain.NewOIDCSession(
+				"access token",
+				time.Now().Add(time.Hour),
+			),
+			myInfo: userInfo1,
+			userID: userID1,
+			isErr:  true,
+			err:    service.ErrCannotDeleteMeFromAdmins,
+		},
+		{
+			description: "getActiveUsersがエラーなのでエラー",
+			authSession: domain.NewOIDCSession(
+				"access token",
+				time.Now().Add(time.Hour),
+			),
+			myInfo:                userInfo1,
+			executeGetActiveUsers: true,
+			userID:                userID2,
+			getActiveUsersErr:     errors.New("test"),
+			isErr:                 true,
+		},
+		{
+			description: "ユーザーが存在しないのでErrInvalidUserID",
+			authSession: domain.NewOIDCSession(
+				"access token",
+				time.Now().Add(time.Hour),
+			),
+			myInfo:                userInfo1,
+			executeGetActiveUsers: true,
+			userID:                values.NewTrapMemberID(uuid.New()),
+			isErr:                 true,
+			err:                   service.ErrInvalidUserID,
+		},
+		{
+			description: "DeleteAdminがErrNoRecordDeletedなのでErrNotAdmin",
+			authSession: domain.NewOIDCSession(
+				"access token",
+				time.Now().Add(time.Hour),
+			),
+			myInfo:                userInfo1,
+			executeGetActiveUsers: true,
+			userID:                userID2,
+			executeDeleteAdmin:    true,
+			DeleteAdminsErr:       repository.ErrNoRecordDeleted,
+			isErr:                 true,
+			err:                   service.ErrNotAdmin,
+		},
+		{
+			description: "DeleteAdminがエラーなのでエラー",
+			authSession: domain.NewOIDCSession(
+				"access token",
+				time.Now().Add(time.Hour),
+			),
+			myInfo:                userInfo1,
+			executeGetActiveUsers: true,
+			userID:                userID2,
+			executeDeleteAdmin:    true,
+			DeleteAdminsErr:       errors.New("test"),
+			isErr:                 true,
+		},
+		{
+			description: "GetAdminsがエラーなのでエラー",
+			authSession: domain.NewOIDCSession(
+				"access token",
+				time.Now().Add(time.Hour),
+			),
+			myInfo:                userInfo1,
+			executeGetActiveUsers: true,
+			userID:                userID2,
+			executeDeleteAdmin:    true,
+			executeGetAdmins:      true,
+			GetAdminsErr:          errors.New("test"),
+			isErr:                 true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			if testCase.getMeErr != nil {
+				mockUserCache.
+					EXPECT().
+					GetMe(gomock.Any(), gomock.Any()).
+					Return(nil, errors.New("test"))
+				mockUserAuth.
+					EXPECT().
+					GetMe(gomock.Any(), gomock.Any()).
+					Return(nil, errors.New("test"))
+			} else {
+				mockUserCache.
+					EXPECT().
+					GetMe(gomock.Any(), gomock.Any()).
+					Return(testCase.myInfo, nil)
+			}
+
+			if testCase.executeGetActiveUsers {
+				if testCase.getActiveUsersErr != nil {
+					mockUserCache.
+						EXPECT().
+						GetActiveUsers(gomock.Any()).
+						Return(nil, errors.New("test"))
+					mockUserAuth.
+						EXPECT().
+						GetActiveUsers(gomock.Any(), gomock.Any()).
+						Return(nil, errors.New("test"))
+				} else {
+					mockUserCache.
+						EXPECT().
+						GetActiveUsers(gomock.Any()).
+						Return(activeUsers, nil)
+				}
+			}
+
+			if testCase.executeDeleteAdmin {
+				mockAdminAuthRepository.
+					EXPECT().
+					DeleteAdmin(ctx, testCase.userID).
+					Return(testCase.DeleteAdminsErr)
+			}
+
+			if testCase.executeGetAdmins {
+				mockAdminAuthRepository.
+					EXPECT().
+					GetAdmins(ctx).
+					Return(testCase.afterAdminIDs, testCase.GetAdminsErr)
+			}
+
+			adminInfos, err := adminAuthService.DeleteAdmin(ctx, testCase.authSession, testCase.userID)
 
 			if testCase.isErr {
 				if testCase.err == nil {
