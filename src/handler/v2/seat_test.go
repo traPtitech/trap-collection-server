609a74f9ceae831a94d60ccbb58c63ee54e1b826
pikachu0310 <sunnon@i.softbank.jp>
2023-03-21 19:53:09 +0900

    TestSeat_PostSeatの実装

--- src/handler/v2/seat_test.go
+++ src/handler/v2/seat_test.go
@@ -1,6 +1,7 @@
 package v2
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"github.com/golang/mock/gomock"
@@ -10,6 +11,7 @@ import (
 	"github.com/traPtitech/trap-collection-server/src/handler/v2/openapi"
 	"github.com/traPtitech/trap-collection-server/src/service"
 	"github.com/traPtitech/trap-collection-server/src/service/mock"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -104,27 +106,111 @@ func TestSeat_GetSeats(t *testing.T) {
 }
 
 func TestSeat_PostSeat(t *testing.T) {
-	type fields struct {
-		seatService service.Seat
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockSeatService := mock.NewMockSeat(ctrl)
+	seatHandler := NewSeat(mockSeatService)
+
+	type test struct {
+		description      string
+		seats            []*domain.Seat
+		res              []*openapi.Seat
+		isBadRequestBody bool
+		newSeat          *openapi.PostSeatJSONRequestBody
+		executePostSeats bool
+		postSeatsErr     error
+		isErr            bool
+		err              error
+		statusCode       int
 	}
-	type args struct {
-		c echo.Context
+
+	testCases := []test{
+		{
+			description:      "正常に席数の変更に成功200",
+			newSeat:          &openapi.PostSeatRequest{Num: 1},
+			executePostSeats: true,
+			statusCode:       http.StatusOK,
+			seats:            []*domain.Seat{domain.NewSeat(1, 2)},
+			res:              []*openapi.Seat{{Id: 1, Status: "in-use"}},
+		},
+		{
+			description:      "postSeatsがエラーなので500",
+			executePostSeats: true,
+			postSeatsErr:     errors.New("error"),
+			isErr:            true,
+			statusCode:       http.StatusInternalServerError,
+		},
+		{
+			description:      "リクエストが不正なので400",
+			isBadRequestBody: true,
+			isErr:            true,
+			statusCode:       http.StatusBadRequest,
+		},
 	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			seat := &Seat{
-				seatService: tt.fields.seatService,
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			e := echo.New()
+
+			reqBody := new(bytes.Buffer)
+			if !testCase.isBadRequestBody {
+				err := json.NewEncoder(reqBody).Encode(testCase.newSeat)
+				if err != nil {
+					log.Printf("failed to create request body")
+					t.Fatal(err)
+				}
+			} else {
+				reqBody = bytes.NewBufferString("bad requset body")
 			}
-			if err := seat.PostSeat(tt.args.c); (err != nil) != tt.wantErr {
-				t.Errorf("Seat.PostSeat() error = %v, wantErr %v", err, tt.wantErr)
+
+			req := httptest.NewRequest(http.MethodPost, "/seats", reqBody)
+			req.Header.Set(echo.HeaderContentType, "application/json")
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+
+			if testCase.executePostSeats {
+				mockSeatService.
+					EXPECT().
+					UpdateSeatNum(gomock.Any(), gomock.Any()).
+					Return(testCase.seats, testCase.postSeatsErr)
+			}
+
+			err := seatHandler.PostSeat(c)
+
+			if testCase.isErr {
+				if testCase.statusCode != 0 {
+					var httpError *echo.HTTPError
+					if errors.As(err, &httpError) {
+						assert.Equal(t, testCase.statusCode, httpError.Code)
+					} else {
+						t.Errorf("error is not *echo.HTTPError")
+					}
+				} else if testCase.err == nil {
+					assert.Error(t, err)
+				} else if !errors.Is(err, testCase.err) {
+					t.Errorf("error must be %v, but actual is %v", testCase.err, err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+			if err != nil {
+				return
+			}
+
+			var resSeats []*openapi.Seat
+			err = json.NewDecoder(rec.Body).Decode(&resSeats)
+			if err != nil {
+				t.Fatalf("failed to decode response body: %v", err)
+			}
+
+			assert.Equal(t, testCase.res, resSeats)
+			assert.Equal(t, len(testCase.res), len(resSeats))
+			for i, seat := range resSeats {
+				assert.Equal(t, seat.Id, resSeats[i].Id)
+				assert.Equal(t, seat.Status, resSeats[i].Status)
 			}
 		})
 	}
