e32f8cd330e55823cf3e8557559dd95f2001fcf7
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2024-09-23 15:48:15 +0900

    :arrow_up: oapi-codegenを新しいリポジトリのやつに移行

--- tools.go
+++ tools.go
@@ -5,8 +5,8 @@ package main
 
 import (
 	_ "github.com/cosmtrek/air"
-	_ "github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen"
 	_ "github.com/go-task/task/v3/cmd/task"
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/google/wire/cmd/wire"
+	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen"
 )
