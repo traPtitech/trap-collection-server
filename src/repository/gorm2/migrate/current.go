e7bb48a450a52b058b06433623a6e76af03fab30
masky5859 <mathsuky2406@gmail.com>
2025-01-13 21:45:24 +0900

    fix:v14に定義していたversion名のunique制約をv15に移す

--- src/repository/gorm2/migrate/current.go
+++ src/repository/gorm2/migrate/current.go
@@ -43,8 +43,8 @@ const (
 
 // アプリケーションのv2
 type (
-	GameTable2              = gameTable2V14 // 実際に使用されるテーブルはv1のGameTableと同一
-	GameVersionTable2       = gameVersionTable2V14
+	GameTable2              = gameTable2V15 // 実際に使用されるテーブルはv1のGameTableと同一
+	GameVersionTable2       = gameVersionTable2V15
 	GameFileTable2          = gameFileTable2V5
 	GameImageTable2         = gameImageTable2V2
 	GameVideoTable2         = gameVideoTable2V2
@@ -55,7 +55,7 @@ type (
 	AdminTable              = adminTable
 	SeatTable2              = seatTableV9
 	SeatStatusTable2        = seatStatusTableV9
-	GameGenreTable          = gameGenreTableV14
+	GameGenreTable          = gameGenreTableV15
 	GameVisibilityTypeTable = gameVisibilityTypeTableV11
 )
 
