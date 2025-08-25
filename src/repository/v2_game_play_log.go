dfa419220a7a8934774587f7614a6ddb867e30f2
masky5859 <mathsuky2406@gmail.com>
2025-08-26 03:43:59 +0900

    fix: repositoryの説明を追加

--- src/repository/v2_game_play_log.go
+++ src/repository/v2_game_play_log.go
@@ -13,20 +13,25 @@ import (
 type GamePlayLogV2 interface {
 	// CreateGamePlayLog
 	// 新しいゲームプレイログを作成する。
+	// 引数として与えられたplayLogのIDが既に存在する場合，ErrDuplicatedUniqueKeyを返す
 	CreateGamePlayLog(ctx context.Context, playLog *domain.GamePlayLog) error
 	// GetGamePlayLog
 	// 指定されたIDのゲームプレイログを取得する。
+	// 該当するプレイログが存在しない場合，ErrRecordNotFoundを返す
 	GetGamePlayLog(ctx context.Context, playLogID values.GamePlayLogID) (*domain.GamePlayLog, error)
 	// UpdateGamePlayLogEndTime
 	// 指定されたIDのゲームプレイログの終了時刻を更新する。
+	// 該当するプレイログが存在しない場合，ErrNoRecordUpdatedを返す
 	UpdateGamePlayLogEndTime(ctx context.Context, playLogID values.GamePlayLogID, endTime time.Time) error
 	// GetGamePlayStats
 	// 指定されたゲームと期間のプレイ統計を取得する。
 	// gameVersionIDがnilの場合、そのゲームのすべてのバージョンの統計を取得する。
 	// start〜endの期間でフィルタリングする。
+	// 統計データが存在しない場合でも空の統計を返すようにする。エラーは発生しない
 	GetGamePlayStats(ctx context.Context, gameID values.GameID, gameVersionID *values.GameVersionID, start, end time.Time) (*domain.GamePlayStats, error)
 	// GetEditionPlayStats
 	// 指定されたエディションと期間のプレイ統計を取得する。
 	// start〜endの期間でフィルタリングする。
+	// 該当するエディションが存在しない場合，ErrRecordNotFoundを返す
 	GetEditionPlayStats(ctx context.Context, editionID values.LauncherVersionID, start, end time.Time) (*domain.EditionPlayStats, error)
 }
