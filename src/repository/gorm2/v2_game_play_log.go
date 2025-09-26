2d96a1db90fc9b370d55269a1d12b500320f2d1e
masky5859 <mathsuky2406@gmail.com>
2025-09-26 23:08:42 +0900

    repositoryのCreateGamePlayLogを実装

--- src/repository/gorm2/v2_game_play_log.go
+++ src/repository/gorm2/v2_game_play_log.go
@@ -2,10 +2,16 @@ package gorm2
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 	"time"
 
+	"github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
 	"github.com/traPtitech/trap-collection-server/src/domain"
 	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/repository"
+	"github.com/traPtitech/trap-collection-server/src/repository/gorm2/schema"
 )
 
 type GamePlayLogV2 struct {
@@ -19,8 +25,33 @@ func NewGamePlayLogV2(db *DB) *GamePlayLogV2 {
 }
 
 func (g *GamePlayLogV2) CreateGamePlayLog(ctx context.Context, playLog *domain.GamePlayLog) error {
-	// TODO: interfaceのコメントを参考に実装を行う
-	panic("not implemented")
+
+	db, err := g.db.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db: %w", err)
+	}
+
+	var endTime sql.NullTime
+
+	gamePlayLogTable := schema.GamePlayLogTable{
+		ID:            uuid.UUID(playLog.GetID()),
+		EditionID:     uuid.UUID(playLog.GetEditionID()),
+		GameID:        uuid.UUID(playLog.GetGameID()),
+		GameVersionID: uuid.UUID(playLog.GetGameVersionID()),
+		StartTime:     playLog.GetStartTime(),
+		EndTime:       endTime,
+		CreatedAt:     playLog.GetCreatedAt(),
+		UpdatedAt:     playLog.GetUpdatedAt(),
+	}
+
+	err = db.Create(&gamePlayLogTable).Error
+	if err != nil {
+		if mysqlErr, ok := err.(*mysql.MySQLError); ok && mysqlErr.Number == 1062 {
+			return repository.ErrDuplicatedUniqueKey
+		}
+		return fmt.Errorf("failed to create game play log: %w", err)
+	}
+	return nil
 }
 
 func (g *GamePlayLogV2) GetGamePlayLog(ctx context.Context, playLogID values.GamePlayLogID) (*domain.GamePlayLog, error) {
