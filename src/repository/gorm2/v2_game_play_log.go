7efb45f5558008f6dcfa5b0de6ec6c00d34fb5f3
ebisen1444 <0294ryotaro@gmail.com>
2025-09-30 22:59:05 +0900

    関数は書いたのでテストを書こうとしているところ

--- src/repository/gorm2/v2_game_play_log.go
+++ src/repository/gorm2/v2_game_play_log.go
@@ -2,10 +2,14 @@ package gorm2
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/traPtitech/trap-collection-server/src/domain"
 	"github.com/traPtitech/trap-collection-server/src/domain/values"
+	"github.com/traPtitech/trap-collection-server/src/repository"
+	"github.com/traPtitech/trap-collection-server/src/repository/gorm2/schema"
+	"gorm.io/gorm"
 )
 
 type GamePlayLogV2 struct {
@@ -24,8 +28,37 @@ func (g *GamePlayLogV2) CreateGamePlayLog(ctx context.Context, playLog *domain.G
 }
 
 func (g *GamePlayLogV2) GetGamePlayLog(ctx context.Context, playLogID values.GamePlayLogID) (*domain.GamePlayLog, error) {
-	// TODO: interfaceのコメントを参考に実装を行う
-	panic("not implemented")
+	db, err := g.db.getDB(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var gamePlayLog schema.GamePlayLogTable //migrateではなくschemaに定義されている構造体を使う
+	err = db.
+		Where("id = ?", playLogID). //playLogIDに合致したレコードを取得
+		First(&gamePlayLog).Error //1件を取得
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, repository.ErrRecordNotFound
+		}
+		return nil, err
+	}
+
+	var endTime *time.Time // endTimeはNULL許容なのでポインタで扱う 
+	if gamePlayLog.EndTime.Valid {
+		endTime = &gamePlayLog.EndTime.Time
+	}
+
+	return domain.NewGamePlayLog(
+		values.GamePlayLogID(gamePlayLog.ID),
+		values.LauncherVersionID(gamePlayLog.EditionID),
+		values.GameID(gamePlayLog.GameID),
+		values.GameVersionID(gamePlayLog.GameVersionID),
+		gamePlayLog.StartTime,
+		endTime,
+		gamePlayLog.CreatedAt,
+		gamePlayLog.UpdatedAt,
+	), nil
 }
 
 func (g *GamePlayLogV2) UpdateGamePlayLogEndTime(ctx context.Context, playLogID values.GamePlayLogID, endTime time.Time) error {
