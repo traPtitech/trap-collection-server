4a904eed7e66a269597d78df22a65f44932c74a1
ebisen1444 <0294ryotaro@gmail.com>
2025-10-02 21:22:22 +0900

    合計回数 時間のみを実装 テストは一旦適当に作成

--- src/repository/gorm2/v2_game_play_log.go
+++ src/repository/gorm2/v2_game_play_log.go
@@ -18,17 +18,23 @@ func NewGamePlayLogV2(db *DB) *GamePlayLogV2 {
 	}
 }
 
-func (g *GamePlayLogV2) CreateGamePlayLog(ctx context.Context, playLog *domain.GamePlayLog) error {
+func (g *GamePlayLogV2) CreateGamePlayLog(_ context.Context, _ *domain.GamePlayLog) error {
 	// TODO: interfaceのコメントを参考に実装を行う
 	panic("not implemented")
 }
 
-func (g *GamePlayLogV2) GetGamePlayLog(ctx context.Context, playLogID values.GamePlayLogID) (*domain.GamePlayLog, error) {
+func (g *GamePlayLogV2) GetGamePlayLog(_ context.Context, _ values.GamePlayLogID) (*domain.GamePlayLog, error) {
 	// TODO: interfaceのコメントを参考に実装を行う
 	panic("not implemented")
 }
 
-func (g *GamePlayLogV2) UpdateGamePlayLogEndTime(ctx context.Context, playLogID values.GamePlayLogID, endTime time.Time) error {
+func (g *GamePlayLogV2) UpdateGamePlayLogEndTime(_ context.Context, _ values.GamePlayLogID, _ time.Time) error {
+	// TODO: interfaceのコメントを参考に実装を行う
+
+	panic("not implemented")
+}
+
+func (g *GamePlayLogV2) GetGamePlayStats(ctx context.Context, gameID values.GameID, gameVersionID *values.GameVersionID, start, end time.Time) (*domain.GamePlayStats, error) {
 	// TODO: interfaceのコメントを参考に実装を行う
 	// GetGamePlayStats
 	// 指定されたゲームと期間のプレイ統計を取得する。
@@ -36,16 +42,37 @@ func (g *GamePlayLogV2) UpdateGamePlayLogEndTime(ctx context.Context, playLogID 
 	// start〜endの期間でフィルタリングする。
 	// 統計データが存在しない場合でも空の統計を返すようにする。エラーは発生しない
 
+	db, err := g.db.getDB(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	stats := db.Where("game_id = ?", gameID).Where("start_time >= ? AND start_time < ?", start, end)
+
+	if gameVersionID != nil {
+		stats = stats.Where("game_version_id = ?", *gameVersionID)
+	}
+
+	var totalPlayCount int
+	var totalPlayTime time.Duration
+
+	err = stats.Select("COUNT(*)").Scan(&totalPlayCount).Error
+	if err != nil {
+		return nil, err
+	}
+
+	err = stats.Where("end_time IS NOT NULL").Select("SUM(TIMESTAMPDIFF(SECOND, start_time, end_time))").Scan(&totalPlayTime).Error
+	if err != nil {
+		return nil, err
+	}
+
 	
-	panic("not implemented")
+
+	return domain.NewGamePlayStats(gameID, totalPlayCount, totalPlayTime, []*domain.HourlyPlayStats{}), nil
+
 }
 
-func (g *GamePlayLogV2) GetGamePlayStats(ctx context.Context, gameID values.GameID, gameVersionID *values.GameVersionID, start, end time.Time) (*domain.GamePlayStats, error) {
-	// TODO: interfaceのコメントを参考に実装を行う
-	panic("not implemented")
-}
-
-func (g *GamePlayLogV2) GetEditionPlayStats(ctx context.Context, editionID values.LauncherVersionID, start, end time.Time) (*domain.EditionPlayStats, error) {
+func (g *GamePlayLogV2) GetEditionPlayStats(_ context.Context, _ values.LauncherVersionID, _ time.Time, _ time.Time) (*domain.EditionPlayStats, error) {
 	// TODO: interfaceのコメントを参考に実装を行う
 	panic("not implemented")
 }
--- src/repository/gorm2/v2_game_play_log_test.go
+++ src/repository/gorm2/v2_game_play_log_test.go
@@ -2,16 +2,22 @@ package gorm2
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/traPtitech/trap-collection-server/src/domain"
 	"github.com/traPtitech/trap-collection-server/src/domain/values"
 	"github.com/traPtitech/trap-collection-server/src/repository"
+	"github.com/traPtitech/trap-collection-server/src/repository/gorm2/schema"
+	"gorm.io/gorm"
 )
 
 func TestCreateGamePlayLog(t *testing.T) {
+	t.Skip("実装していない関数なのでスキップします")
 	t.Parallel()
 
 	ctx := context.Background()
@@ -52,6 +58,7 @@ func TestCreateGamePlayLog(t *testing.T) {
 }
 
 func TestGetGamePlayLog(t *testing.T) {
+	t.Skip("実装していない関数なのでスキップします")
 	t.Parallel()
 
 	ctx := context.Background()
@@ -59,21 +66,21 @@ func TestGetGamePlayLog(t *testing.T) {
 	gamePlayLogRepository := NewGamePlayLogV2(testDB)
 
 	type test struct {
-		description  string
-		playLogID    values.GamePlayLogID
-		expectedLog  *domain.GamePlayLog
-		isErr        bool
-		err          error
+		description string
+		playLogID   values.GamePlayLogID
+		expectedLog *domain.GamePlayLog
+		isErr       bool
+		err         error
 	}
 
 	// TODO: テストを実装する
 	testCases := []test{
 		{
-			description:  "TODO: add test case",
-			playLogID:    values.NewGamePlayLogID(),
-			expectedLog:  nil,
-			isErr:        true,
-			err:          repository.ErrRecordNotFound,
+			description: "TODO: add test case",
+			playLogID:   values.NewGamePlayLogID(),
+			expectedLog: nil,
+			isErr:       true,
+			err:         repository.ErrRecordNotFound,
 		},
 	}
 
@@ -96,6 +103,7 @@ func TestGetGamePlayLog(t *testing.T) {
 }
 
 func TestUpdateGamePlayLogEndTime(t *testing.T) {
+	t.Skip("実装していない関数なのでスキップします")
 	t.Parallel()
 
 	ctx := context.Background()
@@ -138,34 +146,159 @@ func TestUpdateGamePlayLogEndTime(t *testing.T) {
 	}
 }
 
+// テスト用のランダムな日時を生成するヘルパー関数
+func generateRandomEventTimes() (time.Time, time.Time, time.Time, time.Time) {
+	// 1. 基準となる日時をランダムに生成
+	// 例: 過去1年以内（365日 * 24時間）のどこかの時刻
+	randomHoursAgo := time.Duration(rand.Intn(365*24)) * time.Hour
+	baseTime := time.Now().Add(-randomHoursAgo)
+
+	// 2. 各フィールドに加えるランダムな時間を生成
+	// 必ず正の値になるように最小値を設定
+	updateDuration := time.Duration(rand.Intn(60)) * time.Minute     // 0〜59分
+	startDelay := time.Duration(rand.Intn(24)+1) * time.Hour         // 1〜24時間
+	eventDuration := time.Duration(rand.Intn(5*60)+30) * time.Minute // 30分〜5時間29分
+
+	// 3. 大小関係を維持して日時を組み立てる
+	createdAt := baseTime
+	updatedAt := createdAt.Add(updateDuration)
+	startTime := updatedAt.Add(startDelay)
+	endTime := startTime.Add(eventDuration)
+
+	return createdAt, updatedAt, startTime, endTime
+}
+
 func TestGetGamePlayStats(t *testing.T) {
 	t.Parallel()
 
 	ctx := t.Context()
+	db, err := testDB.getDB(ctx)
+	require.NoError(t, err)
+
+	edition1 := schema.EditionTable{
+		ID:   uuid.New(),
+		Name: "Test",
+	}
+	game1 := schema.GameTable2{
+		ID:               uuid.New(),
+		Name:             "Test",
+		VisibilityTypeID: 1,
+	}
+	gameImage1 := schema.GameImageTable2{
+		ID:          uuid.New(),
+		GameID:      game1.ID,
+		ImageTypeID: 1,
+	}
+	gameVideo1 := schema.GameVideoTable2{
+		ID:          uuid.New(),
+		GameID:      game1.ID,
+		VideoTypeID: 1,
+	}
+	gameVersion1 := schema.GameVersionTable2{
+		ID:          uuid.New(),
+		GameID:      game1.ID,
+		GameImageID: gameImage1.ID,
+		GameVideoID: gameVideo1.ID,
+		Name:        "Test",
+		Description: "test",
+	}
+
+	createdAt1, updatedAt1, startTime1, endTime1 := generateRandomEventTimes()
+	gamePlayLog1 := schema.GamePlayLogTable{
+		ID:            uuid.New(),
+		EditionID:     edition1.ID,
+		GameID:        game1.ID,
+		GameVersionID: gameVersion1.ID,
+		StartTime:     startTime1,
+		EndTime:       sql.NullTime{Time: endTime1, Valid: true},
+		CreatedAt:     createdAt1,
+		UpdatedAt:     updatedAt1,
+	}
+
+	createdAt2, updatedAt2, startTime2, endTime2 := generateRandomEventTimes()
+	gamePlayLog2 := schema.GamePlayLogTable{
+		ID:            uuid.New(),
+		EditionID:     edition1.ID,
+		GameID:        game1.ID,
+		GameVersionID: gameVersion1.ID,
+		StartTime:     startTime2,
+		EndTime:       sql.NullTime{Time: endTime2, Valid: true},
+		CreatedAt:     createdAt2,
+		UpdatedAt:     updatedAt2,
+	}
+
+	createdAt3, updatedAt3, startTime3, endTime3 := generateRandomEventTimes()
+	gamePlayLog3 := schema.GamePlayLogTable{
+		ID:            uuid.New(),
+		EditionID:     edition1.ID,
+		GameID:        game1.ID,
+		GameVersionID: gameVersion1.ID,
+		StartTime:     startTime3,
+		EndTime:       sql.NullTime{Time: endTime3, Valid: true},
+		CreatedAt:     createdAt3,
+		UpdatedAt:     updatedAt3,
+	}
+
+	require.NoError(t, db.Create(&edition1).Error)
+	require.NoError(t, db.Create(&game1).Error)
+	require.NoError(t, db.Create(&gameImage1).Error)
+	require.NoError(t, db.Create(&gameVideo1).Error)
+	require.NoError(t, db.Create(&gameVersion1).Error)
+	require.NoError(t, db.Create(&gamePlayLog1).Error)
+	require.NoError(t, db.Create(&gamePlayLog2).Error)
+	require.NoError(t, db.Create(&gamePlayLog3).Error)
+
+	t.Cleanup(func() {
+		ctx := context.Background()
+		require.NoError(t, db.WithContext(ctx).Unscoped().Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&schema.GamePlayLogTable{}).Error)
+		require.NoError(t, db.WithContext(ctx).Unscoped().Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&schema.GameVersionTable2{}).Error)
+		require.NoError(t, db.WithContext(ctx).Unscoped().Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&schema.GameVideoTable2{}).Error)
+		require.NoError(t, db.WithContext(ctx).Unscoped().Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&schema.GameImageTable2{}).Error)
+		require.NoError(t, db.WithContext(ctx).Unscoped().Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&schema.GameTable2{}).Error)
+		require.NoError(t, db.WithContext(ctx).Unscoped().Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&schema.EditionTable{}).Error)
+	})
 
 	gamePlayLogRepository := NewGamePlayLogV2(testDB)
 
 	type test struct {
-		description     string
-		gameID          values.GameID
-		gameVersionID   *values.GameVersionID
-		start           time.Time
-		end             time.Time
-		expectedStats   *domain.GamePlayStats
-		isErr           bool
-		err             error
+		description   string
+		gameID        values.GameID
+		gameVersionID values.GameVersionID
+		start         time.Time
+		end           time.Time
+		expectedStats *domain.GamePlayStats
+		isErr         bool
+		err           error
 	}
 
 	// TODO: テストを実装する
 	testCases := []test{
 		{
-			description:     "TODO: add test case",
-			gameID:          values.NewGameID(),
-			gameVersionID:   nil,
-			start:           time.Now().Add(-24 * time.Hour),
-			end:             time.Now(),
-			expectedStats:   nil,
-			isErr:           false,
+			description:   "普通に取得",
+			gameID:        values.GameID(game1.ID),
+			gameVersionID: values.GameVersionID(gameVersion1.ID),
+			start:         time.Now().Add(-24 * time.Hour), // 24時間前
+			end:           time.Now(),
+			expectedStats: nil,
+			isErr:         false,
+		},
+		{
+			description:   "バージョンID無しで取得",
+			gameID:        values.GameID(game1.ID),
+			gameVersionID: nil,
+			start:         time.Now().Add(-24 * time.Hour), // 24時間前
+			end:           time.Now(),
+			expectedStats: nil,
+			isErr:         false,
+		},
+		{
+			description:   "存在しないゲームIDで取得",
+			gameID:        values.NewGameID(),
+			gameVersionID: values.GameVersionID(gameVersion1.ID),
+			start:         time.Now().Add(-24 * time.Hour), // 24時間前
+			end:           time.Now(),
+			expectedStats: nil,
+			isErr:         false,
 		},
 	}
 
@@ -188,6 +321,7 @@ func TestGetGamePlayStats(t *testing.T) {
 }
 
 func TestGetEditionPlayStats(t *testing.T) {
+	t.Skip("実装していない関数なのでスキップします")
 	t.Parallel()
 
 	ctx := context.Background()
