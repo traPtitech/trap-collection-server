10b9a6fd9854e8d3471b9106cb7281d732ea2ffb
masky5859 <mathsuky2406@gmail.com>
2025-10-05 17:06:55 +0900

    conflict解消漏れ

--- src/repository/gorm2/v2_game_play_log_test.go
+++ src/repository/gorm2/v2_game_play_log_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/traPtitech/trap-collection-server/src/domain"
 	"github.com/traPtitech/trap-collection-server/src/domain/values"
 	"github.com/traPtitech/trap-collection-server/src/repository"
@@ -440,185 +441,277 @@ func TestCreateGamePlayLog(t *testing.T) {
 	}
 }
 
-// func TestGetGamePlayLog(t *testing.T) {
-// 	t.Parallel()
+func TestGetGamePlayLog(t *testing.T) {
 
-// 	ctx := context.Background()
+	ctx := t.Context()
+	db, err := testDB.getDB(ctx)
+	require.NoError(t, err)
 
-// 	gamePlayLogRepository := NewGamePlayLogV2(testDB)
+	now := time.Now()
 
-// 	type test struct {
-// 		description string
-// 		playLogID   values.GamePlayLogID
-// 		expectedLog *domain.GamePlayLog
-// 		isErr       bool
-// 		err         error
-// 	}
+	edition1 := schema.EditionTable{
+		ID:   uuid.New(),
+		Name: "Test",
+	}
+	game1 := schema.GameTable2{
+		ID:               uuid.New(),
+		Name:             "Test",
+		VisibilityTypeID: 1,
+	}
+	gameImage1 := schema.GameImageTable2{
+		ID:          uuid.New(),
+		GameID:      game1.ID,
+		ImageTypeID: 1,
+	}
+	gameVideo1 := schema.GameVideoTable2{
+		ID:          uuid.New(),
+		GameID:      game1.ID,
+		VideoTypeID: 1,
+	}
+	gameVersion1 := schema.GameVersionTable2{
+		ID:          uuid.New(),
+		GameID:      game1.ID,
+		GameImageID: gameImage1.ID,
+		GameVideoID: gameVideo1.ID,
+		Name:        "Test",
+		Description: "test",
+	}
+	gamePlayLog1 := schema.GamePlayLogTable{
+		ID:            uuid.New(),
+		EditionID:     edition1.ID,
+		GameID:        game1.ID,
+		GameVersionID: gameVersion1.ID,
+		StartTime:     now,
+		CreatedAt:     now,
+		UpdatedAt:     now,
+	}
+	gamePlayLog2 := schema.GamePlayLogTable{
+		ID:            uuid.New(),
+		EditionID:     edition1.ID,
+		GameID:        game1.ID,
+		GameVersionID: gameVersion1.ID,
+		StartTime:     now.Add(-1 * time.Hour),
+		EndTime:       sql.NullTime{Time: now, Valid: true},
+		CreatedAt:     now.Add(-1 * time.Hour),
+		UpdatedAt:     now,
+	}
 
-// 	// TODO: テストを実装する
-// 	testCases := []test{
-// 		{
-// 			description: "TODO: add test case",
-// 			playLogID:   values.NewGamePlayLogID(),
-// 			expectedLog: nil,
-// 			isErr:       true,
-// 			err:         repository.ErrRecordNotFound,
-// 		},
-// 	}
+	require.NoError(t, db.Create(&edition1).Error)
+	require.NoError(t, db.Create(&game1).Error)
+	require.NoError(t, db.Create(&gameImage1).Error)
+	require.NoError(t, db.Create(&gameVideo1).Error)
+	require.NoError(t, db.Create(&gameVersion1).Error)
+	require.NoError(t, db.Create(&gamePlayLog1).Error)
+	require.NoError(t, db.Create(&gamePlayLog2).Error)
 
-// 	for _, testCase := range testCases {
-// 		t.Run(testCase.description, func(t *testing.T) {
-// 			log, err := gamePlayLogRepository.GetGamePlayLog(ctx, testCase.playLogID)
+	t.Cleanup(func() {
+		ctx := context.Background()
+		require.NoError(t, db.WithContext(ctx).Unscoped().Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&schema.GamePlayLogTable{}).Error)
+		require.NoError(t, db.WithContext(ctx).Unscoped().Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&schema.GameVersionTable2{}).Error)
+		require.NoError(t, db.WithContext(ctx).Unscoped().Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&schema.GameVideoTable2{}).Error)
+		require.NoError(t, db.WithContext(ctx).Unscoped().Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&schema.GameImageTable2{}).Error)
+		require.NoError(t, db.WithContext(ctx).Unscoped().Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&schema.GameTable2{}).Error)
+		require.NoError(t, db.WithContext(ctx).Unscoped().Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&schema.EditionTable{}).Error)
+	})
 
-// 			if testCase.isErr {
-// 				if testCase.err == nil {
-// 					assert.Error(t, err)
-// 				} else {
-// 					assert.ErrorIs(t, err, testCase.err)
-// 				}
-// 			} else {
-// 				assert.NoError(t, err)
-// 				assert.Equal(t, testCase.expectedLog, log)
-// 			}
-// 		})
-// 	}
-// }
+	type test struct {
+		description     string
+		playLogID       values.GamePlayLogID
+		expectedPlayLog *domain.GamePlayLog
+		expectedErr     error
+	}
 
-// func TestUpdateGamePlayLogEndTime(t *testing.T) {
-// 	t.Parallel()
+	testCases := []test{
+		{
+			description: "正常な場合(EndTimeがNULL)",
+			playLogID:   values.GamePlayLogID(gamePlayLog1.ID),
+			expectedPlayLog: domain.NewGamePlayLog(
+				values.GamePlayLogID(gamePlayLog1.ID),
+				values.LauncherVersionID(gamePlayLog1.EditionID),
+				values.GameID(gamePlayLog1.GameID),
+				values.GameVersionID(gamePlayLog1.GameVersionID),
+				gamePlayLog1.StartTime,
+				nil,
+				gamePlayLog1.CreatedAt,
+				gamePlayLog1.UpdatedAt,
+			),
+		},
+		{
+			description: "正常な場合(EndTimeが非NULL)",
+			playLogID:   values.GamePlayLogID(gamePlayLog2.ID),
+			expectedPlayLog: domain.NewGamePlayLog(
+				values.GamePlayLogID(gamePlayLog2.ID),
+				values.LauncherVersionID(gamePlayLog2.EditionID),
+				values.GameID(gamePlayLog2.GameID),
+				values.GameVersionID(gamePlayLog2.GameVersionID),
+				gamePlayLog2.StartTime,
+				&gamePlayLog2.EndTime.Time,
+				gamePlayLog2.CreatedAt,
+				gamePlayLog2.UpdatedAt,
+			),
+		},
+		{
+			description: "存在しない場合",
+			playLogID:   values.NewGamePlayLogID(),
+			expectedErr: repository.ErrRecordNotFound,
+		},
+	}
 
-// 	ctx := context.Background()
+	gamePlayLogRepository := NewGamePlayLogV2(testDB)
 
-// 	gamePlayLogRepository := NewGamePlayLogV2(testDB)
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			playLog, err := gamePlayLogRepository.GetGamePlayLog(ctx, testCase.playLogID)
 
-// 	type test struct {
-// 		description string
-// 		playLogID   values.GamePlayLogID
-// 		endTime     time.Time
-// 		isErr       bool
-// 		err         error
-// 	}
+			if testCase.expectedErr != nil {
+				assert.ErrorIs(t, err, testCase.expectedErr)
+				assert.Nil(t, playLog)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, playLog)
+				assert.Equal(t, testCase.expectedPlayLog, playLog)
+			}
+		})
+	}
+}
 
-// 	// TODO: テストを実装する
-// 	testCases := []test{
-// 		{
-// 			description: "TODO: add test case",
-// 			playLogID:   values.NewGamePlayLogID(),
-// 			endTime:     time.Now(),
-// 			isErr:       true,
-// 			err:         repository.ErrNoRecordUpdated,
-// 		},
-// 	}
+func TestUpdateGamePlayLogEndTime(t *testing.T) {
+	t.Skip("この機能はまだ実装されていないため、テストをスキップします。")
+	t.Parallel()
 
-// 	for _, testCase := range testCases {
-// 		t.Run(testCase.description, func(t *testing.T) {
-// 			err := gamePlayLogRepository.UpdateGamePlayLogEndTime(ctx, testCase.playLogID, testCase.endTime)
+	ctx := context.Background()
 
-// 			if testCase.isErr {
-// 				if testCase.err == nil {
-// 					assert.Error(t, err)
-// 				} else {
-// 					assert.ErrorIs(t, err, testCase.err)
-// 				}
-// 			} else {
-// 				assert.NoError(t, err)
-// 			}
-// 		})
-// 	}
-// }
+	gamePlayLogRepository := NewGamePlayLogV2(testDB)
 
-// func TestGetGamePlayStats(t *testing.T) {
-// 	t.Parallel()
+	type test struct {
+		description string
+		playLogID   values.GamePlayLogID
+		endTime     time.Time
+		isErr       bool
+		err         error
+	}
 
-// 	ctx := context.Background()
+	// TODO: テストを実装する
+	testCases := []test{
+		{
+			description: "TODO: add test case",
+			playLogID:   values.NewGamePlayLogID(),
+			endTime:     time.Now(),
+			isErr:       true,
+			err:         repository.ErrNoRecordUpdated,
+		},
+	}
 
-// 	gamePlayLogRepository := NewGamePlayLogV2(testDB)
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			err := gamePlayLogRepository.UpdateGamePlayLogEndTime(ctx, testCase.playLogID, testCase.endTime)
 
-// 	type test struct {
-// 		description   string
-// 		gameID        values.GameID
-// 		gameVersionID *values.GameVersionID
-// 		start         time.Time
-// 		end           time.Time
-// 		expectedStats *domain.GamePlayStats
-// 		isErr         bool
-// 		err           error
-// 	}
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else {
+					assert.ErrorIs(t, err, testCase.err)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+func TestGetGamePlayStats(t *testing.T) {
+	t.Skip("この機能はまだ実装されていないため、テストをスキップします。")
+	t.Parallel()
 
-// 	// TODO: テストを実装する
-// 	testCases := []test{
-// 		{
-// 			description:   "TODO: add test case",
-// 			gameID:        values.NewGameID(),
-// 			gameVersionID: nil,
-// 			start:         time.Now().Add(-24 * time.Hour),
-// 			end:           time.Now(),
-// 			expectedStats: nil,
-// 			isErr:         false,
-// 		},
-// 	}
+	ctx := context.Background()
 
-// 	for _, testCase := range testCases {
-// 		t.Run(testCase.description, func(t *testing.T) {
-// 			stats, err := gamePlayLogRepository.GetGamePlayStats(ctx, testCase.gameID, testCase.gameVersionID, testCase.start, testCase.end)
+	gamePlayLogRepository := NewGamePlayLogV2(testDB)
 
-// 			if testCase.isErr {
-// 				if testCase.err == nil {
-// 					assert.Error(t, err)
-// 				} else {
-// 					assert.ErrorIs(t, err, testCase.err)
-// 				}
-// 			} else {
-// 				assert.NoError(t, err)
-// 				assert.Equal(t, testCase.expectedStats, stats)
-// 			}
-// 		})
-// 	}
-// }
+	type test struct {
+		description   string
+		gameID        values.GameID
+		gameVersionID *values.GameVersionID
+		start         time.Time
+		end           time.Time
+		expectedStats *domain.GamePlayStats
+		isErr         bool
+		err           error
+	}
 
-// func TestGetEditionPlayStats(t *testing.T) {
-// 	t.Parallel()
+	// TODO: テストを実装する
+	testCases := []test{
+		{
+			description:   "TODO: add test case",
+			gameID:        values.NewGameID(),
+			gameVersionID: nil,
+			start:         time.Now().Add(-24 * time.Hour),
+			end:           time.Now(),
+			expectedStats: nil,
+			isErr:         false,
+		},
+	}
 
-// 	ctx := context.Background()
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			stats, err := gamePlayLogRepository.GetGamePlayStats(ctx, testCase.gameID, testCase.gameVersionID, testCase.start, testCase.end)
 
-// 	gamePlayLogRepository := NewGamePlayLogV2(testDB)
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else {
+					assert.ErrorIs(t, err, testCase.err)
+				}
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, testCase.expectedStats, stats)
+			}
+		})
+	}
+}
 
-// 	type test struct {
-// 		description   string
-// 		editionID     values.LauncherVersionID
-// 		start         time.Time
-// 		end           time.Time
-// 		expectedStats *domain.EditionPlayStats
-// 		isErr         bool
-// 		err           error
-// 	}
+func TestGetEditionPlayStats(t *testing.T) {
+	t.Skip("この機能はまだ実装されていないため、テストをスキップします。")
+	t.Parallel()
 
-// 	// TODO: テストを実装する
-// 	testCases := []test{
-// 		{
-// 			description:   "TODO: add test case",
-// 			editionID:     values.NewLauncherVersionID(),
-// 			start:         time.Now().Add(-24 * time.Hour),
-// 			end:           time.Now(),
-// 			expectedStats: nil,
-// 			isErr:         false,
-// 		},
-// 	}
+	ctx := context.Background()
 
-// 	for _, testCase := range testCases {
-// 		t.Run(testCase.description, func(t *testing.T) {
-// 			stats, err := gamePlayLogRepository.GetEditionPlayStats(ctx, testCase.editionID, testCase.start, testCase.end)
+	gamePlayLogRepository := NewGamePlayLogV2(testDB)
 
-// 			if testCase.isErr {
-// 				if testCase.err == nil {
-// 					assert.Error(t, err)
-// 				} else {
-// 					assert.ErrorIs(t, err, testCase.err)
-// 				}
-// 			} else {
-// 				assert.NoError(t, err)
-// 				assert.Equal(t, testCase.expectedStats, stats)
-// 			}
-// 		})
-// 	}
-// }
+	type test struct {
+		description   string
+		editionID     values.LauncherVersionID
+		start         time.Time
+		end           time.Time
+		expectedStats *domain.EditionPlayStats
+		isErr         bool
+		err           error
+	}
+
+	// TODO: テストを実装する
+	testCases := []test{
+		{
+			description:   "TODO: add test case",
+			editionID:     values.NewLauncherVersionID(),
+			start:         time.Now().Add(-24 * time.Hour),
+			end:           time.Now(),
+			expectedStats: nil,
+			isErr:         false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			stats, err := gamePlayLogRepository.GetEditionPlayStats(ctx, testCase.editionID, testCase.start, testCase.end)
+
+			if testCase.isErr {
+				if testCase.err == nil {
+					assert.Error(t, err)
+				} else {
+					assert.ErrorIs(t, err, testCase.err)
+				}
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, testCase.expectedStats, stats)
+			}
+		})
+	}
+}
