5dbcf449306c7d1b6f191b549a645f81dadf52a7
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-11-02 22:06:23 +0900

    :sparkles: playLog削除のhandler実装

--- src/handler/v2/game_play_log.go
+++ src/handler/v2/game_play_log.go
@@ -213,5 +213,18 @@ func (gpl *GamePlayLog) GetEditionPlayStats(c echo.Context, editionIDPath openap
 }
 
 func (gpl *GamePlayLog) DeleteGamePlayLog(c echo.Context, editionIDPath openapi.EditionIDInPath, gameIDPath openapi.GameIDInPath, playLogIDPath openapi.PlayLogIDInPath) error {
-	return nil
+	editionID := values.NewLauncherVersionIDFromUUID(editionIDPath)
+	gameID := values.NewGameIDFromUUID(gameIDPath)
+	playLogID := values.GamePlayLogIDFromUUID(playLogIDPath)
+
+	err := gpl.gamePlayLogService.DeleteGamePlayLog(c.Request().Context(), editionID, gameID, playLogID)
+	if errors.Is(err, service.ErrInvalidPlayLogID) {
+		return echo.NewHTTPError(http.StatusNotFound, "play log not found")
+	}
+	if err != nil {
+		log.Printf("error: failed to delete game play log: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to delete game play log")
+	}
+
+	return c.NoContent(http.StatusOK)
 }
