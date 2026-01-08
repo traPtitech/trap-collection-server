cc6f3eeba1529b0bbe7dbd9bd5a0874b6e163749
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2026-01-08 22:17:45 +0900

    :factory: マイグレーションの生成

--- migrations/20260108131449_create_game_creators.sql
+++ migrations/20260108131449_create_game_creators.sql
@@ -1,3 +1,13 @@
+-- Create "game_creator_custom_jobs" table
+CREATE TABLE `game_creator_custom_jobs` (
+  `id` varchar(36) NOT NULL,
+  `game_id` varchar(36) NOT NULL,
+  `display_name` varchar(64) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (current_timestamp()),
+  PRIMARY KEY (`id`),
+  INDEX `idx_game_creator_custom_jobs_game_id` (`game_id`),
+  CONSTRAINT `fk_game_creator_custom_jobs_game` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT
+) CHARSET utf8mb4 COLLATE utf8mb4_general_ci;
 -- Create "game_creators" table
 CREATE TABLE `game_creators` (
   `id` varchar(36) NOT NULL,
@@ -10,15 +20,14 @@ CREATE TABLE `game_creators` (
   UNIQUE INDEX `idx_unique_game_id_user_id` (`game_id`, `user_id`),
   CONSTRAINT `fk_game_creators_game` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT
 ) CHARSET utf8mb4 COLLATE utf8mb4_general_ci;
--- Create "game_creator_custom_jobs" table
-CREATE TABLE `game_creator_custom_jobs` (
-  `id` varchar(36) NOT NULL,
+-- Create "game_creator_custom_job_relations" table
+CREATE TABLE `game_creator_custom_job_relations` (
   `game_creator_id` varchar(36) NOT NULL,
-  `custom_name` varchar(64) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT (current_timestamp()),
-  PRIMARY KEY (`id`),
-  INDEX `idx_game_creator_custom_jobs_game_creator_id` (`game_creator_id`),
-  CONSTRAINT `fk_game_creators_custom_creator_jobs` FOREIGN KEY (`game_creator_id`) REFERENCES `game_creators` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT
+  `custom_job_id` varchar(36) NOT NULL,
+  PRIMARY KEY (`game_creator_id`, `custom_job_id`),
+  INDEX `fk_game_creator_custom_job_relations_game_creator_customa6516cf7` (`custom_job_id`),
+  CONSTRAINT `fk_game_creator_custom_job_relations_game_creator_customa6516cf7` FOREIGN KEY (`custom_job_id`) REFERENCES `game_creator_custom_jobs` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT,
+  CONSTRAINT `fk_game_creator_custom_job_relations_game_creator_table` FOREIGN KEY (`game_creator_id`) REFERENCES `game_creators` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT
 ) CHARSET utf8mb4 COLLATE utf8mb4_general_ci;
 -- Create "game_creator_jobs" table
 CREATE TABLE `game_creator_jobs` (
