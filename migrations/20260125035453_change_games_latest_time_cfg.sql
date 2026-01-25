bf4bef0421500bb2161c372be0afedc11a43e2d6
ebisen1444 <0294ryotaro@gmail.com>
2026-01-25 14:21:05 +0900

    LatesGameVersionAtの設定を-allにしてたのを書き込み読み込み不可に

--- migrations/20260125035453_change_games_latest_time_cfg.sql
+++
@@ -1,2 +0,0 @@
--- Modify "games" table
-ALTER TABLE `games` DROP COLUMN `latest_version_updated_at`;
---
+++ migrations/20260125052027_change_games_latest_time_cfg.sql
@@ -0,0 +1,2 @@
+-- Modify "games" table
+ALTER TABLE `games` MODIFY COLUMN `latest_version_updated_at` datetime NULL;
