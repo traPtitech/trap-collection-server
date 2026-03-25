bff77fb094d812bdc5fb85109389bdf218a8bf55
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2026-03-25 21:32:30 +0900

    :memo: コメントの修正

--- src/service/game_creator.go
+++ src/service/game_creator.go
@@ -22,7 +22,7 @@ type GameCreator interface {
 	// ゲームクリエイターのジョブを置き換える形で編集する。
 	// 該当するゲームが存在しない場合、ErrInvalidGameIDを返す。
 	// 存在しないユーザーIDが含まれる場合、 ErrInvalidUserIDを返す。
-	// すでに存在するカスタムジョブ名が新しいカスタムジョブとして含まれる場合、ErrDuplicateGameCreatorCustomJobNameを返す。
+	// すでに存在するカスタムジョブ名が新しいカスタムジョブとして含まれる場合、ErrDuplicateCustomJobDisplayName を返す。
 	// 同一ユーザーに同じjob idが複数含まれる場合は、ErrDuplicateGameCreatorJobIDを返す。
 	EditGameCreators(ctx context.Context, session *domain.OIDCSession, gameID values.GameID, inputs []*EditGameCreatorJobInput) error
 }
