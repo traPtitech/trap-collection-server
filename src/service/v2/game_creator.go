a75d94bd4e570b69a47a47f440c4fcc04ce95c15
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2026-04-24 21:02:11 +0900

    :adhesive_bandage: creator側でactive usersを取っていたところをall usersに変更

--- src/service/v2/game_creator.go
+++ src/service/v2/game_creator.go
@@ -127,8 +127,7 @@ func (gc *GameCreator) validateEditGameCreatorsInput(
 	presetJobsMap map[values.GameCreatorJobID]*domain.GameCreatorJob,
 	inputs []*service.EditGameCreatorJobInput,
 ) (*editGameCreatorsValidatedInput, error) {
-	// TODO: 今の実装は現役しか取得できないので、凍結済みユーザーを取得できるようにする
-	activeMembers, err := gc.user.getActiveUsers(ctx, session)
+	activeMembers, err := gc.user.getAllUsers(ctx, session)
 	if err != nil {
 		return nil, fmt.Errorf("get active users: %w", err)
 	}
