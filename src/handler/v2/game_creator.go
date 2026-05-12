c29ed50b11bfb31cb993b0d44b79c7042b0d8077
w4mally <kazuma246r@gmail.com>
2026-05-12 20:55:45 +0900

    GET /games/{gameID}/creators/jobs のハンドラー実装

--- src/handler/v2/game_creator.go
+++ src/handler/v2/game_creator.go
@@ -1,18 +1,58 @@
 package v2
 
 import (
+	"errors"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/traPtitech/trap-collection-server/src/domain/values"
 	"github.com/traPtitech/trap-collection-server/src/handler/v2/openapi"
+	"github.com/traPtitech/trap-collection-server/src/service"
 )
 
-type GameCreator struct{}
+type GameCreator struct {
+	gameCreatorService service.GameCreator
+}
+
+func NewGameCreator(gameCreatorService service.GameCreator) *GameCreator {
+	return &GameCreator{
+		gameCreatorService: gameCreatorService,
+	}
+}
 
 // ゲームクリエイターのジョブ一覧の取得
 // (GET /games/{gameID}/creators/jobs)
-func (gc *GameCreator) GetGameCreatorJobs(c echo.Context, _ openapi.GameIDInPath) error {
-	return c.NoContent(http.StatusNotImplemented)
+func (gc *GameCreator) GetGameCreatorJobs(c echo.Context, gameID openapi.GameIDInPath) error {
+	presentJobs, customJobs, err := gc.gameCreatorService.GetGameCreatorJobs(
+		c.Request().Context(),
+		values.NewGameIDFromUUID(gameID),
+	)
+	if errors.Is(err, service.ErrInvalidGameID) {
+		return echo.NewHTTPError(http.StatusNotFound, "Invalid gameID")
+	}
+	if err != nil {
+		log.Printf("error: faild to get game creator jobs: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "faild to get game creator jobs")
+	}
+
+	res := make([]openapi.GameCreatorJob, 0, len(presentJobs)+len(customJobs))
+	for _, job := range presentJobs {
+		res = append(res, openapi.GameCreatorJob{
+			Id:          openapi.GameCreatorJobID(job.GetID()),
+			DisplayName: openapi.GameCreatorJobDisplayName(job.GetDisplayName()),
+			IsCustomJob: false,
+		})
+	}
+	for _, job := range customJobs {
+		res = append(res, openapi.GameCreatorJob{
+			Id:          openapi.GameCreatorJobID(job.GetID()),
+			DisplayName: openapi.GameCreatorJobDisplayName(job.GetDisplayName()),
+			IsCustomJob: true,
+		})
+	}
+
+	return c.JSON(http.StatusOK, res)
 }
 
 // ゲームクリエイター一覧の取得
