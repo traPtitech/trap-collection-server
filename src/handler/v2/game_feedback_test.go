cb439faa2c2e0bd6ebbada135d508c64497253de
masky5859 <mathsuky2406@gmail.com>
2026-09-18 16:07:18 +0900

    Test null feedback question requests

--- src/handler/v2/game_feedback_test.go
+++ src/handler/v2/game_feedback_test.go
@@ -167,4 +167,13 @@ func TestPutFeedbackQuestions(t *testing.T) {
 		require.ErrorAs(t, err, &httpError)
 		assert.Equal(t, http.StatusBadRequest, httpError.Code)
 	})
+
+	t.Run("rejects null questions before service", func(t *testing.T) {
+		handler := NewGameFeedback(mock.NewMockGameFeedback(gomock.NewController(t)))
+		c, _, _ := setupTestRequest(t, http.MethodPut, "/games/"+uuid.UUID(gameID).String()+"/feedback-questions", withJSONBody(t, map[string]any{"questions": nil}))
+		err := handler.PutFeedbackQuestions(c, openapi.GameIDInPath(gameID))
+		var httpError *echo.HTTPError
+		require.ErrorAs(t, err, &httpError)
+		assert.Equal(t, http.StatusBadRequest, httpError.Code)
+	})
 }
